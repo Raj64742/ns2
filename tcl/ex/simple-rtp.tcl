aaeaabef36c57a33263bc2331d8e8db056b3a09f
elan <elan>
1996-12-31 22:54:16 +0000

    Modified to match new RTPSession API.

--- tcl/ex/simple-rtp.tcl
+++ tcl/ex/simple-rtp.tcl
@@ -1,3 +1,37 @@
+#
+# Copyright (c) @ Regents of the University of California.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+# 	This product includes software developed by the MASH Research
+# 	Group at the University of California Berkeley.
+# 4. Neither the name of the University nor of the Research Group may be
+#    used to endorse or promote products derived from this software without
+#    specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/ex/simple-rtp.tcl,v 1.2 1996/12/31 22:54:16 elan Exp $
+#
 
 set ns [new MultiSim]
 
@@ -18,10 +52,10 @@ set s1 [new Session/RTP]
 set s2 [new Session/RTP]
 set s3 [new Session/RTP]
 
-$s0 tx-bw 400kb/s
-$s1 tx-bw 400kb/s
-$s2 tx-bw 400kb/s
-$s3 tx-bw 400kb/s
+$s0 session_bw 400kb/s
+$s1 session_bw 400kb/s
+$s2 session_bw 400kb/s
+$s3 session_bw 400kb/s
 
 $s0 attach-to $n0
 $s1 attach-to $n1
@@ -30,19 +64,19 @@ $s3 attach-to $n3
 
 $ns at 1.4 "$s0 join-group 0x8000"
 $ns at 1.5 "$s0 start"
-$ns at 1.6 "$s0 transmit"
+$ns at 1.6 "$s0 transmit 400kb/s"
 
 $ns at 1.7 "$s1 join-group 0x8000"
 $ns at 1.8 "$s1 start"
-$ns at 1.9 "$s1 transmit"
+$ns at 1.9 "$s1 transmit 400kb/s"
 
 $ns at 2.0 "$s2 join-group 0x8000"
 $ns at 2.1 "$s2 start"
-$ns at 2.2 "$s2 transmit"
+$ns at 2.2 "$s2 transmit 400kb/s"
 
 $ns at 2.3 "$s3 join-group 0x8000"
 $ns at 2.4 "$s3 start"
-$ns at 2.5 "$s3 transmit"
+$ns at 2.5 "$s3 transmit 400kb/s"
 
 $ns at 4.0 "finish"
 
