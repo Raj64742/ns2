0105dda151b2320939f6458c27d123668dd03ae3
mccanne <mccanne>
1997-01-28 03:25:16 +0000

    oops

--- tcl/ex/simple-nam.tcl
+++ tcl/ex/simple-nam.tcl
@@ -4,13 +4,13 @@ proc nam_config {net} {
         $net node 2 circe
         $net node 3 circe
 
-        mklink $net 0 2 1.5Mb 10ms right
+        mklink $net 0 2 1.5Mb 2ms right-up
         mklink $net 2 3 1.5Mb 10ms right
-#        mklink $net 0 1 1.5Mb 10ms right-up
-        mklink $net 1 2 1.5Mb 10ms right-down
+#        mklink $net 0 1 1.5Mb 2ms
+        mklink $net 1 2 1.5Mb 2ms right-down
 
-        $net queue 0 2 0.5
-        $net queue 2 0 0.5
+        $net queue 2 3 0.5
+        $net queue 3 2 0.5
 
         $net color 1 red
         $net color 2 white
--- tcl/ex/ss.tcl
+++ tcl/ex/ss.tcl
@@ -6,25 +6,30 @@ set n1 [$ns node]
 set n2 [$ns node]
 set n3 [$ns node]
 
+puts n0=[$n0 id]
+puts n1=[$n1 id]
+puts n2=[$n2 id]
+puts n3=[$n3 id]
+
 set f [open out.tr w]
 $ns trace-all $f
 
-$ns duplex-link $n0 $n1 1.5Mb 10ms drop-tail
-$ns duplex-link $n0 $n2 1.5Mb 10ms drop-tail
-$ns duplex-link $n1 $n2 1.5Mb 10ms drop-tail
-$ns duplex-link $n2 $n3 1.5Mb 10ms drop-tail
+#$ns duplex-link $n0 $n1 1.5Mb 10ms DropTail
+$ns duplex-link $n0 $n2 10Mb 2ms DropTail
+$ns duplex-link $n1 $n2 10Mb 2ms DropTail
+$ns duplex-link $n2 $n3 1.5Mb 10ms DropTail
 
-set cbr0 [new agent/cbr]
+set cbr0 [new Agent/CBR]
 $ns attach-agent $n0 $cbr0
 
-set cbr1 [new agent/cbr]
+set cbr1 [new Agent/CBR]
 $ns attach-agent $n3 $cbr1
 $cbr1 set cls 1
 
-set null0 [new agent/null]
+set null0 [new Agent/Null]
 $ns attach-agent $n3 $null0
 
-set null1 [new agent/null]
+set null1 [new Agent/Null]
 $ns attach-agent $n1 $null1
 
 $ns connect $cbr0 $null0
@@ -33,17 +38,17 @@ $ns connect $cbr1 $null1
 $ns at 1.0 "$cbr0 start"
 $ns at 1.1 "$cbr1 start"
 
-set tcp [new agent/tcp]
+set tcp [new Agent/TCP]
 $tcp set cls 2
-set sink [new agent/tcp-sink]
+set sink [new Agent/TCPSink]
 $ns attach-agent $n0 $tcp
 $ns attach-agent $n3 $sink
 $ns connect $tcp $sink
-set ftp [new source/ftp]
+set ftp [new Source/FTP]
 $ftp set agent $tcp
 $ns at 1.2 "$ftp start"
 
-puts [$cbr0 set packet-size]
+puts [$cbr0 set packetSize_]
 puts [$cbr0 set interval_]
 
 $ns at 2.0 "finish"
