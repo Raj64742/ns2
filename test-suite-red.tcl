3c173aeee26ca90d4450a8ceb0f0f2172a1c13bc
kfall <kfall>
1997-03-27 22:38:21 +0000

    Update this version of test-suite-red.tcl to match the ns-1.2a3 version.

--- test-suite-red.tcl
+++ test-suite-red.tcl
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 1995 The Regents of the University of California.
+# Copyright (c) 1995-1997 The Regents of the University of California.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -30,20 +30,26 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/Attic/test-suite-red.tcl,v 1.1 1996/12/19 03:22:46 mccanne Exp $
-#
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/Attic/test-suite-red.tcl,v 1.2 1997/03/27 22:38:21 kfall Exp $
 #
 # This test suite reproduces most of the tests from the following note:
-# Floyd, S., Simulator Tests. July 1995.  
-# URL ftp://ftp.ee.lbl.gov/papers/simtests.ps.Z.
+# Floyd, S., 
+# Ns Simulator Tests for Random Early Detection (RED), October 1996.
+# URL ftp://ftp.ee.lbl.gov/papers/redsims.ps.Z.
 #
-# To run all tests:  test-all
-# To run individual tests:
-# ns test-suite.tcl tahoe1
-# ns test-suite.tcl tahoe2
-# ...
-#
-#
+# To run all tests: test-all-red
+
+set flowfile fairflow.tr
+set flowgraphfile fairflow.xgr
+set pthresh 100
+
+# to generate flow graph with unforced drops:
+#       set category 1
+#       set awkprocedure unforcedmakeawk
+# to generate flow graph with forced drops:
+#       set category 0
+#       set awkprocedure forcedmakeawk
+
 # Create a simple six node topology:
 #
 #        s1		    s3
@@ -179,11 +185,199 @@ proc openTrace { stopTime testName } {
 	return $T
 }
 
+proc create_flowstats {} {
+
+	global r1 r2 r1fm flowfile pthresh category
+
+	set r1fm [new flowmgr srcdstcls]
+	set flowdesc [open $flowfile w]
+	$r1fm attach $flowdesc
+## 2 "categories" of drops
+        $r1fm ndropcats 2
+## after 100 total packets have been dropped, invoke flowDump
+        $r1fm cat-drops-pthresh $category $pthresh
+	$r1fm cat-drops-pcallback $category flowDump 
+	[ns link $r1 $r2] flow-mgr $r1fm
+}
+
+proc create_flowstats1 {} {
+
+	global r1 r2 r1fm flowfile pthresh 
+
+#	set r1fm [new flowmgr srcdstcls]
+ 	set r1fm [new flowmgr redsrcdstcls]
+	set flowdesc [open $flowfile w]
+	$r1fm attach $flowdesc
+## 2 "categories" of drops
+        $r1fm ndropcats 2
+## after 100 total packets have been dropped, invoke flowDump1
+        $r1fm total-drops-pthresh $pthresh
+	$r1fm total-drops-pcallback flowDump1 
+	[ns link $r1 $r2] flow-mgr $r1fm
+}
+
+#
+# awk code used to produce:
+#       x axis: # arrivals for this flow+category / # total arrivals [bytes]
+#       y axis: # drops for this flow+category / # drops this category [pkts]
+proc unforcedmakeawk { } {
+        set awkCode {
+            BEGIN { print "\"flow 0" }
+            {
+                if ($2 != prev) {
+                        print " "; print "\"flow " $2; print 100.0 * $9/$13, 100.0 * $10 / $14; prev = $2
+                } else
+                        print 100.0 * $9 / $13, 100.0 * $10 / $14
+            }
+        }
+        return $awkCode
+}
+
+#
+# awk code used to produce:
+#       x axis: # arrivals for this flow+category / # total arrivals [bytes]
+#       y axis: # drops for this flow+category / # drops this category [bytes]
+proc forcedmakeawk { } {
+        set awkCode {
+            BEGIN { print "\"flow 0" }
+            {
+                if ($2 != prev) {
+                        print " "; print "\"flow " $2; print 100.0 * $9/$13, 100.0 * $11 / $15; prev = $2
+                } else
+                        print 100.0 * $9 / $13, 100.0 * $11 / $15
+            }
+        }
+        return $awkCode
+}
+
+#
+# awk code used to produce:
+#      x axis: # arrivals for this flow+category / # total arrivals [bytes]
+#      y axis: # drops for this flow / # drops [pkts and bytes combined]
+proc allmakeawk { } {
+        set awkCode {
+            BEGIN { print "\"flow 0"; cat0=0; cat1=0}
+            {
+                if ($1 != prevtime && cat1 + cat0 > 0){
+			cat1_part = frac_packets * cat1 / ( cat1 + cat0 ) 
+			cat0_part = frac_bytes * cat0 / ( cat1 + cat0 ) 
+                        print 100.0 * frac_arrivals, 100.0 * ( cat1_part + cat0_part )
+			frac_bytes = 0; frac_packets = 0; frac_arrivals = 0;
+			cat1 = 0; cat0 = 0;
+			prevtime = $1
+		}
+		if ($2 != prev) {
+			print " "; print "\"flow "prev;
+			prev = $2
+		}
+		if ($3==0) {  
+			if ($15>0) {frac_bytes = $11 / $15} 
+			else {frac_bytes = 0}
+			cat0 = $14
+		} else if ($3==1) { 
+			if ($14>0) {frac_packets = $10 / $14}
+			else {frac_packets = 0}
+			if ($13>0) {frac_arrivals = ( $9 / $13 )} 
+			else {frac_arrivals = 0}
+			cat1 = $14
+		}
+		prevtime = $1
+            }
+	    END {
+		cat1_part = frac_packets * cat1 / ( cat1 + cat0 ) 
+		cat0_part = frac_bytes * cat0 / ( cat1 + cat0 ) 
+                print 100.0 * frac_arrivals, 100.0 * ( cat1_part + cat0_part )
+	    }
+        }
+        return $awkCode
+}
+
+proc create_flow_graph { graphtitle graphfile } {
+        global flowfile awkprocedure
+        set tmpfile1 /tmp/fg1[pid]
+        set tmpfile2 /tmp/fg2[pid]
+
+        exec rm -f $graphfile
+        set outdesc [open $graphfile w]
+        #
+        # this next part is xgraph specific
+        #
+        puts $outdesc "TitleText: $graphtitle"
+        puts $outdesc "Device: Postscript"
+
+        exec rm -f $tmpfile1 $tmpfile2
+        puts "writing flow xgraph data to $graphfile..."
+
+        exec sort -n +1 -o $flowfile $flowfile
+        exec awk [$awkprocedure] $flowfile >@ $outdesc
+        close $outdesc
+}
+
+proc finish_flow { file } {
+	global flowgraphfile
+	create_flow_graph $file $flowgraphfile
+	puts "running xgraph..."
+	exec xgraph -bb -tk -nl -m -lx 0,100 -ly 0,100 -x "% of data bytes" -y "% of discards" $flowgraphfile &
+	exit 0
+}
+
+proc tcpDumpAll { tcpSrc interval label } { 
+        proc dump { src interval label } {  
+                ns at [expr [ns now] + $interval] "dump $src $interval $label"
+		puts time=[ns now]/class=$label/ack=[$src get ack]
+        }
+        puts $label:window=[$tcpSrc get window]/packet-size=[$tcpSrc get packet-size]
+        ns at 0.0 "dump $tcpSrc $interval $label"
+}       
+
+
+proc flowDump { link fm flow category } {
+	$fm dump $category
+	$fm resetcounters
+}
+
+proc flowDump1 { link fm flow } {
+	$fm dump 
+	$fm resetcounters
+}
+
 proc test_red1 {} {
 	global s1 s2 r1 r2 s3 s4
 	create_testnet2
         set stoptime 10.0
 	set testname test_red1
+	
+	set tcp1 [ns_create_connection tcp-reno $s1 tcp-sink $s3 0]
+	$tcp1 set window 15
+
+	set tcp2 [ns_create_connection tcp-reno $s2 tcp-sink $s3 1]
+	$tcp2 set window 15
+
+	set ftp1 [$tcp1 source ftp]
+	set ftp2 [$tcp2 source ftp]
+
+	ns at 0.0 "$ftp1 start"
+	ns at 3.0 "$ftp2 start"
+	ns at $stoptime "plotQueue $testname"
+
+	tcpDump $tcp1 5.0
+
+	# trace only the bottleneck link
+	[ns link $r1 $r2] trace [openTrace $stoptime $testname ]
+
+	puts seed=[ns random 0]
+	ns run
+}
+
+# For this test the average queue size in measured in bytes.
+proc test_red1_bytes {} {
+	global s1 s2 r1 r2 s3 s4
+	create_testnet2
+        set stoptime 10.0
+	set testname test_red1_bytes
+        [ns link $r1 $r2] set bytes true
+	[ns link $r1 $r2] set mean_pktsize 1000
+
 	
 	set tcp1 [ns_create_connection tcp-reno $s1 tcp-sink $s3 0]
 	$tcp1 set window 15
@@ -307,12 +501,138 @@ proc test_red_twoway {} {
 	ns run
 }
 
+# The queue is measured in "bytes".
+proc test_red_twowaybytes {} {
+	global s1 s2 r1 r2 s3 s4
+	create_testnet2
+	set stoptime 10.0
+	set testname test_red_twowaybytes
+	[ns link $r1 $r2] set bytes true
+	[ns link $r2 $r1] set bytes true
+		
+	set tcp1 [ns_create_connection tcp-reno $s1 tcp-sink $s3 0]
+	$tcp1 set window 15
+	set tcp2 [ns_create_connection tcp-reno $s2 tcp-sink $s4 1]
+	$tcp2 set window 15
+	set ftp1 [$tcp1 source ftp]
+	set ftp2 [$tcp2 source ftp]
+
+	set tcp3 [ns_create_connection tcp-reno $s3 tcp-sink $s1 2]
+	$tcp3 set window 15
+	set tcp4 [ns_create_connection tcp-reno $s4 tcp-sink $s2 3]
+	$tcp4 set window 15
+	set ftp3 [$tcp3 source ftp]
+	set telnet1 [$tcp4 source telnet] ; $telnet1 set interval 0
+
+	ns at 0.0 "$ftp1 start"
+	ns at 2.0 "$ftp2 start"
+	ns at 3.5 "$ftp3 start"
+	ns at 1.0 "$telnet1 start"
+	ns at $stoptime "plotQueue $testname"
+
+	tcpDump $tcp1 5.0
+
+	# trace only the bottleneck link
+	[ns link $r1 $r2] trace [openTrace $stoptime $testname]
+
+	puts seed=[ns random 0]
+	ns run
+}
+
+proc new_tcp { startTime source dest window class dump size } {
+	set tcp [ns_create_connection tcp-reno $source tcp-sink $dest $class ]
+	$tcp set window $window
+	if {$size > 0}  {$tcp set packet-size $size }
+	set ftp [$tcp source ftp]
+	ns at $startTime "$ftp start"
+        if {$dump == 1 } {tcpDumpAll $tcp 20.0 $class }
+}
+
+proc new_cbr { startTime source dest pktSize interval class } {
+ 	set cbr [ns_create_cbr $source $dest $pktSize $interval $class ]
+	ns at $startTime "$cbr start"
+}
+
+proc flows {} {
+	global s1 s2 r1 r2 s3 s4 r1fm qgraphfile flowfile 
+        set stoptime 500.0
+	set testname test_two
+	
+	create_testnet2
+	[ns link $r1 $r2] set mean_pktsize 1000
+	[ns link $r2 $r1] set mean_pktsize 1000
+	[ns link $r1 $r2] set linterm 10
+	[ns link $r2 $r1] set linterm 10
+	[ns link $r1 $r2] set queue-limit 100
+	[ns link $r2 $r1] set queue-limit 100
+
+	create_flowstats 
+	[ns link $r1 $r2] set bytes true
+	[ns link $r1 $r2] set wait false
+
+        new_tcp 1.0 $s1 $s3 100 1 1 1000
+	new_tcp 1.2 $s2 $s4 100 2 1 50
+	new_cbr 1.4 $s1 $s4 190 0.003 3
+
+	ns at $stoptime "$r1fm flush"
+	ns at $stoptime "finish_flow $testname"
+
+	ns run
+}
+
+proc test_flows {} {
+	global category awkprocedure
+   	set category 1
+	set awkprocedure unforcedmakeawk
+	set seed [ns random 0]
+	puts seed=$seed
+	flows 
+}
+
+proc test_flows1 {} {
+	global category awkprocedure 
+   	set category 0
+	set awkprocedure forcedmakeawk
+	set seed [ns random 0]
+#	ns random $seed
+	puts seed=$seed 
+	flows 
+}
+
+proc test_flowsAll {} {
+	global s1 s2 r1 r2 s3 s4 r1fm qgraphfile flowfile awkprocedure
+        set stoptime 500.0
+	set testname test_two
+	set awkprocedure allmakeawk
+	
+	create_testnet2
+	[ns link $r1 $r2] set mean_pktsize 1000
+	[ns link $r2 $r1] set mean_pktsize 1000
+	[ns link $r1 $r2] set linterm 10
+	[ns link $r2 $r1] set linterm 10
+	[ns link $r1 $r2] set queue-limit 100
+	[ns link $r2 $r1] set queue-limit 100
+
+	create_flowstats1  
+	[ns link $r1 $r2] set bytes true
+	[ns link $r1 $r2] set wait false
+
+        new_tcp 1.0 $s1 $s3 100 1 1 1000
+	new_tcp 1.2 $s2 $s4 100 2 1 50
+	new_cbr 1.4 $s1 $s4 190 0.003 3
+
+	ns at $stoptime "$r1fm flush"
+	ns at $stoptime "finish_flow $testname"
+
+	puts seed=[ns random 0]
+	ns run
+}
+
 if { $argc != 1 } {
-	puts stderr {usage: ns test-suite.tcl [ tahoe1 tahoe2 ... reno reno2 ... ]}
+	puts stderr {usage: ns $argv [ two ]}
 	exit 1
 }
 if { "[info procs test_$argv]" != "test_$argv" } {
-	puts stderr "test-suite.tcl: no such test: $argv"
+	puts stderr "$argv: no such test: $argv"
 }
 test_$argv
-
