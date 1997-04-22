ab36bc88e518e41e38d5a43590b07de0ce1389a4
hari <hari>
1997-04-22 18:25:20 +0000

    Maclist

--- tcl/ex/mac-test.tcl
+++ tcl/ex/mac-test.tcl
@@ -1,14 +1,19 @@
+
 set flags(0) 0
 set stop 100
 set trfile temp
+set tcpfile0 tcpstats.out
 set bw 100Kb
 set delay 10ms
 set ll LL/Base
 set ifq Queue/DropTail
 set mac Mac/Multihop
+set mhflag 1
+
+expr [ns-random 0]
 
 if {$argc == 0} {
-	puts "Usage: $argv0 \[-stop $stop\] \[-tr $trfile\]"
+	puts "Usage: $argv0 \[-stop $stop\] \[-tr $trfile\] \[-stat $tcpf0\]"
 	puts "\t\[-bw $bw] \[-delay $delay\]"
 	puts "\t\[-ll lltype\] \[-ifq ifqtype\] \[-mac mactype\]"
 	exit 1
@@ -30,38 +35,69 @@ for {set i 0} {$i < $argc} {incr i} {
 	} elseif {$opt == "-mac"} {
 		set mac Mac/[lindex $argv [incr i]]
 	} elseif {$opt == "-tr"} {
+		regexp {^(.+)\..*$} $argv0 match ext
 		set trfile [lindex $argv [incr i]]
+	} elseif {$opt == "-stat"} {
+		regexp {^(.+)\..*$} $argv0 match ext
+		set tcpf0 [lindex $argv [incr i]]
 	} elseif {[string range $opt 0 0] == "-"} {
 		set flags([string range $opt 1 1]) 1
 	}
 }
 
+if {!($mac == "Mac/Multihop")} {
+	set mhflag 0
+}
 
 set ns [new Simulator]
 set trfd [open $trfile w]
 $ns trace-all $trfd
+set tcpfd0 [open $tcpfile0 w]
 
-set n(0) [$ns node]
-set n(1) [$ns node]
-set n(2) [$ns node]
-set n(3) [$ns node]
+if {!($mhflag)} {
+	set n(0) [$ns node]
+	set n(1) [$ns node]
+	set n(2) [$ns node]
+	set n(3) [$ns node]
+	set shl [$ns shared-duplex-link "$n(0) $n(1) $n(2) $n(3)" \
+			$bw $delay DropTail $ll $ifq $mac]
+	[[$shl set channel_] drop-target] attach $trfd
 
-set shl [$ns shared-duplex-link "$n(0) $n(1) $n(2) $n(3)" $bw $delay DropTail $ll $ifq $mac]
-[[$shl set channel_] drop-target] attach $trfd
+} else {
+	set n(0) [$ns macnode]
+	set n(1) [$ns macnode]
+	set n(2) [$ns macnode]
+	set n(3) [$ns macnode]
+	# build topology
+	set shl01 [$ns shared-duplex-link "$n(0) $n(1)" $bw $delay DropTail \
+			$ll $ifq $mac]
+	set shl12 [$ns shared-duplex-link "$n(1) $n(2)" $bw $delay DropTail \
+			$ll $ifq $mac]
+	set shl23 [$ns shared-duplex-link "$n(2) $n(3)" $bw $delay DropTail \
+			$ll $ifq $mac]
+	$n(0) linkmacs
+	$n(1) linkmacs
+	$n(2) linkmacs
+	$n(3) linkmacs
+	[[$shl01 set channel_] drop-target] attach $trfd
+}	
 
 
-set tcp(0) [$ns create-connection TCP/Reno $n(1) TCPSink $n(0) 0]
+set tcp(0) [$ns create-connection TCP/Reno $n(1) TCPSink/DelAck $n(0) 0]
 set ftp(0) [$tcp(0) attach-source FTP]
 set tcp(1) [$ns create-connection TCP/Reno $n(0) TCPSink $n(1) 0]
 set ftp(1) [$tcp(1) attach-source FTP]
-set tcp(2) [$ns create-connection TCP/Reno $n(0) TCPSink $n(2) 0]
+set tcp(2) [$ns create-connection TCP/Reno $n(1) TCPSink $n(0) 0]
 set ftp(2) [$tcp(2) attach-source FTP]
-set tcp(3) [$ns create-connection TCP/Reno $n(0) TCPSink $n(3) 0]
+set tcp(3) [$ns create-connection TCP/Reno $n(2) TCPSink $n(3) 0]
 set ftp(3) [$tcp(3) attach-source FTP]
+
+$tcp(0) trace $tcpfd0
+
 # $ns at 0.000 "$ftp(0) start"
-$ns at 0.001 "$ftp(1) start"
-$ns at 0.002 "$ftp(2) start"
-$ns at 0.003 "$ftp(3) start"
+$ns at 0 "$ftp(1) start"
+#$ns at 1.0 "$ftp(2) start"
+#$ns at 0 "$ftp(3) start"
 
 set cbr(2:3) [$ns create-connection CBR $n(2) Null $n(3) 0]
 # $ns at 5.1 "$cbr(2:3) start"
@@ -169,6 +205,9 @@ proc finish {} {
 		eval exec xgraph -nl -M -display $env(DISPLAY) \
 				[lsort [glob $trfile.*]]
 	}
+	
+	exec tail -1 temp.ack.1.0 >> bw.0.1
+	exec gawk { {print $1, $2, $2/$1} } bw.0.1 > /dev/stdout
 	exit 0
 }
 
