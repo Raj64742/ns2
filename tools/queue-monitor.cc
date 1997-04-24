7abb5718d81afe67f29ab57b7b11b7cd4fa23fc4
kfall <kfall>
1997-04-24 03:11:27 +0000

    Addition of a QueueMonitorCompat: 	keeps per-class-id link stats,         used by compatibility library to emulate         ns-1 functionality
    New stats in QueueMonitor
    	counters for drops/arrivals/departs
    	(in packets, bytes)
    	drops now differentiated from departs

--- tools/queue-monitor.cc
+++ tools/queue-monitor.cc
@@ -39,29 +39,54 @@ static char rcsid[] =
 
 #include "queue-monitor.h"
 
-int QueueMonitor::command(int argc, const char*const* argv) {
+int QueueMonitor::command(int argc, const char*const* argv)
+{
         Tcl& tcl = Tcl::instance();
 
         if (argc == 2) {
                 if (strcmp(argv[1], "get-bytes-integrator") == 0) {
-                        tcl.resultf("%s", bytesInt_->name());
+			if (bytesInt_)
+				tcl.resultf("%s", bytesInt_->name());
+			else
+				tcl.resultf("");
                         return (TCL_OK);
                 }
                 if (strcmp(argv[1], "get-pkts-integrator") == 0) {
-                        tcl.resultf("%s", pktsInt_->name());
+			if (pktsInt_)
+				tcl.resultf("%s", pktsInt_->name());
+			else
+				tcl.resultf("");
+                        return (TCL_OK);
+                }
+                if (strcmp(argv[1], "get-delay-samples") == 0) {
+			if (delaySamp_)
+				tcl.resultf("%s", delaySamp_->name());
+			else
+				tcl.resultf("");
                         return (TCL_OK);
                 }
         }
 
         if (argc == 3) {
                 if (strcmp(argv[1], "set-bytes-integrator") == 0) {
-			bytesInt_ = (BytesIntegrator *)
+			bytesInt_ = (Integrator *)
 				TclObject::lookup(argv[2]);
+			if (bytesInt_ == NULL)
+				return (TCL_ERROR);
                         return (TCL_OK);
                 }
                 if (strcmp(argv[1], "set-pkts-integrator") == 0) {
-			pktsInt_ = (PktsIntegrator *)
+			pktsInt_ = (Integrator *)
 				TclObject::lookup(argv[2]);
+			if (pktsInt_ == NULL)
+				return (TCL_ERROR);
+                        return (TCL_OK);
+                }
+                if (strcmp(argv[1], "set-delay-samples") == 0) {
+			delaySamp_ = (Samples*)
+				TclObject::lookup(argv[2]);
+			if (delaySamp_ == NULL)
+				return (TCL_ERROR);
                         return (TCL_OK);
                 }
         }
@@ -75,39 +100,57 @@ static class QueueMonitorClass : public TclClass {
         }
 } queue_monitor_class;
 
-static class BytesIntegratorClass : public TclClass {
- public:
-        BytesIntegratorClass() : TclClass("BytesIntegrator") {}
-        TclObject* create(int argc, const char*const* argv) {
-                return (new BytesIntegrator());
-        }
-} bytes_integrator_class;
-
-static class PktsIntegratorClass : public TclClass {
- public:
-        PktsIntegratorClass() : TclClass("PktsIntegrator") {}
-        TclObject* create(int argc, const char*const* argv) {
-                return (new PktsIntegrator());
-        }
-} pkts_integrator_class;
-
-
+// packet arrival to a queue
 void QueueMonitor::in(Packet* p)
 {
-	size_ += ((hdr_cmn*)p->access(off_cmn_))->size();
-        pkts_++;
+	hdr_cmn* hdr = (hdr_cmn*)p->access(off_cmn_);
         double now = Scheduler::instance().clock();
-        bytesInt_->newPoint(now, double(size_));
-        pktsInt_->newPoint(now, double(pkts_));
+	int pktsz = hdr->size();
+
+	barrivals_ += pktsz;
+	parrivals_++;
+	size_ += pktsz;
+	pkts_++;
+	if (bytesInt_)
+		bytesInt_->newPoint(now, double(size_));
+	if (pktsInt_)
+		pktsInt_->newPoint(now, double(pkts_));
+	if (delaySamp_)
+		hdr->timestamp() = now;
 }
 
 void QueueMonitor::out(Packet* p)
 {
-        size_ -= ((hdr_cmn*)p->access(off_cmn_))->size();
-        pkts_--;
+	hdr_cmn* hdr = (hdr_cmn*)p->access(off_cmn_);
         double now = Scheduler::instance().clock();
-        bytesInt_->newPoint(now, double(size_));
-        pktsInt_->newPoint(now, double(pkts_));
+	int pktsz = hdr->size();
+
+        size_ -= pktsz;
+        pkts_--;
+	bdepartures_ += pktsz;
+	pdepartures_++;
+	if (bytesInt_)
+		bytesInt_->newPoint(now, double(size_));
+	if (pktsInt_)
+		pktsInt_->newPoint(now, double(pkts_));
+	if (delaySamp_)
+		delaySamp_->newPoint(now - hdr->timestamp());
+}
+
+void QueueMonitor::drop(Packet* p)
+{
+	hdr_cmn* hdr = (hdr_cmn*)p->access(off_cmn_);
+        double now = Scheduler::instance().clock();
+	int pktsz = hdr->size();
+
+	size_ -= pktsz;
+	pkts_--;
+	bdrops_ += pktsz;
+	pdrops_++;
+	if (bytesInt_)
+		bytesInt_->newPoint(now, double(size_));
+	if (pktsInt_)
+		pktsInt_->newPoint(now, double(pkts_));
 }
 
 static class SnoopQueueInClass : public TclClass {
@@ -125,3 +168,102 @@ public:
                 return (new SnoopQueueOut());
         }
 } snoopq_out_class;
+
+static class SnoopQueueDropClass : public TclClass {
+public:
+        SnoopQueueDropClass() : TclClass("SnoopQueue/Drop") {}
+        TclObject* create(int argc, const char*const* argv) {
+                return (new SnoopQueueDrop());
+        }
+} snoopq_drop_class;
+
+/*
+ * a 'QueueMonitorCompat', which is used by the compat
+ * code to produce the link statistics used available in ns-1
+ *
+ * in ns-1, the counters are the number of departures
+ */
+
+#include "ip.h"
+QueueMonitorCompat::QueueMonitorCompat()
+{
+	memset(pkts_, '\0', sizeof(pkts_));
+	memset(bytes_, '\0', sizeof(bytes_));
+	memset(drops_, '\0', sizeof(drops_));
+	bind("off_ip_", &off_ip_);
+}
+
+void QueueMonitorCompat::out(Packet* pkt)
+{
+	hdr_ip* iph = (hdr_ip*)pkt->access(off_ip_);
+	int fid = iph->flowid();
+	if (fid < 0 || fid > QM_FIDMAX) {
+		fprintf(stderr, "warning: pkt with fid over qm max %d\n",
+			QM_FIDMAX);
+	} else {
+		bytes_[fid] += ((hdr_cmn*)pkt->access(off_cmn_))->size();
+		pkts_[fid]++;
+	}
+	QueueMonitor::out(pkt);
+}
+
+void QueueMonitorCompat::in(Packet* pkt)
+{
+	//
+	// we're not keeping per-class running queue lengths, so
+	// don't really have to do anything here
+	//
+	QueueMonitor::in(pkt);
+}
+
+void QueueMonitorCompat::drop(Packet* pkt)
+{
+
+	hdr_ip* iph = (hdr_ip*)pkt->access(off_ip_);
+	int fid = iph->flowid();
+	if (fid < 0 || fid > QM_FIDMAX) {
+		fprintf(stderr, "warning: pkt with fid over qm max %d\n",
+			QM_FIDMAX);
+	} else {
+		++drops_[fid];
+	}
+	QueueMonitor::drop(pkt);
+}
+
+int QueueMonitorCompat::command(int argc, const char*const* argv)
+{
+        Tcl& tcl = Tcl::instance();
+	int fid;
+	if (argc == 3) {
+		if (strcmp(argv[1], "bytes") == 0) {
+			if ((fid = atoi(argv[2])) >= 0 && (fid < QM_FIDMAX)) {
+				tcl.resultf("%u", bytes_[fid]);
+				return (TCL_OK);
+			}
+			return (TCL_ERROR);
+		}
+		if (strcmp(argv[1], "pkts") == 0) {
+			if ((fid = atoi(argv[2])) >= 0 && (fid < QM_FIDMAX)) {
+				tcl.resultf("%u", pkts_[fid]);
+				return (TCL_OK);
+			}
+			return (TCL_ERROR);
+		}
+		if (strcmp(argv[1], "drops") == 0) {
+			if ((fid = atoi(argv[2])) >= 0 && (fid < QM_FIDMAX)) {
+				tcl.resultf("%u", drops_[fid]);
+				return (TCL_OK);
+			}
+			return (TCL_ERROR);
+		}
+	}
+	return (QueueMonitor::command(argc, argv));
+}
+
+static class QueueMonitorCompatClass : public TclClass {
+ public: 
+        QueueMonitorCompatClass() : TclClass("QueueMonitor/Compat") {}
+        TclObject* create(int argc, const char*const* argv) { 
+                return (new QueueMonitorCompat);
+        }       
+} queue_monitor_compat_class;
