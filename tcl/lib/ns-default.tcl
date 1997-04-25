c480b0bc53983543cf1722a450717f9d6750e6f6
kfall <kfall>
1997-04-25 02:00:08 +0000

    for RED: 	remove ptc_ 	add queue-in-bytes_, whether to measure 		the queue in bytes

--- tcl/lib/ns-default.tcl
+++ tcl/lib/ns-default.tcl
@@ -30,7 +30,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/lib/ns-default.tcl,v 1.20 1997/04/24 03:23:33 kfall Exp $
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/lib/ns-default.tcl,v 1.21 1997/04/25 02:00:08 kfall Exp $
 
 
 #
@@ -94,10 +94,10 @@ Queue/SFQ set buckets_ 16
 Queue/FQ set secsPerByte_ 0
 
 Queue/RED set bytes_ false
+Queue/RED set queue-in-bytes_ false
 Queue/RED set thresh_ 5
 Queue/RED set maxthresh_ 15
 Queue/RED set mean_pktsize_ 500
-Queue/RED set ptc_ [expr 1.5e6 / (8. * 500)]
 Queue/RED set q_weight_ 0.002
 Queue/RED set wait_ true
 Queue/RED set linterm_ 10
