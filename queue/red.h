db2ce590e920fe5980b3975b090b0538e1d09325
hari <hari>
1997-04-28 02:39:43 +0000

    Header file for red.cc

---
+++ queue/red.h
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) 1990-1997 Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the Computer Systems
+ *	Engineering Group at Lawrence Berkeley Laboratory.
+ * 4. Neither the name of the University nor of the Laboratory may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ *
+ * Here is one set of parameters from one of my simulations:
+ * 
+ * ed [ q_weight=0.002 thresh=5 linterm=30 maxthresh=15
+ *         mean_pktsize=500 dropmech=random-drop queue-size=60
+ *         plot-file=none bytes=false doubleq=false dqthresh=50 
+ *	   wait=true ]
+ * 
+ * 1/"linterm" is the max probability of dropping a packet. 
+ * There are different options that make the code
+ * more messy that it would otherwise be.  For example,
+ * "doubleq" and "dqthresh" are for a queue that gives priority to
+ *   small (control) packets, 
+ * "bytes" indicates whether the queue should be measured in bytes 
+ *   or in packets, 
+ * "dropmech" indicates whether the drop function should be random-drop 
+ *   or drop-tail when/if the queue overflows, and 
+ *   the commented-out Holt-Winters method for computing the average queue 
+ *   size can be ignored.
+ * "wait" indicates whether the gateway should wait between dropping
+ *   packets.
+ */
+
+
+#ifndef ns_red_h
+#define ns_red_h
+
+#ifndef lint
+static char rcsid[] =
+    "@(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/queue/red.h,v 1.1.2.1 1997/04/28 02:39:43 hari Exp $ (LBL)";
+#endif
+
+#include <math.h>
+#include <string.h>
+#include <sys/types.h>
+
+#include "queue.h"
+#include "Tcl.h"
+#include "packet.h"
+#include "random.h"
+#include "flags.h"
+
+/*
+ * Early drop parameters, supplied by user
+ */
+struct edp {
+	/*
+	 * User supplied.
+	 */
+	int mean_pktsize;	/* avg pkt size, linked into Tcl */
+	int bytes;		/* true if queue in bytes, false if packets */
+	int wait;		/* true for waiting between dropped packets */
+	int setbit;		/* true to set congestion indication bit */
+	int fracthresh;         /* true if the thresholds are specified as 
+				   fractions of the max. queue length */
+
+	double th_min;		/* minimum threshold of average queue size */
+	double th_max;		/* maximum threshold of average queue size */
+	double max_p_inv;	/* 1/max_p, for max_p = maximum prob.  */
+	double q_w;		/* queue weight */
+	/*
+	 * Computed as a function of user supplied paramters.
+	 */
+	double ptc;		/* packet time constant in packets/second */
+	/* misc. variables */
+	int adjusted_for_fracthresh_; /* whether we have adjust for threshold
+					 expressed as fractions of the queue size */
+	int adjusted_for_bytes_;      /* whether we have adjusted for byte-counting
+					 instead of packet-counting */
+};
+
+/*
+ * Early drop variables, maintained by RED
+ */
+struct edv {
+	float v_ave;		/* average queue size */
+	float v_slope;		/* used in computing average queue size */
+	float v_r;			
+	float v_prob;		/* prob. of packet drop */
+	float v_prob1;		/* prob. of packet drop before "count". */
+	float v_a;		/* v_prob = v_a * v_ave + v_b */
+	float v_b;
+	int count;		/* # of packets since last drop */
+	int count_bytes;	/* # of bytes since last drop */
+	int old;		/* 0 when average queue first exceeds thresh */
+};
+
+class REDQueue : public Queue {
+ public:	
+	REDQueue();
+ protected:
+	int command(int argc, const char*const* argv);
+	void enque(Packet* pkt);
+	Packet* deque();
+	void reset();
+	void run_estimator(int nqueued, int m);
+	void plot();
+	void plot1(int qlen);
+	int drop_early(Packet* pkt);
+	int bcount_;
+
+	/*
+	 * Static state.
+	 */
+	int drop_tail_;
+	edp edp_;
+	int doubleq_;	/* for experiments with priority for small packets */
+	int dqthresh_;	/* for experiments with priority for small packets */
+
+	/*
+	 * Dynamic state.
+	 */
+	int idle_;
+	double idletime_;
+	edv edv_;
+	void print_edp();
+	void print_edv();
+};
+
+static class REDClass : public TclClass {
+public:
+	REDClass() : TclClass("Queue/RED") {}
+	TclObject* create(int argc, const char*const* argv) {
+		return (new REDQueue);
+	}
+} class_red;
+
+#endif
