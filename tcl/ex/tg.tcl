a299e1cf065157afb788d07faada6de500dfcd6a
breslau <breslau>
1997-05-02 22:18:46 +0000

    Updated tcl scripts demonstrating use of traffic generation modules.

--- tcl/ex/tg.tcl
+++ tcl/ex/tg.tcl
@@ -1,33 +1,47 @@
 ##
 ## simple example to demonstrate use of traffic generation modules
 ##
-## topology consists of 2 nodes connected by a point-to-point link
-## 4 agents source traffic from node n0 destined for null agents
-## at node n1.  agent s1 is an exponential on/off source, s2 is
-## a pareto on/off source, agents s3 and s4 generate traffic
-## based on a trace file.
+## topology consists of 6 nodes.  4 traffic sources are attached
+## to each of 4 'access' nodes.  These nodes have links to a single
+## 'concentrator' node, which is connected by the bottleneck link
+## to the destination node.
+## 
+## there are 4 source agents that generate packets
+## 1 generates traffic based on an Exponential On/Off distribution
+## 1 generates traffic based on a Pareto On/Off distribution
+## 2 generate traffic using the trace file 'example-trace'.
 ##
-
-source tg-finish.tcl
-set stoptime 100.0
-set plottime 101.0
+set stoptime 10.0
+set plottime 11.0
 
 set ns [new Simulator]
 
 set n0 [$ns node]
 set n1 [$ns node]
 
-$ns duplex-link $n0 $n1 1.5Mb 10ms DropTail
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
 
 set f [open out.tr w]
-$ns trace-queue $n0 $n1 $f
+$ns trace-all $f
+
+$ns duplex-link $n0 $n1 1.5Mb 10ms DropTail
+
+$ns duplex-link $n0 $n2 10Mb 5ms DropTail
+$ns duplex-link $n0 $n3 10Mb 5ms DropTail
+$ns duplex-link $n0 $n4 10Mb 5ms DropTail
+$ns duplex-link $n0 $n5 10Mb 5ms DropTail
+
+#$ns trace-queue $n0 $n1 $f
 
 
 ## set up the exponential on/off source, parameterized by packet size,
 ## ave on time, ave off time and peak rate
 
 set s1 [new Agent/CBR/UDP]
-$ns attach-agent $n0 $s1
+$ns attach-agent $n2 $s1
 
 set null1 [new Agent/Null]
 $ns attach-agent $n1 $null1
@@ -38,7 +52,7 @@ set exp1 [new Traffic/Expoo]
 $exp1 set packet-size 210
 $exp1 set burst-time 500ms
 $exp1 set idle-time 500ms
-$exp1 set rate 64k
+$exp1 set rate 100k
 
 $s1 attach-traffic $exp1
 
@@ -46,7 +60,7 @@ $s1 attach-traffic $exp1
 ## ave on time, ave  off time, peak rate and pareto shape parameter
 
 set s2 [new Agent/CBR/UDP]
-$ns attach-agent $n0 $s2
+$ns attach-agent $n3 $s2
 
 set null2 [new Agent/Null]
 $ns attach-agent $n1 $null2
@@ -57,7 +71,7 @@ set pareto2 [new Traffic/Pareto]
 $pareto2 set packet-size 210
 $pareto2 set burst-time 500ms
 $pareto2 set idle-time 500ms
-$pareto2 set rate 64k
+$pareto2 set rate 200k
 $pareto2 set shape 1.5
 
 $s2 attach-traffic $pareto2
@@ -70,7 +84,7 @@ $tfile filename example-trace
 ## set up a source that uses the trace file
 
 set s3 [new Agent/CBR/UDP]
-$ns attach-agent $n0 $s3
+$ns attach-agent $n4 $s3
 
 set null3 [new Agent/Null]
 $ns attach-agent $n1 $null3
@@ -90,7 +104,7 @@ $s3 attach-traffic $trace3
 ## attach a 2nd source to the same trace file
 
 set s4 [new Agent/CBR/UDP]
-$ns attach-agent $n0 $s4
+$ns attach-agent $n5 $s4
 
 set null4 [new Agent/Null]
 $ns attach-agent $n1 $null4
@@ -115,11 +129,71 @@ $ns at $stoptime "$s4 stop"
 $ns at $plottime "close $f"
 $ns at $plottime "finish tg"
 
+proc finish file {
+ 
+        set f [open temp.rands w]
+        puts $f "TitleText: $file"
+        puts $f "Device: Postscript"
+
+        exec rm -f temp.p1 temp.p2 temp.p3 temp.p4
+        exec touch temp.p1 temp.p2 temp.p3 temp.p4
+        #
+        #
+	exec awk {
+		{
+			if (($1 == "+") && ($3 != 0) && ($9 == 2.0)) \
+			     print $2, 2
+		}
+	} out.tr > temp.p1
+	exec awk {
+		{
+			if (($1 == "+") && ($3 != 0) && ($9 == 3.0)) \
+			     print $2, 3
+		}
+	} out.tr > temp.p2
+	exec awk {
+		{
+			if (($1 == "+") && ($3 != 0) && ($9 == 4.0)) \
+			     print $2, 4
+		}
+	} out.tr > temp.p3
+	exec awk {
+		{
+			if (($1 == "+") && ($3 != 0) && ($9 == 5.0)) \
+			     print $2, 5
+		}
+	} out.tr > temp.p4
+
+	puts $f [format "\n\"ExpOnOff"]
+	flush $f
+	exec cat temp.p1 >@ $f
+	flush $f
+	puts $f [format "\n\"ParetoOnOff"]
+	flush $f
+	exec cat temp.p2 >@ $f
+	flush $f
+	puts $f [format "\n\"Trace1"]
+	flush $f
+	exec cat temp.p3 >@ $f
+	flush $f
+	puts $f [format "\n\"Trace2"]
+	flush $f
+	exec cat temp.p4 >@ $f
+	flush $f
+
+        close $f
+	exec rm -f temp.p1 temp.p2 temp.p3 temp.p4
+        exec xgraph -bb -tk -nl -m -x time -y "source node" temp.rands &
+
+	exec awk -f ../nam-demo/nstonam.awk out.tr > tg-nam.tr
+	puts "running nam..."
+	exec nam tg-nam &
+
+	exec rm -f out.tr
+
+}
+
 $ns run
 
-## run nam?
-## exec awk -f ../nam-demo/nstonam.awk out.tr > tg-nam.tr
-## exec rm -f out
-## puts "running nam..."
-## exec nam tg-nam &
-## exit 0
+
+exit 0
