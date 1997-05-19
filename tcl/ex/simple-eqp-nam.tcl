7c79c3a21e4aa9fb6b5f672e0845f5bd04321ad5
kannan <kannan>
1997-05-19 23:00:01 +0000

    Hopefully, the permissions problems are now fixed.

---
+++ tcl/ex/simple-eqp-nam.tcl
@@ -0,0 +1,42 @@
+proc nam_config {net} {
+    $net node 0 circle
+    $net node 1 circle
+    $net node 2 other
+    $net node 3 other
+    $net node 4 box
+    $net node 5 other
+
+    mklink $net 0 2  10Mb  2ms right-down
+    mklink $net 1 2  10Mb  2ms right-up
+
+    mklink $net 2 3 1.5Mb 10ms right-up
+    mklink $net 2 5 1.5Mb 10ms right-down
+    mklink $net 3 4 1.5Mb 10ms right-down
+    mklink $net 5 4 1.5Mb 10ms right-up
+
+    $net color 0 blue
+    $net color 1 red
+    $net color 2 white
+
+    $net queue 2 3 0
+    $net queue 2 5 0
+
+    # chart utilization from 1 to 2 width 5sec
+    # chart avgutilization from 1 to 2 width 5sec
+}
+
+proc link-up {src dst} {
+    ecolor $src $dst green
+}
+
+proc link-down {src dst} {
+    ecolor $src $dst red
+}
+
+proc node-up src {
+    ncolor $src green
+}
+
+proc node-down src {
+    ncolor $src red
+}
