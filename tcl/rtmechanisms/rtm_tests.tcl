7d1096b7fa66cc989626d25cb97bb95f7e62d593
kfall <kfall>
1997-06-20 01:58:18 +0000

    move init proc to this file

--- tcl/rtmechanisms/rtm_tests.tcl
+++ tcl/rtmechanisms/rtm_tests.tcl
@@ -2,6 +2,39 @@
 # here are the main tests for detecting
 #	unfriendly, unresponsive and high-bw flows
 #
+
+RTMechanisms instproc init { ns cbqlink rtt mtu } { 
+        $self instvar Safety_factor_
+        $self instvar Max_cbw_  
+        $self instvar Maxallot_
+        $self instvar Mintime_
+        $self instvar npenalty_     
+	$self instvar cbqlink_
+	$self instvar last_reward_ last_detect_
+	$self instvar Reward_interval_
+	$self instvar Detect_interval_
+	$self instvar ns_
+
+	set cbqlink_ $cbqlink
+	set Rtt_ $rtt
+	set Mtu_ $mtu
+	set ns_ $ns
+	set Reward_interval_  5.0
+	set Detect_interval_  5.0
+
+        set Safety_factor_ 1.2
+        set Max_cbw_ 46750
+        set Maxallot_ 0.98          
+        set Mintime_ 0          
+        set npenalty_ 0 
+	set last_reward_ 0.0
+	set last_detect_ 0.0
+        set High_const_ 12000   
+
+	$ns_ at $Reward_interval_ "$self do_reward"
+	$ns_ at $Detect_interval_ "$self do_detect"
+}      
+
 RTMechanisms instproc test_friendly { flow_bw ref_bw } {
         $self instvar Safety_factor_
         if { $flow_bw  > ($Safety_factor_ * $ref_bw) } {
