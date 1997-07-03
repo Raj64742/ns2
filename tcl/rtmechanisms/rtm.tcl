557240aa9b5293122c3ca27a1e996e8575b91e4f
kfall <kfall>
1997-07-03 03:24:37 +0000

    trace acks also, add another source

--- tcl/rtmechanisms/rtm.tcl
+++ tcl/rtmechanisms/rtm.tcl
@@ -18,7 +18,7 @@ source mechanisms.tcl
 # - 10Mb/s, 3ms, drop-tail
 # = 1.5Mb/s, 20ms, CBQ
 #
-proc create_topology tf {
+proc create_topology { tf af }  {
 	global ns
 	global s1 s2 s3 s4
 	global r1 r2
@@ -36,12 +36,14 @@ proc create_topology tf {
 	set cl [new Classifier/Hash/SrcDestFid 33]
 	$ns simplex-link $r1 $r2 1.5Mb 20ms "CBQ $cl"
 	set cbqlink [$ns link $r1 $r2]
-	$ns simplex-link $r2 $r1 1.5Mb 20ms RED
+	[$cbqlink queue] algorithm "formal"
+	$ns simplex-link $r2 $r1 1.5Mb 20ms DropTail
 	[[$ns link $r2 $r1] queue] set limit_ 25
 	$ns duplex-link $s3 $r2 10Mb 4ms DropTail
 	$ns duplex-link $s4 $r2 10Mb 5ms DropTail
 
-#	$ns trace-queue $n2 $n3 $tf
+	$cbqlink trace $ns $tf	; # trace pkts
+	[$ns link $r2 $r1] trace $ns $af ; # trace acks
 	return $cbqlink
 }
 
@@ -80,7 +82,6 @@ proc finish { tf ff } {
 	close $tf
 	close $ff
 	puts "simulation complete"
-	exit 0
 }
 
 proc sim1 {} {
@@ -91,10 +92,13 @@ proc sim1 {} {
 
 	puts "starting sim1"
 
-	set start1 1.0
-	set start2 1.2
-	set start3 1.4
-	set endsim 300.0
+	set start1 1.1
+	set start2 2.1
+	set start3 1.3
+	set start4 1.6
+	set pause4 12.0
+	set again4 18.0
+	set endsim 30.0
 
 	set rtt 0.06
 	set mtu 512
@@ -102,7 +106,8 @@ proc sim1 {} {
 	set ns [new Simulator]
 
 	set tracef [open out.tr w]
-	set cbqlink [create_topology $tracef]
+	set ackf [open ack.tr w]
+	set cbqlink [create_topology $tracef $ackf]
 
 	set rtm [new RTMechanisms $ns $cbqlink $rtt $mtu]
 
@@ -120,15 +125,23 @@ proc sim1 {} {
 	$rtm bindboxes
 
 	set src1 [create_tcp_source $s1 $s3 1 100 1000]
-	set src2 [create_tcp_source $s2 $s4 2 100 50]
-	set src3 [create_source $s1 $s4 3]
+	set src2 [create_tcp_source $s2 $s4 100 100 50]
+	set src3 [create_source $s1 $s4 200]
+		# 500Kb/s CBR
 		$src3 set packetSize_ 190
 		$src3 set interval_ 0.003
+	set src4 [create_source $s1 $s4 300]
+		# 434Kb/s CBR
+		$src3 set packetSize_ 190
+		$src3 set interval_ 0.0035
 
 	$ns at $start1 "$src1 start"
 	$ns at $start2 "$src2 start"
 	$ns at $start3 "$src3 start"
-	$ns at $endsim "finish $gflowf $bflowf"
+	$ns at $start4 "$src4 start"
+	$ns at $pause4 "$src4 stop"
+	$ns at $again4 "$src4 start"
+	$ns at $endsim "finish $gflowf $bflowf; $rtm plot out.tr out.xgr"
 	$ns run
 }
 sim1
