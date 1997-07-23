4c2739b1abebbd8fd47238d6108cc070da0a571f
kfall <kfall>
1997-07-23 02:55:33 +0000

    added rcs info

--- tcp/tcp-full.h
+++ tcp/tcp-full.h
@@ -30,6 +30,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
+ * @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcp/tcp-full.h,v 1.2 1997/07/23 02:55:33 kfall Exp $ (LBL)
  */
 
 #ifndef ns_tcp_full_h
