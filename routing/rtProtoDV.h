e8fd1e653b56c8bce69ee51ee9fd8aeef37fa1f3
kfall <kfall>
1997-07-23 02:49:45 +0000

    add copyright/rcs info add #ifdef around contents

--- routing/rtProtoDV.h
+++ routing/rtProtoDV.h
@@ -1,3 +1,27 @@
+/*
+ * rtProtoDV.h
+ * Copyright (C) 1997 by USC/ISI
+ * All rights reserved.                                            
+ *                                                                
+ * Redistribution and use in source and binary forms are permitted
+ * provided that the above copyright notice and this paragraph are
+ * duplicated in all such forms and that any documentation, advertising
+ * materials, and other materials related to such distribution and use
+ * acknowledge that the software was developed by the University of
+ * Southern California, Information Sciences Institute.  The name of the
+ * University may not be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+ * 
+ * @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/routing/rtProtoDV.h,v 1.2 1997/07/23 02:49:45 kfall Exp $ (USC/ISI)
+ */
+
+#ifndef ns_rtprotodv_h
+#define ns_rtprotodv_h
+
 #include "packet.h"
 #include "ip.h"
 
@@ -19,3 +43,5 @@ public:
 protected:
 	int off_DV_;
 };
+
+#endif
