62155ffa2e084404737fa9c7b16a74cea8e9f874
ahelmy <ahelmy>
1997-08-04 21:56:34 +0000

    [AH] updated timer.tcl to be more compatible with C++ timer API [added sched and expire functions]. The sched API is still different, in OTcl it is the same as reshed, in C++ it aborts if the timer is already scheduled.

--- tcl/ex/timer.tcl
+++ tcl/ex/timer.tcl
@@ -8,6 +8,11 @@
 # 
 Class Timer
 
+# sched is the same as resched; the previous setting is cancelled
+# and another event is scheduled. No state is kept for the timers.
+# This is different than the C++ timer API in timer-handler.cc,h; where a 
+# sched aborts if the timer is already set. C++ timers maintain state 
+# (e.g. IDLE, PENDING..etc) that is checked before the timer is scheduled.
 Timer instproc sched delay {
 	global ns
 	$self instvar id_
@@ -26,4 +31,14 @@ Timer instproc cancel {} {
 		$ns cancel $id_
 		unset id_
 	}
+}
+
+# resched and expire are added to have a similar API to C++ timers.
+Timer instproc resched delay {} {
+	$self sched $delay 
+}
+
+# the subclass must provide the timeout function
+Timer instproc expire {} {
+	$self timeout
 }
