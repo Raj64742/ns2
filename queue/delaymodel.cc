6ead4a06910391780b9f15356a78cb7c3c627e95
polly <polly>
1997-08-08 02:59:00 +0000

    Added delay models for session level packet distribution. Modified structure of 'virtual links'. Added helper functions to monitor packets in transit.

---
+++ queue/delaymodel.cc
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 1997 Regents of the University of California.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ *	This product includes software developed by the Daedalus Research
+ *	Group at the University of California Berkeley.
+ * 4. Neither the name of the University nor of the Laboratory may be used
+ *    to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Contributed by Giao Nguyen, http://daedalus.cs.berkeley.edu/~gnguyen
+ */
+
+#ifndef lint
+static const char rcsid[] =
+    "@(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/queue/delaymodel.cc,v 1.1 1997/08/08 02:59:00 polly Exp $ (UCB)";
+#endif
+
+#include "packet.h"
+#include "delaymodel.h"
+
+static class DelayModelClass : public TclClass {
+public:
+	DelayModelClass() : TclClass("DelayModel") {}
+	TclObject* create(int argc, const char*const* argv) {
+		return (new DelayModel);
+	}
+} class_delaymodel;
+
+DelayModel::DelayModel() : Connector(), bandwidth_(0)
+{
+}
+
+
+int DelayModel::command(int argc, const char*const* argv)
+{
+	Tcl& tcl = Tcl::instance();
+	if (argc == 3) {
+		if (strcmp(argv[1], "ranvar") == 0) {
+			ranvar_ = (RandomVariable*) TclObject::lookup(argv[2]);
+			return (TCL_OK);
+		} else if (strcmp(argv[1], "bandwidth") == 0) {
+			bandwidth_ = atof(argv[2]);
+			return (TCL_OK);
+		} 
+	} else if (argc == 2) {
+		if (strcmp(argv[1], "ranvar") == 0) {
+			tcl.resultf("%s", ranvar_->name());
+			return (TCL_OK);
+		}
+	}
+	return Connector::command(argc, argv);
+}
+
+void DelayModel::recv(Packet* p, Handler*)
+{
+        double delay = ranvar_->value();
+	static int tmp = 0;
+
+	double txt = txtime(p);
+	Scheduler& s = Scheduler::instance();
+	s.schedule(target_, p, txt + delay);
+}
+
+
---
+++ tcl/ex/newmcast/pktintran.tcl
@@ -0,0 +1,70 @@
+set ns [new Simulator]
+Simulator set EnableMcast_ 1
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+set n3 [$ns node]
+set n4 [$ns node]
+set n5 [$ns node]
+
+set f [open out-mc2.tr w]
+$ns trace-all $f
+
+Simulator set NumberInterfaces_ 1
+$ns duplex-link $n0 $n1 1.5Mb 10ms DropTail
+$ns duplex-link $n0 $n2 1.5Mb 10ms DropTail
+$ns duplex-link $n1 $n3 1.5Mb 10ms DropTail
+$ns duplex-link $n1 $n4 1.5Mb 10ms DropTail
+$ns duplex-link $n2 $n4 1.5Mb 10ms DropTail
+$ns duplex-link $n2 $n5 1.5Mb 10ms DropTail
+
+### Start multicast configuration
+DM set PruneTimeout 0.3
+dynamicDM set ReportRouteTimeout 0.15
+
+set mproto CtrMcast
+set mrthandle [$ns mrtproto $mproto  {}]
+### End of multicast  config
+
+set cbr0 [new Agent/CBR]
+$ns attach-agent $n0 $cbr0
+$cbr0 set dst_ 0x8002
+ 
+set rcvr [new Agent/LossMonitor]
+$ns attach-agent $n3 $rcvr
+$ns attach-agent $n4 $rcvr
+$ns attach-agent $n5 $rcvr
+
+$ns at 0.2 "$n3 join-group $rcvr 0x8002"
+$ns at 0.4 "$n4 join-group $rcvr 0x8002"
+$ns at 0.6 "$n3 leave-group $rcvr 0x8002"
+$ns at 0.7 "$n5 join-group $rcvr 0x8002"
+$ns at 0.8 "$n3 join-group $rcvr 0x8002"
+
+#### Link between n0 & n1 down at 1.0, up at 1.2
+$ns rtmodel-at 1.0 down $n0 $n1
+$ns rtmodel-at 1.2 up $n0 $n1
+####
+set mmonitor [$ns McastMonitor]
+$ns at 0.0 "$mmonitor trace-topo 0 [expr 0x8002]"
+
+$ns at 0.1 "$cbr0 start"
+ 
+$ns at 1.6 "finish"
+
+proc finish {} {
+        #puts "converting output to nam format..."
+        global ns
+        $ns flush-trace
+        global rcvr
+        # puts "lost [$rcvr set nlost_] pkt, rcv [$rcvr set npkts_]"
+        exec awk -f ../../nam-demo/nstonam.awk out-mc2.tr > mcast2-nam.tr
+        exec rm -f out
+        #XXX
+        #puts "running nam..."
+        exec ./nam mcast2-nam &
+        exit 0
+}
+
+$ns run Session
