60aa4d01961b2d8ed0478b73fd028c1bb71cd39e
ahelmy <ahelmy>
1997-08-10 08:57:38 +0000

    [AH] updates to join source-group pairs, leave groups ..etc.

--- tcl/pim/pim-vifs.tcl
+++ tcl/pim/pim-vifs.tcl
@@ -22,7 +22,18 @@
  # 
  #
 PIM instproc leave-group { group } {
-	# essential while no timers 
+        # essential while no timers
+        $self instvar MRTArray Node
+        if [info exists MRTArray($group)] {
+                $self del-oif $MRTArray($group) [$Node id] "WC"
+                return 1
+        }
+        #puts "node [$Node id] leave group and no WC Ent.. !!! ERROR"
+        return 0
+}
+
+PIM instproc leave-group-source { group source } {
+        # XXX TBDone
 }
 
 PIM instproc join-group { group } {
@@ -31,10 +42,32 @@ PIM instproc join-group { group } {
 	$self add_leaf $group
 }
 
+PIM instproc join-group-source { group source } {
+        # like accept_group_report
+        # puts "$self join-group $group"
+        $self add_source_leaf $group $source
+}
+
+PIM instproc add_source_leaf { group srcID } {
+        $self instvar MRTArray
+        set notfound 1
+        if [info exists MRTArray($srcID:$group)] {
+                set notfound 0
+        } elseif { ! [$self findSG $srcID $group 1 [PIM set SG]] } {
+                return 0
+        }
+        if $notfound {
+           $self send-join "SG" $srcID $group
+           set iif [$self get-iif-label [$MRTArray($srcID:$group) getNextHop]]
+           $MRTArray($srcID:$group) setiif $iif
+        }
+}
+
 # typically add_leaf is called with the vif number too... if we have igmp
 PIM instproc add_leaf { group } {
 	$self instvar Node MRTArray
 	# puts "node [$Node id] $self addleaf $group"
+  # XXX move the WKG proc into arbiter.. XXX
 	if { $group == [PIM set ALL_PIM_ROUTERS] } {
 		# puts "group is [PIM set ALL_PIM_ROUTERS]"
 		return 0
@@ -45,6 +78,11 @@ PIM instproc add_leaf { group } {
 	} elseif { ! [$self findWC $group 1] } { return 0 }
 	if $notfound {
 		$self send-join "WC" 0 $group
+                set iif [$self get-iif-label [$MRTArray($group) getNextHop]]
+                $MRTArray($group) setiif $iif
+        } elseif { [$self compute-oifs $MRTArray($group)] == "" &&
+                ! [$Node check-local $group] } {
+                $self send-join "WC" 0 $group
 	}
 	$self check-sources $group $notfound
 }
@@ -157,30 +195,99 @@ PIM instproc add-oif { ent origin code } {
 			}
 		}
 		if { $modify && [$entry getflags] & [PIM set CACHE] } {
-			set srcAdd [expr $source << 8]
-			$Node add-mfc $srcAdd $group -1 $newOif
+			$Node add-mfc $source $group -1 $newOif
 		}
 	    }
 	  }
 	} elseif { [$ent info class == "SGEnt"] } {
 	     if { [$ent getflags] & [PIM set CACHE] } {
+                # XXX fix cache Ent.. !! not defined.. ! later
 		if { [$cacheEnt is-pruned $index] } {
                         $cacheEnt remove-prune $index
 		}
 
 		$ent add-oif $index $newOif
-		set srcAdd [expr $source << 8]
-		$Node add-mfc $srcAdd $group -1 $newOif
+		$Node add-mfc $source $group -1 $newOif
 	     }
 	}
 
 }	
 
-PIM instproc del-oif { ent origin code } { 
-	$self instvar Node ns
-	# puts "node [$Node id] pruning oif $origin"
-	set oifInfo [$Node get-oif [$ns set link_([$Node id]:$origin)]]
-	$ent del-oif [lindex $oifInfo 0] [lindex $oifInfo 1]
+PIM instproc del-oif { ent origin code } {
+        $self instvar Node ns groupArray MRTArray 
+        # puts "node [$Node id] pruning oif $origin"
+        if { $origin != [$Node id] } {
+          set oifInfo [$Node get-oif [$ns set link_([$Node id]:$origin)]]
+          set index [lindex $oifInfo 0]
+          set oif [lindex $oifInfo 1]
+        } else {
+          set index -1
+          set oif "local"
+        }
+       set group [$ent get-group]
+
+        # XXX chk all S,Gs under *,G and mod as necessary..
+        # especially check the S,G caches...
+        if { [$ent info class] == "WCEnt" } {
+          if [info exists groupArray($group)] {
+            # XXX do we need to check group,src pairs
+            set locals [$Node check-local $group]   
+            foreach source $groupArray($group) {
+                set entry $MRTArray($source:$group)
+                if { ! [expr [$entry getflags] & [PIM set CACHE]] } {
+                        continue
+                }
+                set oiflist [$self compute-oifs $entry]
+                if { [set idx [$self index $oiflist $oif]] != -1
+                        || $oif == "local" } {
+                        if { $oif != "local" } {
+                          $Node del-mfc $source $group $oif
+                        }
+                        if { [llength $oiflist] <= 1 && ! $locals } {
+                                $self send-prune "SG_RP" $source $group
+                        }
+                }
+            }
+          }
+          $ent rem-oif $index $oif
+          return 1
+          # XXX may need more clean up for cache lists... etc XXX
+        } elseif { [$ent info class] == "SGEnt" } {
+                $ent del-oif $index $oif
+                if { [expr [$ent getflags] & [PIM set CACHE]] } {
+                        set source [$ent set source]
+                        $Node del-mfc $source $group $oif
+                }
+        }
+}
+
+PIM instproc index { oifs iface } {
+        set k [lsearch -exact $oifs $iface]
+        return $k
+}
+
+PIM instproc compute-oif-ids { ent } {
+        set oiflist [$self compute-oifs $ent]
+        return [$self oifs2labels $oiflist]
+}
+
+PIM instproc oifs2labels { oiflist } {  
+        set labelList ""  
+        foreach oif $oiflist {
+                lappend labelList [$oif set intf_label_]
+        }
+        return $labelList
+}
+ 
+PIM instproc get-iif-label { nxthop } {
+        $self instvar Node ns
+        if { $nxthop == [$Node id] } {
+                return -1
+        }
+        set oifInfo [$Node get-oif [$ns set link_([$Node id]:$nxthop)]]
+        set oif [lindex $oifInfo 1]   
+        #puts "oif is $oif [$oif info class]" 
+        return [$oif set intf_label_]
 }
 
 PIM instproc mergeoifs { list1 list2 } {
@@ -233,3 +340,15 @@ PIM instproc nexthopWC { group } {
 	$ent setNextHop $nxt
 	return $nxt
 }
+
+PIM instproc nexthopSG { source group } {
+        $self instvar MRTArray
+        set ent $MRTArray($source:$group)
+        if { [set nxt [$ent getNextHop]] != -1 } {
+                return $nxt  
+        }
+        set nxt [$self nexthop $source]
+        $ent setNextHop $nxt
+        return $nxt
+}
+
