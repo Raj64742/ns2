2f51517b3c7706ad5fb41c1e71cb4fdee4320960
sfloyd <sfloyd>
1997-10-18 12:27:57 +0000

    Change in calling routine for ns.

--- tcl/rtmechanisms/Collapseall.tcl
+++ tcl/rtmechanisms/Collapseall.tcl
@@ -1,5 +1,5 @@
 #
-# To run this:  "./ns Collapseall.tcl" 
+# To run this:  "../../ns Collapseall.tcl" 
 #
 source Fairnessall1.tcl
 set datafile Collapse.data
@@ -11,21 +11,21 @@ exec rm -f $datafile
 # for interval=0.0008, CBR arrival rate is 1 Mbps
 # intervals 0.0001 to 0.0009
 for {set i 4} {$i <= 9} {incr i 1} {
-    puts "./ns Collapse.tcl simple 0.000$i"
+    puts "../../ns Collapse.tcl simple 0.000$i"
 # need catch for those "warning: no class variable Queue/RED::fracminthresh_"
-    catch {exec ./ns Collapse.tcl simple 0.000$i} result
+    catch {exec ../../ns Collapse.tcl simple 0.000$i} result
     append $infile $datafile
 }
 # intervals 0.001 to 0.009
 for {set i 1} {$i <= 9} {incr i 1} {
-    puts "./ns Collapse.tcl simple 0.00$i"
-    catch {exec ./ns Collapse.tcl simple 0.00$i} result
+    puts "../../ns Collapse.tcl simple 0.00$i"
+    catch {exec ../../ns Collapse.tcl simple 0.00$i} result
     append $infile $datafile
 }
 # intervals 0.01 to 0.09
 for {set i 1} {$i <= 9} {incr i 1} {
-    puts "./ns Collapse.tcl simple 0.0$i"
-    catch {exec ./ns Collapse.tcl simple 0.0$i} result
+    puts "../../ns Collapse.tcl simple 0.0$i"
+    catch {exec ../../ns Collapse.tcl simple 0.0$i} result
     append $infile $datafile
 }
 finish
