e1104a0df53bb7f6a1e829fcb49f1d6d63a9ac9d
sfloyd <sfloyd>
1997-11-05 05:19:17 +0000

    Modified to use attach-fmon and makeflowmon from ns.

--- tcl/rtmechanisms/Flows.v2.tcl
+++ tcl/rtmechanisms/Flows.v2.tcl
@@ -173,10 +173,10 @@ proc create_flowstats { link dump stoptime } {
     
     global ns r1 r2 r1fm flowfile drop_interval flowdesc
 
-    set r1fm [makeflowmon]
+    set r1fm [$ns makeflowmon Fid]
     set flowdesc [open $flowfile w]
     $r1fm attach $flowdesc
-    attach-fmon $link $r1fm
+    $ns attach-fmon $link $r1fm 1
     $ns at $drop_interval "flowmonDump $r1fm $dump $link $stoptime"
 }
 
@@ -184,10 +184,10 @@ proc create_flowstats1 { link dump stoptime } {
 
     global ns r1 r2 r1fm flowfile drop_interval flowdesc
     
-    set r1fm [makeflowmon]
+    set r1fm [$ns makeflowmon Fid]
     set flowdesc [open $flowfile w]
     $r1fm attach $flowdesc
-    attach-fmon $link $r1fm
+    $ns attach-fmon $link $r1fm 1
     $ns at $drop_interval "flowmonDump $r1fm $dump $link $stoptime"
 }
 
