a4aab4dd4d2863d7eb92cd784d83366435bcde4e
kfall <kfall>
1998-01-08 01:16:10 +0000

    new file for config of emulation stuff

---
+++ tcl/emulate/ns-emulate.tcl
@@ -0,0 +1,8 @@
+#
+# ns-emulate.tcl
+#	configuration for ns emulation
+#
+Network/Pcap/Live set snaplen_ 1000
+Network/Pcap/Live set promisc_ false
+Network/Pcap/Live set livetimeout_ 0
+Network/Pcap/Live set optimize_ true
