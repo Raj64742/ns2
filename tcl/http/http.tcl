4bbdd6ab757993ad41d2cd80661d5c59d13c2e51
gnguyen <gnguyen>
1998-01-22 23:41:34 +0000

    Bug fixes

--- tcl/http/http.tcl
+++ tcl/http/http.tcl
@@ -1,9 +1,46 @@
+#
+# Copyright (c) 1997 Regents of the University of California.
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+# 	This product includes software developed by the MASH Research
+# 	Group at the University of California Berkeley.
+# 4. Neither the name of the University nor of the Research Group may be
+#    used to endorse or promote products derived from this software without
+#    specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+# Code contributed by Giao Nguyen, http://daedalus.cs.berkeley.edu/~gnguyen
+#
+
 Class Http
 
 Http set srcType_ TcpFull	# type of source agent
 Http set snkType_ ""		# type of sink agent: "" means same as srcType_
 Http set phttp_ 0		# persistent http
 Http set maxConnections_ 4	# maximum number of concurrent connections
+
+# The following rv* parameters can take a value or a RandomVariable object
 Http set rvThinkTime_ 3.0	# value for think time in seconds
 Http set rvReqLen_ 256		# length of request in byte
 Http set rvRepLen_ 4000		# length of reply in byte
@@ -108,10 +145,10 @@ Http instproc doneReply {id} {
 
 	if {$id == 0} {
 		set numReq_ [value $rvNumImg_]
-		for {set i 1} {$i <= $maxConnections_} {incr i} {
-			if {$i > numReq_} break
-			$client_($i) sendByte [$self value $rvRequestLen_]
-			decr numReq_
+		for {set i 0} {$numReq_ > 0} {decr numReq_} {
+			set id [expr 1 + ($i % $maxConnections_)]
+			if {[incr i] > $maxConnections_} break
+			$client_($id) sendByte [$self value $rvRequestLen_]
 		}
 		return
 	}
@@ -119,7 +156,7 @@ Http instproc doneReply {id} {
 	if {$numReq_ <= 0} {
 		$self donePage
 	} else {
-		$client_($i) sendByte [$self value $rvRequestLen_]
+		$client_($id) sendByte [$self value $rvRequestLen_]
 		decr numReq_
 	}
 }
