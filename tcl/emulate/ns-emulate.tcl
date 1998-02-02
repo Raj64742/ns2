ed322aa59e4276242a31d55204608229a06525b9
kfall <kfall>
1998-02-02 22:59:48 +0000

    bump snaplen

--- tcl/emulate/ns-emulate.tcl
+++ tcl/emulate/ns-emulate.tcl
@@ -2,7 +2,7 @@
 # ns-emulate.tcl
 #	configuration for ns emulation
 #
-Network/Pcap/Live set snaplen_ 1000
+Network/Pcap/Live set snaplen_ 4096
 Network/Pcap/Live set promisc_ false
 Network/Pcap/Live set timeout_ 0
 Network/Pcap/Live set optimize_ true
