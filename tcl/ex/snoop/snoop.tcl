715b0eff9b92190de42aa7e3ab6ea422bb2f3076
hari <hari>
1998-02-16 22:21:34 +0000

    Lots of changes and addns.

--- tcl/ex/snoop/snoop.tcl
+++ tcl/ex/snoop/snoop.tcl
@@ -12,9 +12,9 @@ if [file executable ../../../ns] {
 # Test script for the snoop protocol (and other link protocols).
 
 # Example ways of running this script are:
-# ns snoop.tcl -e 0.2 -eu time -stop 20 -ll Snoop -r
-# ns snoop.tcl -e 32768 -eu byte -stop 20 -ll Snoop -r
-# ns snoop.tcl -e 100 -eu pkt -stop 20 -ll Snoop -r
+# ns snoop.tcl -e 0.2 -eu time -stop 20 -ll LLSnoop -r
+# ns snoop.tcl -e 32768 -eu byte -stop 20 -ll LLSnoop -r
+# ns snoop.tcl -e 100 -eu pkt -stop 20 -ll LLSnoop -r
 
 # These are for the time-, byte- and packet-based error models.  
 # In the first case, 0.2 is the mean number for seconds between errors,
@@ -58,8 +58,13 @@ if [file executable ../../../ns] {
 
 source ${nshome}tcl/lan/ns-mac.tcl
 source ${nshome}tcl/lan/ns-lan.tcl
+source ${nshome}tcl/lan/ns-ll.tcl
 source ${nshome}tcl/lib/ns-errmodel.tcl
 source ${nshome}tcl/ex/snoop/util.tcl
+source ~/src/ns-2/tcl/lib/ns-trace.tcl
+#source ${nshome}tcl/ex/www/http.tcl
+source $nshome/tcl/http/http.tcl
+
 
 set env(PATH) "${nshome}bin:$env(PATH)"
 
@@ -67,16 +72,16 @@ set opt(tr)	out
 set opt(ltr)    lan
 set opt(stop)	20
 set opt(num)	1
+set opt(nsrc)	1
 set tcptrace    [open tcp.tr w]
 
 set opt(seed)   0
-set opt(bw)	2Mb
-set opt(delay)	3ms
-set opt(qsize)  20
 set opt(ll)	LL
 set opt(ifq)	Queue/DropTail
+set opt(qtype)	DropTail
 set opt(mac)	Mac/Csma/Ca
 set opt(chan)	Channel
+
 set opt(tp)	TCP/Reno
 set opt(sink)	TCPSink
 set opt(source)	FTP
@@ -85,12 +90,28 @@ set opt(em)     expo
 set opt(eu)     time
 set opt(trans)  ""
 
-LL/Snoop set snoopTick_ [expr [Agent/TCP set tcpTick_]/5]
-LL/Snoop set bandwidth_ 2Mb
-LL/Snoop set delay_ 0
-LL/Snoop set snoopDisable_ 0
-LL/Snoop set srtt_ 50ms
-LL/Snoop set rttvar_ 100ms
+set opt(nsrv)  1
+set opt(ncli)  1
+set opt(ncbr)  0
+set opt(ngw)   2
+
+set opt(bw)    2Mb
+set opt(delay) 3ms
+set opt(ibw)    10Mb
+set opt(idelay) 1ms
+
+set opt(qsize) 20
+set opt(win)   8
+
+set opt(webmodel) $nshome/tcl/http/data
+set opt(phttp) 0
+
+Snoop set snoopTick_ [expr [Agent/TCP set tcpTick_]/5]
+Snoop set bandwidth_ 2Mb
+Snoop set delay_ 0
+Snoop set snoopDisable_ 0
+Snoop set srtt_ 50ms
+Snoop set rttvar_ 100ms
 
 if {$argc == 0} {
 	puts "Usage: $argv0 \[-stop sec\] \[-r value\] \[-num nodes\]"
@@ -102,7 +123,7 @@ if {$argc == 0} {
 
 proc getopt {argc argv} {
 	global opt
-	lappend optlist tr stop num r
+	lappend optlist tr stop num
 	lappend optlist bw delay ll ifq mac chan tp sink source cbr
 
 	for {set i 0} {$i < $argc} {incr i} {
@@ -177,7 +198,7 @@ proc trace-mac {lan trfd} {
 
 # Topology
 # s(0) --------- node(0) - - - - node(1)
-# Snoop runs at node(0).  Make sure opt(ll) is set to LL/Snoop
+# Snoop runs at node(0).  Make sure opt(ll) is set to LL/LLSnoop
 #
 proc create-topology {num} {
 	global ns opt
@@ -186,40 +207,140 @@ proc create-topology {num} {
 	# nodes on wireless LAN; $node(0) is base station
 	for {set i 0} {$i <= $num} {incr i} {
 		set node($i) [$ns node]
-		lappend nodelist $node($i)
+		if { $i > 0 } {
+			lappend nodelist $node($i)
+		}
 	}
 	Queue set limit_ $opt(qsize)
+
 	set lan [$ns make-lan $nodelist $opt(bw) $opt(delay) \
-			$opt(ll) $opt(ifq) $opt(mac) $opt(chan)]
-#	puts "WLAN: $lan $opt(bw) $opt(delay) $opt(ll) $opt(ifq) $opt(mac) $opt(chan)"
+			LL $opt(ifq) $opt(mac) $opt(chan)]
+	$lan addNode [list $node(0)] $opt(bw) $opt(delay) $opt(ll) $opt(ifq) \
+			$opt(mac) LL
 
 	set s(0) [$ns node]
-	$ns duplex-link $s(0) $node(0) 10Mb 1ms DropTail
-	
+	$ns duplex-link $s(0) $node(0) $opt(ibw) $opt(idelay) DropTail
 }
 
-proc create-source {num} {
+proc create-source {nsrc} {
 	global ns opt
 	global lan node s source tcptrace
 
-	for {set i 1} {$i <= $num} {incr i} {
+	for {set i 1} {$i <= $nsrc} {incr i} {
 		set tp($i) [$ns create-connection $opt(tp) \
 				$s(0) $opt(sink) $node($i) 0]
 		set source($i) [$tp($i) attach-source $opt(source)]
 		$ns at [expr $i/1000.0] "$source($i) start"
 		setupTcpTracing $tp($i) $tcptrace
 		$tp($i) set tcpTick_ 0.5
-		LL/Snoop set snoopTick_ 0.1
+		Snoop set snoopTick_ 0.1
 		$tp($i) trace $tcptrace
 	}
 
 	for {set i 0} {$i < $opt(cbr)} {incr i} {
-		set dst [expr $i % $num + 1]
+		set dst [expr $i % $nsrc + 1]
 		set cbr($i) [$ns create-connection CBR \
 				$s(0) Null $node($dst) 0]
 		$ns at 0 "$cbr($i) start"
 	}
 }
+
+
+proc create-www-topology { } {
+	global ns opt c s cb lan lanlist gw
+
+	Queue set limit_ $opt(qsize)
+
+	# Set up Web servers
+	for { set i 0 } { $i < $opt(nsrv) } { incr i } {
+		set s($i) [$ns node]
+	}
+
+	# Set up Web clients
+	for { set i 0 } { $i < $opt(ncli) } { incr i } {
+		set c($i) [$ns node]
+		lappend lanlist $c($i)
+	}
+
+	# Set up CBR cross-traffic sources
+	for { set i 0 } { $i < $opt(ncbr) } { incr i } {
+		set cb($i) [$ns node]
+	}
+
+	# Set up intermediate gateways and hook them up
+	for { set i 0 } { $i < $opt(ngw) } { incr i } {
+		set gw($i) [$ns node]
+		if { $i > 0 } {
+			$ns duplex-link $gw([expr $i-1]) $gw($i) \
+					$opt(bw) $opt(delay) $opt(qtype)
+		}
+	}
+
+	# Hook up sources to gateways
+	for { set i 0 } { $i < $opt(nsrv) } { incr i } {
+		$ns duplex-link $s($i) $gw(0) 10Mb 2ms $opt(qtype)
+	}
+
+	# Hook up clients to last gateway (base station) on wireless LAN
+	for { set i 0 } { $i < $opt(ncli) } { incr i } {
+		set lan [$ns make-lan $lanlist $opt(bw) $opt(delay) \
+				LL $opt(ifq) $opt(mac) $opt(chan)]
+		$lan addNode [list $gw([expr $opt(ngw) - 1])] \
+				$opt(bw) $opt(delay) $opt(ll) $opt(ifq) \
+				$opt(mac) LL
+	}
+
+	# Hook up CBR sources to gateways
+	for { set i 0 } { $i < $opt(ncbr) } { incr i } {
+		$ns duplex-link $cb($i) $gw(0) 10Mb 2ms $opt(qtype)
+	}
+
+
+}
+
+proc create-webModel path {
+	Http setCDF rvClientTime 0
+#	Http setCDF rvClientTime $path/HttpThinkTime.cdf
+	Http setCDF rvReqLen $path/HttpRequestLength.cdf
+	Http setCDF rvNumImg $path/HttpConnections.cdf
+	set rv [Http setCDF rvRepLen $path/HttpReplyLength.cdf]
+	Http setCDF rvImgLen $rv
+}
+
+proc new_http {i server client} {
+	global ns opt http webm
+	set webopt "-srcType $opt(tp) -snkType $opt(sink) -phttp $opt(phttp)"
+	set http($i) [eval new Http $ns $client $server $webopt]
+	puts "i $i"
+	$ns at [expr 0 + $i/1000.0] "$http($i) start"
+}
+
+proc create-www-source { } {
+	global ns opt c s cb tcptrace
+
+	create-webModel $opt(webmodel)
+
+	for {set i 0} {$i < $opt(ncli)} {incr i} {
+		# pick a random server to go to
+		set srv [expr round([uniform 0 [expr $opt(nsrv)-1]])]
+		new_http $i $s($srv) $c($i)
+	}
+	
+	for { set i 0 } { $i < $opt(ncbr) } { incr i } {
+		set cbr($i) [new Agent/CBR]
+		$cbr($i) set interval_ $opt(cbr)
+		$ns attach-agent $cb($i) $cbr($i)
+
+		set null($i) [new Agent/Null]
+		# pick a random sink for the CBR traffic
+		set dst [expr round([uniform 0 [expr $opt(ncli)-1]])]
+		$ns attach-agent $c($dst) $null($i)
+		$ns connect $cbr($i) $null($i)
+		# start cbr's at a time randomly chosen in [0,1]
+		$ns at [uniform 0 1] "$cbr($i) start"
+	}
+}
+
 
 ## MAIN ##
 getopt $argc $argv
@@ -233,21 +354,38 @@ set ns [new Simulator]
 create-trace $opt(tr)
 $ns trace-all $trfd
 
-create-topology $opt(num)
+if { [info exists opt(www)] } {
+	create-www-topology
+} else {
+	create-topology $opt(num)
+}
 
 $lan trace $ns $trfd
 
 if [info exists opt(tracemac)] { trace-mac $lan $ltrfd }
 
 if { [info exists opt(e)] } {
-	for {set i 1} {$i <= $opt(num)} {incr i} {
-		lappend dstlist $node($i)
+	if { $opt(e) == 0 || $opt(e) == "" } {
+		puts "error rate 0 ==> no errors"
+	} else {
+		if { [info exists opt(www)] } {
+			$lan create-error $gw([expr $opt(ngw) - 1]) $lanlist \
+					$opt(em) $opt(e) $opt(eu) $opt(trans)
+		} else {
+			for {set i 1} {$i <= $opt(num)} {incr i} {
+				lappend dstlist $node($i)
+			}
+			$lan create-error $node(0) $dstlist $opt(em) $opt(e) \
+					$opt(eu) $opt(trans)
+		}
 	}
-	$lan create-error $node(0) $dstlist $opt(em) $opt(e) $opt(eu) \
-			$opt(trans)
 }
 
-create-source $opt(num)
+if { [info exists opt(www)] } {
+	create-www-source
+} else {
+	create-source $opt(nsrc)
+}
 
 $ns at $opt(stop) "finish"
 $ns run
