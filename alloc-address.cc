57f9a38e66bdccdf9f8250f128e3a2caab643c0d
haldar <haldar>
1998-04-01 17:47:01 +0000

    Defines functions that allocate bits for ns address structure and returns corresponding mask and shift values.

---
+++ alloc-address.cc
@@ -0,0 +1,231 @@
+/*
+ * Copyright (c) 1997 Regents of the University of California.
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. All advertising materials mentioning features or use of this software
+ *    must display the following acknowledgement:
+ * 	This product includes software developed by the Daedalus Research
+ * 	Group at the University of California Berkeley.
+ * 4. Neither the name of the University nor of the Research Group may be
+ *    used to endorse or promote products derived from this software without
+ *    specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
+
+/* functions invoked to allocate bits to the ns-address space */
+
+
+#include <stdlib.h>
+#include <math.h>
+#include <assert.h>
+#include "config.h"
+#include "tclcl.h"
+
+
+
+class AllocAddr : public TclObject {
+public:
+    AllocAddr();
+    ~AllocAddr();
+    int command(int argc, const char*const* argv);
+protected:
+    void get_mask(nsmask_t  *mask, int fieldsize);
+    void alloc(int n);
+    void check_size(int n);
+    bool AllocAddr::find_free(int len, int *pos);
+    bool AllocAddr::test(int which);
+    void set_field(int offset, int len, nsmask_t *mask, int *shift);
+    void free_field(int len);
+    int size_;
+    int *bitmap_;
+};
+
+
+class AllocAddrClass : public TclClass {
+public:
+    AllocAddrClass() : TclClass("AllocAddr") {}
+    TclObject* create(int, const char*const*) {
+	return (new AllocAddr());
+    }
+} AllocAddr_class;
+
+
+
+int AllocAddr::command(int argc, const char*const* argv)
+{
+    int offset,
+	addrsize,
+	shift,
+	fieldlen;
+    nsmask_t mask;
+  
+    Tcl& tcl = Tcl::instance();
+  
+    if (argc == 3) {
+	if (strcmp(argv[1], "freebit") == 0) {
+	    fieldlen = atoi(argv[2]);
+	    free_field(fieldlen);
+	    return (TCL_OK);
+	}
+    }
+
+
+    if (argc == 4) {
+	if (strcmp(argv[1], "setbit") == 0) {
+	    fieldlen = atoi(argv[2]);
+	    addrsize = atoi(argv[3]);
+	    check_size(addrsize);
+	    if (!find_free(fieldlen, &offset)) {
+		
+		tcl.result("alloc-address: no contiguous space found\n");
+		return (TCL_ERROR);
+	    }
+	    set_field(offset, fieldlen, &mask, &shift);
+	    tcl.resultf("0x%x %d", mask, shift);
+	    return (TCL_OK);
+	}
+    }
+}
+
+
+
+
+AllocAddr::AllocAddr()
+{
+    size_ = 0;     
+    bitmap_ = 0;
+
+}
+
+
+AllocAddr::~AllocAddr()
+{
+    delete [] bitmap_;
+
+}
+
+
+
+void AllocAddr::alloc(int n)
+{
+    size_ = n;
+    bitmap_ = new int[n];
+    for (int i=0; i < n; i++)
+	bitmap_[i] = 0;
+}
+
+
+
+
+void AllocAddr::check_size(int n)
+{
+    if (n < size_)
+	return;
+    if (size_ == 0) {
+	alloc(n);
+	return;
+    }
+    
+    int *old = bitmap_;
+    int osize = size_;
+    alloc(n);
+    for (int i = 0; i < osize; i++) 
+	bitmap_[i] = old[i];
+    delete [] old;
+}
+
+
+void AllocAddr::get_mask(nsmask_t *mask, int fieldsize)
+{
+    int temp;
+    temp = (int)(pow(2, (double)fieldsize));
+    *mask = temp-1;
+	
+}
+
+
+bool AllocAddr::test(int which)
+{
+    
+    assert(which >= 0 && which < size_);
+    if (bitmap_[which] == 1)
+	return TRUE;
+    else
+	return FALSE;
+}
+
+
+bool AllocAddr::find_free(int len, int *pos) 
+{
+    
+    int count = 0;
+    int temp = 0;
+    
+    for (int i = 0; i < size_; i++)
+        if (!test(i)) {
+	    /**** check if n contiguous bits are free ****/
+	    temp = i;
+	    for (int k = 0; k < len; k++, i++){
+		if(test(i)) {
+		    count = 0;
+		    break;
+		}
+		count++;
+	    }
+	    if (count) {
+		*pos = temp;
+		return 1;
+	    }
+	}
+    return 0;
+}
+
+
+
+
+
+void AllocAddr::set_field(int offset, int len, nsmask_t *mask, int *shift)
+{
+    int i, k;
+
+    for (k = 0, i = offset; k < len; k++, i++) {
+	bitmap_[i] = 1; 
+    }
+    *shift = offset;
+    get_mask(mask, len);
+}
+
+
+
+void AllocAddr::free_field(int len)
+{
+    int count = 0;
+
+    for (int i = 0; i < size_; i++) {
+	if (test(i)) {
+	    bitmap_[i] = 0;
+	    count++;
+	}
+	if (count == len)
+	    break;
+    }
+}
