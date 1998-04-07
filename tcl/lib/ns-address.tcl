a68803d9c43a1ebce649a317af9c3162bf03fc75
haldar <haldar>
1998-04-07 22:00:14 +0000

    lot of changes seen in API's: API's have been re-named, some have been re-structured based on comments/feedbacks received from vint-usc.

--- tcl/lib/ns-address.tcl
+++ tcl/lib/ns-address.tcl
@@ -39,47 +39,75 @@
 # DEFADDRSIZE_ and MAXADDRSIZE_ defined in ns-default.tcl
 
 
-# Name :            set_address() 
-# Synopsis :        set_address
+# Name :            set-address-format ()
+# Options :         default, expanded, hierarchical (default) and hierarchical (with
+#                    specific allocations) 
+# Synopsis :        set-address-format <option> <additional info, if required by option> 
+#
+# Option:1          def (default settings)
+# Synopsis :        set-address-format def
 # Description:      This allows default settings in the following manner:
 #                    * 8bits portid
-#                    * if mcast option (EnableMcast) set,
-#                      * 1 bit for mcast and 7 bits nodeid
-#                      * else 8 bits nodeid
-#                    * if Hierarchical routing option (EnableHierRt_) set,
-#                      * sets a 3 level hierarchy with 8 bits in each level
-#                      * or 8 8 7 (if mcast)
+#                    * 1 bit for mcast and 7 bits nodeid (mcast should be enabled
+#                      before Simulator is inited (new Simulator is called)--> leads to
+#                      unnecessary wasting of 1 bit even if mcast is not set.
 #
 #
+# Option :2         expanded
+# Synopsis :        set-address-format expanded
+# Description:      This allows to expand the address space from default size 16 to 32
+#                   with bit allocated to the field in the foll manner:
+#                    * 8bits portid
+#                    * 1 bit for mcast and 21 bits nodeid (and same comments regarding
+#                       setting mcast as for default option above)
 #
-
-# Name :            set_portaddress() 
-# Synopsis :        set_portaddress <#bits for portid> 
-# Description :     This should be used incase of need to extend portid. Similar to 
-#                   set_address(). 
-#                   * Sets portbits as specified (increases size of addressspace, if reqd)
+#
+# Option :3         hierarchical (default)
+# Synopsis :        set-address-format hierarchical
+# Description:      This allows setting of hierarchical address as follows:
+#                   * Sets 8bits portid
 #                   * Sets mcast bit (if specified)
-#                   * Sets remaining bits for nodeid.
+#                   * Sets default hierchical levels with:
+#                      * 3 levels of hierarchy and
+#                      * 8 bits in each level (looks like 8 8 8)
+#                      * 8 8 7 if mcast is enabled.
 #
 #
-#
-#
-
-# Name :            set_hieraddress() 
-# Synopsis :        set_hieraddress <#levels of hierarchy> <#bits for each level>> 
-# Description :     This should be used for hierarchical routing with specific 
-#                   requirement of number of hierarchy levels. 
-#                   
-#                   * Sets 8 bits for portbits 
+# Option :4        hierarchical (specified)
+# Synopsis :        set-address-format hierarchical <#n hierarchy levels> <#bits for 
+#                   level1> <#bits for level 2> ....<#bits for nth level>
+#                   e.g  
+#                   set-address-format hierarchical 4 2 2 2 10
+# Description:      This allows setting of hierarchical address as specified for each 
+#                   level.
+#                   * Sets 8bits portid
 #                   * Sets mcast bit (if specified)
 #                   * Sets hierchical levels with bits specified for each level.
+
+
+
+# Name :            expand-port-field-bits ()
+# Synopsis :        expand-port-field-bits <#bits for portid> 
+# Description :     This should be used incase of need to extend portid. 
+#                   This commnad may be used in conjuction with 
+#                   set-address-format command explained above.
+#                   expand-port-field-bits checks and raises error in the foll. cases
+#                    * if the requested portsize cannot be accomodated (i.e  
+#                         if sufficient num.of free bits are not available)
+#                    * if requested portsize is less than equal to existing portsize.
+#                   and incase of no errors sets port field with bits as specified.
+#2
 #
+#
+#
+
 
 # Errors returned : * if # of bits specified less than 0.
 #                   * if bit positions clash (contiguous # of requested free bits not 
 #                     found)
 #                   * if total # of bits exceed MAXADDRSIZE_
-#                   * if hierarchical setting is attempted w/o setting right option.
+#                   * if expand-port-field-bits is attempted with portbits less or equal
+#                     to the existing portsize.
 #                   * if # hierarchy levels donot match with #bits specified (for
 #                     each level). 
 # 
@@ -89,69 +117,92 @@ Class AddrParams
 Class AllocAddrBits
 
 
-Simulator instproc get-allocaddr {} {
+Simulator instproc get-AllocAddrBits {prog} {
     $self instvar allocAddr_
     if ![info exists allocAddr_] {
+	set allocAddr_ [new AllocAddrBits]
+    } elseif ![string compare $prog "new"] {
+	puts "Warning: existing Address Space was destroyed\n"
 	set allocAddr_ [new AllocAddrBits]
     }
     return $allocAddr_
 }
 
 
+Simulator instproc set-address-format {opt args} {
+    set len [llength $args]
+    if {[string compare $opt "def"] == 0} {
+	$self set-address 8 8
+    } elseif {[string compare $opt "expanded"] == 0} {
+	$self set-address 22 8
+    } elseif {[string compare $opt "hierarchical"] == 0 && $len == 0} {
+	if [Simulator set EnableMcast_] {
+	    
+	    $self set-hieraddress 3 7 8 8
+	} else {
+	    $self set-hieraddress 3 8 8 8
+	}
+    } else {
+	if {[string compare $opt "hierarchical"] == 0 && $len > 0} {
+	    eval $self set-hieraddress [lindex $args 0] [lrange $args 1 [expr $len - 1]]
+	}
+    } 
+}
 
-# sets address to default values  
 
-Simulator instproc set-address {} {
-    set a [$self get-allocaddr]
+Simulator instproc expand-port-field-bits nbits {
+    set a [$self get-AllocAddrBits "notnew"]
+    if {$nbits <= [$a set portsize_]} {
+	error "expand-port-field-bits:requested portsize less than or equal to existing portsize"
+    } 
+
+    $a expand-portbits $nbits
+}
+
+
+# sets address for nodeid and port fields 
+# The order of setting bits for different fields does matter
+# and should be as follows:
+# mcast
+# idbits
+# and finally portbits
+# this is true for both set-address and set-hieraddress
+
+Simulator instproc set-address {node port} {
+    set a [$self get-AllocAddrBits "new"]
     $a set size_ [AllocAddrBits set DEFADDRSIZE_]
-    $a set-portbits 8
-    if [Simulator set EnableMcast_] {
-	$a set-mcastbits 1
+    if {[expr $node + $port] > [$a set size_]} {
+	$a set size_ [AllocAddrBits set MAXADDRSIZE_]
     }
-    set lastbit [expr 8 - [$a set mcastsize_]]
-    if [Simulator set EnableHierRt_] {
-	$a set-idbits 3 8 8 $lastbit
-    }
-    if ![Simulator set EnableHierRt_] {
-    # else {
-	$a set-idbits 1 $lastbit
-    }
+	
+    # one bit is set aside for mcast as default :: this waste of 1 bit may be avoided, if 
+    # mcast option is enabled before the initialization of Simulator.
+    #     if [Simulator set EnableMcast_] {
+    $a set-mcastbits 1
+    #     }
+
+    set lastbit [expr $node - [$a set mcastsize_]]
+    $a set-idbits 1 $lastbit
+    $a set-portbits $port
+
 }
 
 
 
 
-#sets port bits and allocates remaining bits to id
+#sets hierarchical bits
 
-Simulator instproc set-portaddress bits {
-    if {$bits < 1} {
-	error "\# bits for port < 1"
+Simulator instproc set-hieraddress {hlevel args} {
+    set a [$self get-AllocAddrBits "new"]
+    $a set size_ [AllocAddrBits set MAXADDRSIZE_]
+    if ![Simulator set EnableHierRt_] {
+	Simulator set EnableHierRt_ 1
     }
-    set a [$self get-allocaddr]
-    $a set size_ [AllocAddrBits set DEFADDRSIZE_]
-    $a set-portbits $bits
     if [Simulator set EnableMcast_] {
 	$a set-mcastbits 1
     }
-    set lastbit [expr [$a set size_] - [$a set mcastsize_] - $bits]
-    $a set-idbits 1 $lastbit
-} 
-
-
-
-#sets hierarchical idbits
-
-Simulator instproc set-hieraddress {hlevel args} {
-    if ![Simulator set EnableHierRt_] {
-	error "set_hieraddress: EnableHierRt_ not set"
-    }
-    set a [$self get-allocaddr]
-    $a set size_ [AllocAddrBits set MAXADDRSIZE_]
-    eval $a set-portbits 8 
-    if [Simulator set EnableMcast_] {
-	eval $a set-mcastbits 1
-    }
     eval $a set-idbits $hlevel $args
+    $a set-portbits 8 
 }
 
 
@@ -159,21 +210,28 @@ Simulator instproc set-hieraddress {hlevel args} {
 
 AllocAddrBits instproc init {} {
     eval $self next
-    $self instvar size_ portsize_ idsize_ mcastsize_ params_
+    $self instvar size_ portsize_ idsize_ mcastsize_
     set size_ 0
     set portsize_ 0
     set idsize_ 0
     set mcastsize_ 0
-    set params_ [new AddrParams]
 }
 
 
-AllocAddrBits instproc get-address {} {
+AllocAddrBits instproc get-AllocAddr {} {
     $self instvar addrStruct_
     if ![info exists addrStruct_] {
 	set addrStruct_ [new AllocAddr]
     }
     return $addrStruct_
+}
+
+AllocAddrBits instproc get-Address {} {
+    $self instvar address_
+    if ![info exists address_] {
+	set address_ [new Address]
+    }
+    return $address_
 }
 
 
@@ -186,88 +244,181 @@ AllocAddrBits instproc chksize {bit_num prog} {
     if {$totsize > [AllocAddrBits set MAXADDRSIZE_]} {
 	error "$prog : Total \# bits exceed MAXADDRSIZE"
     }
-    if {$totsize > [AllocAddrBits set DEFADDRSIZE_]} {
-	set size_ [AllocAddrBits set MAXADDRSIZE_]
+    if { $size_ < [AllocAddrBits set MAXADDRSIZE_]} {
+	if {$totsize > [AllocAddrBits set DEFADDRSIZE_]} {
+	    set size_ [AllocAddrBits set MAXADDRSIZE_]
+	    return 1
+	} 
     }
+    return 0
 
 }
 
 
 
 AllocAddrBits instproc set-portbits {bit_num} {
-    $self instvar size_ portsize_ params_
+    $self instvar size_ portsize_ 
     set portsize_ $bit_num
-    $self chksize $portsize_ "setport"
-    set a [$self get-address] 
+    if [$self chksize portsize_ "setport"] {
+	error "set-portbits: size_ has been changed."
+    }
+    
+    set a [$self get-AllocAddr] 
     set v [$a setbit $bit_num $size_]
-    $params_ set PortMask_ [lindex $v 0]
-    $params_ set PortShift_ [lindex $v 1]
+    
+    AddrParams set PortMask_ [lindex $v 0]
+    AddrParams set PortShift_ [lindex $v 1]
     ### TESTING
-    set mask [lindex $v 0]
-    set shift [lindex $v 1]
-    puts "size = $size_\n Portshift = $shift \n PortMask = $mask\n"
+    # set mask [lindex $v 0]
+    # set shift [lindex $v 1]
+    # puts "Portshift = $shift \n PortMask = $mask\n"
+
+    set ad [$self get-Address]
+    $ad portbits-are [AddrParams set PortShift_] [AddrParams set PortMask_]
+}
+
+
+
+AllocAddrBits instproc expand-portbits nbits {
+    $self instvar portsize_ size_ mcastsize_ idsize_ hlevel_ hbits_ addrStruct_
+    set temp $portsize_
+    set portsize_ $nbits
+    
+    # these extra checks and re-allocation are done to ensure mcast bit is always the
+    # MSB; so when size_ gets expanded as a result of expanding port, need to re-allocate 
+    # mcast and id bits.
+    
+    if [$self chksize $nbits "expandport"] {
+	set addrStruct_ [new AllocAddr]
+	if {$mcastsize_ > 0} {
+	    $self set-mcastbits $mcastsize_
+	}
+	if {$idsize_ > 0} {
+	    eval $self set-idbits $hlevel_ $hbits_ 
+	}
+	$self set-portbits $nbits
+    } else {
+	set a [$self get-AllocAddr] 
+	set v [$a expand-port [expr $nbits - $temp] $size_ $temp ]
+	AddrParams set PortMask_ [lindex $v 0]
+	AddrParams set PortShift_ [lindex $v 1]
+	set ad [$self get-Address]
+	$ad portbits-are [AddrParams set PortShift_] [AddrParams set PortMask_]
+	### TESTING
+	# set mask [lindex $v 0]
+	# set shift [lindex $v 1]
+	# puts "size = $size_\n Portshift = $shift \n PortMask = $mask\n"
+    }
 }
 
 
 
 AllocAddrBits instproc set-mcastbits {bit_num} {
-    $self instvar size_ mcastsize_ params_
+    $self instvar size_ mcastsize_
     if {$bit_num != 1} {
 	error "setmcast : mcastbit > 1"
     }
     set mcastsize_ $bit_num
-    $self chksize mcastsize_ "setmcast"
-    set mcastsize_ $bit_num
-    set a [$self get-address] 
+
+    #chk to ensure there;s no change in size
+    if [$self chksize mcastsize_ "setmcast"] {
+    # assert {$chk == 0} --> assert doesn't seem to work
+	error "set-mcastbits: size_ has been changed."
+    }
+    set a [$self get-AllocAddr] 
     set v [$a setbit $bit_num $size_]
-    $params_ set McastMask_ [lindex $v 0]
-    $params_ set McastShift_ [lindex $v 1]
+    AddrParams set McastMask_ [lindex $v 0]
+    AddrParams set McastShift_ [lindex $v 1]
     ### TESTING
-    set mask [lindex $v 0]
-    set shift [lindex $v 1]
-    puts "size = $size_\n Mcastshift = $shift \n McastMask = $mask\n"
+    # set mask [lindex $v 0]
+    # set shift [lindex $v 1]
+    # puts "Mcastshift = $shift \n McastMask = $mask\n"
+
+    set ad [$self get-Address]
+    $ad mcastbits-are [AddrParams set McastShift_] [AddrParams set McastMask_]
+
 }
 
 
 AllocAddrBits instproc set-idbits {nlevel args} {
-    $self instvar size_ portsize_ idsize_ params_
+    $self instvar size_ portsize_ idsize_ hlevel_ hbits_
     if {$nlevel != [llength $args]} {
 	error "setid: hlevel < 1 OR nlevel and \# args donot match"
     }
-    set a [$self get-address] 
+    set a [$self get-AllocAddr] 
     set old 0
+    set idsize_ 0
+
     for {set i 0} {$i < $nlevel} {incr i} {
-	set idlevel($i) [lindex $args $i]
-	set idsize_ [expr $idsize_ + $idlevel($i)]
-	$self chksize $idlevel($i) "setid"
-	set v [$a setbit $idlevel($i) $size_]
-     	$params_ set NodeMask_([expr $i+1]) [lindex $v 0]
-     	$params_ set NodeShift_([expr $i+1]) [lindex $v 1]
-	### TESTING
-	set mask [lindex $v 0]
-	set shift [lindex $v 1]
-	puts "size = $size_\n Nodeshift = $shift \n NodeMask = $mask\n"
-	set old [expr $old + $idlevel($i)]
+	set bpl($i) [lindex $args $i]
+	set idsize_ [expr $idsize_ + $bpl($i)]
+	
+	# check to ensure there's no change in size
+	set chk [$self chksize $bpl($i) "setid"]
+	# assert {$chk ==0}
+	if {$chk > 0} {
+	    error "set-mcastbits: size_ has been changed."
+	}
+	set v [$a setbit $bpl($i) $size_]
+     	AddrParams set NodeMask_([expr $i+1]) [lindex $v 0]
+	set m([expr $i+1]) [lindex $v 0]
+     	AddrParams set NodeShift_([expr $i+1]) [lindex $v 1]
+	set s([expr $i+1]) [lindex $v 1]
+	AddrParams set hlevel_ $nlevel
+	set hlevel_ $nlevel
+	lappend hbits_ $bpl($i)
+	set old [expr $old + $bpl($i)]
     }
     
+    set ad [$self get-Address]
+    eval $ad idsbits-are [array get s]
+    eval $ad idmbits-are [array get m]
+    
+    ### TESTING
+    # set mask [lindex $v 0]
+    # set shift [lindex $v 1]
+    # puts "Nodeshift = $shift \n NodeMask = $mask\n"
 }
 
 
-AddrParams instproc init {} {
-    eval $self next
-    $self instvar PortShift_ PortMask_ NodeShift_ NodeMask_ 
-    $self instvar McastShift_ McastMask_
+
+AddrParams proc levels-in-nodeaddr dst {
+    for {set i 1} {$i <= [$class set hlevel_]} {incr i} {
+	set tmp [expr dst >> [$class set NodeShift_($i)] & [$class set NodeMask_($i)]]
+	if {$tmp != ""} {
+	    incr count
+	}
+    }
+    assert {$count <= [$class set hlevel_]}
+    return $count
+}
+	
+
+AddrParams proc get-nodeaddr {level dst} {
+    set tmp [expr dst >> [$class set NodeShift_($level)] & [$class set NodeMask_($level)]]
+    return tmp
+}
+    
+    
+
+
+
+
+# AddrParams instproc init {} {
+#     eval $self next
+#     $self instvar PortShift_ PortMask_ NodeShift_ NodeMask_ 
+#     $self instvar McastShift_ McastMask_
   
-}
+# }
 
-AddrParams proc instance {} {
-    set ap [AddrParams info instances]
-    if {$ap != ""} {
-	return $ap
-    }
+# AddrParams proc instance {} {
+#     set ap [AddrParams info instances]
+#     if {$ap != ""} {
+# 	return $ap
+#     }
     
-    error "Cannot find instance of AddrParams"
-}
+#     error "Cannot find instance of AddrParams"
+# }
 
 
 
