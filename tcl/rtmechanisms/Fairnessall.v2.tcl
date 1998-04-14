90c51a1523b82556fa6a7f469e38d664c56fcf8a
sfloyd <sfloyd>
1998-04-14 01:50:40 +0000

    Changes in progress, updating from v1 to v2.

--- tcl/rtmechanisms/Fairnessall.v2.tcl
+++ tcl/rtmechanisms/Fairnessall.v2.tcl
@@ -3,10 +3,28 @@
 #
 set datafile Fairness.data
 source Fairnessall1.v2.tcl
+set scheduling [lindex $argv 0]
+set cbrs [lindex $argv 1]
+set tcps [lindex $argv 2]
+set type [lindex $argv 3]
+if {$type == "Collapse"} {
+  set bandwidth 128Kb
+} else {
+  set bandwidth 10Mb
+}
 #
-set bandwidth 10Mb
-set infile Fairness.tr
+set singlefile temp.tr
+set label $type.$scheduling.$cbrs.$tcps
+set infile temp.tr
 set psfile Fairness.ps
+
+# Run a single simulation.
+proc run_sim {bandwidth scheduling cbrs tcps singlefile datafile i} {
+  set interval [expr $cbrs * 0.000$i]
+  puts "../../ns Collapse.v2.tcl simple $interval $bandwidth $scheduling $cbrs $tcps"
+  exec ../../ns Collapse.v2.tcl simple $interval $bandwidth $scheduling $cbrs $tcps
+  append $singlefile $datafile 
+}
 
 exec rm -f $datafile
 # for interval=0.00008, CBR arrival rate is 10 Mbps
@@ -14,23 +32,19 @@ exec rm -f $datafile
 # intervals 0.0001 to 0.0009
 for {set i 4} {$i <= 9} {incr i 1} {
     puts "../../ns Collapse.v2.tcl simple 0.000$i $bandwidth"
-# need catch for those "warning: no class variable Queue/RED::fracminthresh_"
-    catch {exec ../../ns Collapse.v2.tcl simple 0.000$i $bandwidth} result
-#    puts stderr $result
+    run_sim $bandwidth $scheduling $cbrs $tcps $singlefile $datafile $i
     append $infile $datafile
 }
 # intervals 0.001 to 0.009
 for {set i 1} {$i <= 9} {incr i 1} {
     puts "../../ns Collapse.v2.tcl simple 0.00$i $bandwidth"
-    catch {exec ../../ns Collapse.v2.tcl simple 0.00$i $bandwidth} result
-#    puts stderr $result
+    run_sim $bandwidth $scheduling $cbrs $tcps $singlefile $datafile $i
     append $infile $datafile
 }
 # intervals 0.01 to 0.09
 for {set i 1} {$i <= 9} {incr i 1} {
     puts "../../ns Collapse.v2.tcl simple 0.0$i $bandwidth"
-    catch {exec ../../ns Collapse.v2.tcl simple 0.0$i $bandwidth} result
-#    puts stderr $result
+    run_sim $bandwidth $scheduling $cbrs $tcps $singlefile $datafile $i
     append $infile $datafile
 }
 finish
