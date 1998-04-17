8c5887ce4e4755b7da93bad29e169d8fbc70fbdc
kfall <kfall>
1998-04-17 18:57:18 +0000

    put if's around relevant part

--- tcl/emulate/ns-emulate.tcl
+++ tcl/emulate/ns-emulate.tcl
@@ -2,7 +2,9 @@
 # ns-emulate.tcl
 #	configuration for ns emulation
 #
-Network/Pcap/Live set snaplen_ 4096
-Network/Pcap/Live set promisc_ false
-Network/Pcap/Live set timeout_ 0
-Network/Pcap/Live set optimize_ true
+if [TclObject is-class Network/Pcap/Live] {
+	Network/Pcap/Live set snaplen_ 4096
+	Network/Pcap/Live set promisc_ false
+	Network/Pcap/Live set timeout_ 0
+	Network/Pcap/Live set optimize_ true
+}
