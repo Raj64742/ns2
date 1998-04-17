3bcbef624bdca91206c9b0d027b6c192f41739ad
haldar <haldar>
1998-04-17 22:44:37 +0000

    added support for hier routing

--- tcl/lib/ns-route.tcl
+++ tcl/lib/ns-route.tcl
@@ -30,7 +30,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/lib/ns-route.tcl,v 1.2 1997/10/21 00:54:20 ahelmy Exp $
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/lib/ns-route.tcl,v 1.3 1998/04/17 22:45:41 haldar Exp $
 #
 
 Simulator instproc rtproto {proto args} {
@@ -46,51 +46,51 @@ Simulator instproc get-routelogic {} {
 }
 
 Simulator instproc compute-routes {} {
-	$self instvar Node_ routingTable_ link_
-	#
-	# Compute all the routes using the route-logic helper object.
-	#
-        set r [$self get-routelogic]
-        #set r [new RouteLogic]		;# must not create multiple instances
-	foreach ln [array names link_] {
-		set L [split $ln :]
-		set srcID [lindex $L 0]
-		set dstID [lindex $L 1]
-	        if { [$link_($ln) up?] == "up" } {
-			$r insert $srcID $dstID [$link_($ln) cost?]
-		} else {
-			$r reset $srcID $dstID
-		}
+    $self instvar Node_ routingTable_ link_
+    #
+    # Compute all the routes using the route-logic helper object.
+    #
+    set r [$self get-routelogic]
+    #set r [new RouteLogic]		;# must not create multiple instances
+    foreach ln [array names link_] {
+	set L [split $ln :]
+	set srcID [lindex $L 0]
+	set dstID [lindex $L 1]
+	if { [$link_($ln) up?] == "up" } {
+	    $r insert $srcID $dstID [$link_($ln) cost?]
+	} else {
+	    $r reset $srcID $dstID
 	}
-	$r compute
-	#$r dump $nn
+    }
+    $r compute
+    #$r dump $nn
 
-	#
-	# Set up each classifer (aka node) to act as a router.
-	# Point each classifer table to the link object that
-	# in turns points to the right node.
-	#
-	set i 0
-	set n [Node set nn_]
-	while { $i < $n } {
-		set n1 $Node_($i)
-		set j 0
-		while { $j < $n } {
-			if { $i != $j } {
-			        # shortened nexthop to nh, to fit add-route in
-			        # a single line
-				set nh [$r lookup $i $j]
-			        if { $nh >= 0 } {
-					$n1 add-route $j [$link_($i:$nh) head]
-				}
-			} 
-			incr j
+    #
+    # Set up each classifer (aka node) to act as a router.
+    # Point each classifer table to the link object that
+    # in turns points to the right node.
+    #
+    set i 0
+    set n [Node set nn_]
+    while { $i < $n } {
+	set n1 $Node_($i)
+	set j 0
+	while { $j < $n } {
+	    if { $i != $j } {
+		# shortened nexthop to nh, to fit add-route in
+		# a single line
+		set nh [$r lookup $i $j]
+		if { $nh >= 0 } {
+		    $n1 add-route $j [$link_($i:$nh) head]
 		}
-		incr i
+	    } 
+	    incr j
 	}
-#	delete $r
-#XXX used by multicast router
-	#set routingTable_ $r		;# already set by get-routelogic
+	incr i
+    }
+    #	delete $r
+    #XXX used by multicast router
+    #set routingTable_ $r		;# already set by get-routelogic
 }
 
 RouteLogic instproc register {proto args} {
@@ -140,25 +140,124 @@ RouteLogic instproc notify {} {
     }
     if { [rtObject info instances] == ""} {
 	foreach node [[Simulator instance] all-nodes-list] {
-		# XXX using dummy 0 for 'changes'
-		$node notify-mcast 0
+	    # XXX using dummy 0 for 'changes'
+	    $node notify-mcast 0
 	}
     }
 }
+
+
+#
+# bad hack to create address for hier-route-lookup at each level
+# i.e at level 2, address to lookup should be 0.1 and not just 1
+#
+
+RouteLogic instproc append-addr {level addrstr} {
+    
+    if {$level != 0} {
+	set str [lindex $addrstr 0]
+	for {set i 1} {$i < $level} {incr i} {
+	    append str . [lindex $addrstr [expr $i]]
+	}
+	return $str
+    }
+}
+
+
+
+#
+# Hierarchical routing support
+#
+Simulator instproc compute-hier-routes {} {
+    $self instvar Node_ routingTable_ link_
+    set r [$self get-routelogic]
+    #
+    # send hierarchical data :
+    # array of cluster size, #clusters, #domains
+    # assuming 3 levels of hierarchy --> this should be extended to support
+    # n-levels of hierarchy
+    #
+    puts "Computing Hierarchical routes\n"
+    set level [AddrParams set hlevel_]
+    $r hlevel-is $level
+    eval $r send-hdata [AddrParams set eilevel_]
+    eval $r send-hlastdata [AddrParams set eilastlevel_]
+    
+    foreach ln [array names link_] {
+	set L [split $ln :]
+	set srcID [[$self get-node-by-id [lindex $L 0]] node-addr]
+	set dstID [[$self get-node-by-id [lindex $L 1]] node-addr]
+	if { [$link_($ln) up?] == "up" } {
+	    $r hier-insert $srcID $dstID [$link_($ln) cost?]
+	} else {
+	    $r hier-reset $srcID $dstID
+	}
+    }
+    $r hier-compute
+    
+    #
+    # Set up each classifier (n for n levels of hierarchy) for every node
+    #
+    set i 0
+    set n [Node set nn_]
+    
+    #
+    #for each node
+    #
+    while { $i < $n } {
+	set n1 $Node_($i)
+	set addr [$n1 node-addr]
+	set L [$n1 split-addrstr $addr]
+	#
+	# for each hierarchy level, populate classifier for that level
+	#
+	for {set k 0} {$k < $level} {incr k} {
+	    set csize [AddrParams elements-in-level? $addr $k]
+	    
+	    #
+	    # for each element in that level (elements maybe nodes or clusters or domains 
+	    #
+	    if {$k > 0} {
+		set prefix [$r append-addr $k $L]
+	    }
+	    for {set m 0} {$m < $csize} {incr m} {
+		if { $m != [lindex $L $k]} {
+		    if {$k > 0} {
+			set str $prefix
+			append str . $m
+		    } else {
+			set str $m
+		    }
+
+		    set nh [$r hier-lookup $addr $str]
+		    # puts "from $addr to $str - nexthop = $nh"
+		    set node [$self get-node-id $nh]
+		    if { $node >= 0 } {
+			$n1 add-hroute $str [$link_($i:$node) head]
+		    }
+		}
+	    }
+	}
+	incr i
+    }
+}
+
+
+
 
 #
 # debugging method to dump table (see route.cc for C++ methods)
 #
 RouteLogic instproc dump nn {
-	set i 0
-	while { $i < $nn } {
-		set j 0
-		while { $j < $nn } {
-			puts "$i -> $j via [$self lookup $i $j]"
-			incr j
-		}
-		incr i
+    set i 0
+    while { $i < $nn } {
+	set j 0
+	while { $j < $nn } {
+	    puts "$i -> $j via [$self lookup $i $j]"
+	    incr j
 	}
+	incr i
+    }
 }
 
 #####
