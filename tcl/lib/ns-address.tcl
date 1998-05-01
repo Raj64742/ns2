040b074213889e531462ee1525b8011ccce862a7
haldar <haldar>
1998-05-01 19:07:51 +0000

    modified to allow variable number of clusters

--- tcl/lib/ns-address.tcl
+++ tcl/lib/ns-address.tcl
@@ -120,45 +120,45 @@ Class AllocAddrBits
 
 
 Simulator instproc get-AllocAddrBits {prog} {
-    $self instvar allocAddr_
-    if ![info exists allocAddr_] {
-	set allocAddr_ [new AllocAddrBits]
-    } elseif ![string compare $prog "new"] {
-	# puts "Warning: existing Address Space was destroyed\n"
-	set allocAddr_ [new AllocAddrBits]
-    }
-    return $allocAddr_
+	$self instvar allocAddr_
+	if ![info exists allocAddr_] {
+		set allocAddr_ [new AllocAddrBits]
+	} elseif ![string compare $prog "new"] {
+		# puts "Warning: existing Address Space was destroyed\n"
+		set allocAddr_ [new AllocAddrBits]
+	}
+	return $allocAddr_
 }
 
 
 Simulator instproc set-address-format {opt args} {
-    set len [llength $args]
-    if {[string compare $opt "def"] == 0} {
-	$self set-address 8 8
-    } elseif {[string compare $opt "expanded"] == 0} {
-	$self set-address 23 8
-    } elseif {[string compare $opt "hierarchical"] == 0 && $len == 0} {
-	if [Simulator set EnableMcast_] {
-	    
-	    $self set-hieraddress 3 6 8 8
+	set len [llength $args]
+	if {[string compare $opt "def"] == 0} {
+		$self set-address 8 8
+	} elseif {[string compare $opt "expanded"] == 0} {
+		$self set-address 23 8
+	} elseif {[string compare $opt "hierarchical"] == 0 && $len == 0} {
+		if [Simulator set EnableMcast_] {
+			
+			$self set-hieraddress 3 6 8 8
+		} else {
+			$self set-hieraddress 3 7 8 8
+		}
 	} else {
-	    $self set-hieraddress 3 7 8 8
-	}
-    } else {
-	if {[string compare $opt "hierarchical"] == 0 && $len > 0} {
-	    eval $self set-hieraddress [lindex $args 0] [lrange $args 1 [expr $len - 1]]
-	}
-    } 
+		if {[string compare $opt "hierarchical"] == 0 && $len > 0} {
+			eval $self set-hieraddress [lindex $args 0] [lrange $args 1 [expr $len - 1]]
+		}
+	} 
 }
 
 
 Simulator instproc expand-port-field-bits nbits {
-    set a [$self get-AllocAddrBits "notnew"]
-    if {$nbits <= [$a set portsize_]} {
-	error "expand-port-field-bits:requested portsize less than or equal to existing portsize"
-    } 
+	set a [$self get-AllocAddrBits "notnew"]
+	if {$nbits <= [$a set portsize_]} {
+		error "expand-port-field-bits:requested portsize less than or equal to existing portsize"
+	} 
 
-    $a expand-portbits $nbits
+	$a expand-portbits $nbits
 }
 
 
@@ -171,21 +171,21 @@ Simulator instproc expand-port-field-bits nbits {
 # this is true for both set-address and set-hieraddress
 
 Simulator instproc set-address {node port} {
-    set a [$self get-AllocAddrBits "new"]
-    $a set size_ [AllocAddrBits set DEFADDRSIZE_]
-    if {[expr $node + $port] > [$a set size_]} {
-	$a set size_ [AllocAddrBits set MAXADDRSIZE_]
-    }
+	set a [$self get-AllocAddrBits "new"]
+	$a set size_ [AllocAddrBits set DEFADDRSIZE_]
+	if {[expr $node + $port] > [$a set size_]} {
+		$a set size_ [AllocAddrBits set MAXADDRSIZE_]
+	}
 	
-    # one bit is set aside for mcast as default :: this waste of 1 bit may be avoided, if 
-    # mcast option is enabled before the initialization of Simulator.
-    #     if [Simulator set EnableMcast_] {
-    $a set-mcastbits 1
-    #     }
+	# one bit is set aside for mcast as default :: this waste of 1 bit may be avoided, if 
+	# mcast option is enabled before the initialization of Simulator.
+	#     if [Simulator set EnableMcast_] {
+	$a set-mcastbits 1
+	#     }
 
-    set lastbit [expr $node - [$a set mcastsize_]]
-    $a set-idbits 1 $lastbit
-    $a set-portbits $port
+	set lastbit [expr $node - [$a set mcastsize_]]
+	$a set-idbits 1 $lastbit
+	$a set-portbits $port
 
 }
 
@@ -195,191 +195,191 @@ Simulator instproc set-address {node port} {
 #sets hierarchical bits
 
 Simulator instproc set-hieraddress {hlevel args} {
-    set a [$self get-AllocAddrBits "new"]
-    $a set size_ [AllocAddrBits set MAXADDRSIZE_]
-    if ![Simulator set EnableHierRt_] {
-	Simulator set EnableHierRt_ 1
-    }
-    if [Simulator set EnableMcast_] {
-	$a set-mcastbits 1
-    }
-    eval $a set-idbits $hlevel $args
-    $a set-portbits 8 
+	set a [$self get-AllocAddrBits "new"]
+	$a set size_ [AllocAddrBits set MAXADDRSIZE_]
+	if ![Simulator set EnableHierRt_] {
+		Simulator set EnableHierRt_ 1
+	}
+	if [Simulator set EnableMcast_] {
+		$a set-mcastbits 1
+	}
+	eval $a set-idbits $hlevel $args
+	$a set-portbits 8 
 }
 
 
 
 
 AllocAddrBits instproc init {} {
-    eval $self next
-    $self instvar size_ portsize_ idsize_ mcastsize_
-    set size_ 0
-    set portsize_ 0
-    set idsize_ 0
-    set mcastsize_ 0
+	eval $self next
+	$self instvar size_ portsize_ idsize_ mcastsize_
+	set size_ 0
+	set portsize_ 0
+	set idsize_ 0
+	set mcastsize_ 0
 }
 
 
 AllocAddrBits instproc get-AllocAddr {} {
-    $self instvar addrStruct_
-    if ![info exists addrStruct_] {
-	set addrStruct_ [new AllocAddr]
-    }
-    return $addrStruct_
+	$self instvar addrStruct_
+	if ![info exists addrStruct_] {
+		set addrStruct_ [new AllocAddr]
+	}
+	return $addrStruct_
 }
 
 AllocAddrBits instproc get-Address {} {
-    $self instvar address_
-    if ![info exists address_] {
-	set address_ [new Address]
-    }
-    return $address_
+	$self instvar address_
+	if ![info exists address_] {
+		set address_ [new Address]
+	}
+	return $address_
 }
 
 
 AllocAddrBits instproc chksize {bit_num prog} {
-    $self instvar size_ portsize_ idsize_ mcastsize_  
-    if {$bit_num <= 0 } {
-	error "$prog : \# bits less than 1"
-    }
-    set totsize [expr $portsize_ + $idsize_ + $mcastsize_]
-    if {$totsize > [AllocAddrBits set MAXADDRSIZE_]} {
-	error "$prog : Total \# bits exceed MAXADDRSIZE"
-    }
-    if { $size_ < [AllocAddrBits set MAXADDRSIZE_]} {
-	if {$totsize > [AllocAddrBits set DEFADDRSIZE_]} {
-	    set size_ [AllocAddrBits set MAXADDRSIZE_]
-	    return 1
-	} 
-    }
-    return 0
+	$self instvar size_ portsize_ idsize_ mcastsize_  
+	if {$bit_num <= 0 } {
+		error "$prog : \# bits less than 1"
+	}
+	set totsize [expr $portsize_ + $idsize_ + $mcastsize_]
+	if {$totsize > [AllocAddrBits set MAXADDRSIZE_]} {
+		error "$prog : Total \# bits exceed MAXADDRSIZE"
+	}
+	if { $size_ < [AllocAddrBits set MAXADDRSIZE_]} {
+		if {$totsize > [AllocAddrBits set DEFADDRSIZE_]} {
+			set size_ [AllocAddrBits set MAXADDRSIZE_]
+			return 1
+		} 
+	}
+	return 0
 
 }
 
 
 
 AllocAddrBits instproc set-portbits {bit_num} {
-    $self instvar size_ portsize_ 
-    set portsize_ $bit_num
-    if [$self chksize portsize_ "setport"] {
-	error "set-portbits: size_ has been changed."
-    }
-    
-    set a [$self get-AllocAddr] 
-    set v [$a setbit $bit_num $size_]
-    
-    AddrParams set PortMask_ [lindex $v 0]
-    AddrParams set PortShift_ [lindex $v 1]
-    ### TESTING
-    # set mask [lindex $v 0]
-    # set shift [lindex $v 1]
-    # puts "Portshift = $shift \n PortMask = $mask\n"
+	$self instvar size_ portsize_ 
+	set portsize_ $bit_num
+	if [$self chksize portsize_ "setport"] {
+		error "set-portbits: size_ has been changed."
+	}
+	
+	set a [$self get-AllocAddr] 
+	set v [$a setbit $bit_num $size_]
+	
+	AddrParams set PortMask_ [lindex $v 0]
+	AddrParams set PortShift_ [lindex $v 1]
+	### TESTING
+	# set mask [lindex $v 0]
+	# set shift [lindex $v 1]
+	# puts "Portshift = $shift \n PortMask = $mask\n"
 
-    set ad [$self get-Address]
-    $ad portbits-are [AddrParams set PortShift_] [AddrParams set PortMask_]
+	set ad [$self get-Address]
+	$ad portbits-are [AddrParams set PortShift_] [AddrParams set PortMask_]
 }
 
 
 
 AllocAddrBits instproc expand-portbits nbits {
-    $self instvar portsize_ size_ mcastsize_ idsize_ hlevel_ hbits_ addrStruct_
-    set temp $portsize_
-    set portsize_ $nbits
-    
-    # these extra checks and re-allocation are done to ensure mcast bit is always the
-    # MSB; so when size_ gets expanded as a result of expanding port, need to re-allocate 
-    # mcast and id bits.
-    
-    if [$self chksize $nbits "expandport"] {
-	set addrStruct_ [new AllocAddr]
-	if {$mcastsize_ > 0} {
-	    $self set-mcastbits $mcastsize_
+	$self instvar portsize_ size_ mcastsize_ idsize_ hlevel_ hbits_ addrStruct_
+	set temp $portsize_
+	set portsize_ $nbits
+	
+	# these extra checks and re-allocation are done to ensure mcast bit is always the
+	# MSB; so when size_ gets expanded as a result of expanding port, need to re-allocate 
+	# mcast and id bits.
+	
+	if [$self chksize $nbits "expandport"] {
+		set addrStruct_ [new AllocAddr]
+		if {$mcastsize_ > 0} {
+			$self set-mcastbits $mcastsize_
+		}
+		if {$idsize_ > 0} {
+			eval $self set-idbits $hlevel_ $hbits_ 
+		}
+		$self set-portbits $nbits
+	} else {
+		set a [$self get-AllocAddr] 
+		set v [$a expand-port [expr $nbits - $temp] $size_ $temp ]
+		AddrParams set PortMask_ [lindex $v 0]
+		AddrParams set PortShift_ [lindex $v 1]
+		set ad [$self get-Address]
+		$ad portbits-are [AddrParams set PortShift_] [AddrParams set PortMask_]
+		### TESTING
+		# set mask [lindex $v 0]
+		# set shift [lindex $v 1]
+		# puts "size = $size_\n Portshift = $shift \n PortMask = $mask\n"
 	}
-	if {$idsize_ > 0} {
-	    eval $self set-idbits $hlevel_ $hbits_ 
-	}
-	$self set-portbits $nbits
-    } else {
-	set a [$self get-AllocAddr] 
-	set v [$a expand-port [expr $nbits - $temp] $size_ $temp ]
-	AddrParams set PortMask_ [lindex $v 0]
-	AddrParams set PortShift_ [lindex $v 1]
-	set ad [$self get-Address]
-	$ad portbits-are [AddrParams set PortShift_] [AddrParams set PortMask_]
-	### TESTING
-	# set mask [lindex $v 0]
-	# set shift [lindex $v 1]
-	# puts "size = $size_\n Portshift = $shift \n PortMask = $mask\n"
-    }
 }
 
 
 
 AllocAddrBits instproc set-mcastbits {bit_num} {
-    $self instvar size_ mcastsize_
-    if {$bit_num != 1} {
-	error "setmcast : mcastbit > 1"
-    }
-    set mcastsize_ $bit_num
+	$self instvar size_ mcastsize_
+	if {$bit_num != 1} {
+		error "setmcast : mcastbit > 1"
+	}
+	set mcastsize_ $bit_num
 
-    #chk to ensure there;s no change in size
-    if [$self chksize mcastsize_ "setmcast"] {
-    # assert {$chk == 0} --> assert doesn't seem to work
-	error "set-mcastbits: size_ has been changed."
-    }
-    set a [$self get-AllocAddr] 
-    set v [$a setbit $bit_num $size_]
-    AddrParams set McastMask_ [lindex $v 0]
-    AddrParams set McastShift_ [lindex $v 1]
-    ### TESTING
-    # set mask [lindex $v 0]
-    # set shift [lindex $v 1]
-    # puts "Mcastshift = $shift \n McastMask = $mask\n"
+	#chk to ensure there;s no change in size
+	if [$self chksize mcastsize_ "setmcast"] {
+		# assert {$chk == 0} --> assert doesn't seem to work
+		error "set-mcastbits: size_ has been changed."
+	}
+	set a [$self get-AllocAddr] 
+	set v [$a setbit $bit_num $size_]
+	AddrParams set McastMask_ [lindex $v 0]
+	AddrParams set McastShift_ [lindex $v 1]
+	### TESTING
+	# set mask [lindex $v 0]
+	# set shift [lindex $v 1]
+	# puts "Mcastshift = $shift \n McastMask = $mask\n"
 
-    set ad [$self get-Address]
-    $ad mcastbits-are [AddrParams set McastShift_] [AddrParams set McastMask_]
+	set ad [$self get-Address]
+	$ad mcastbits-are [AddrParams set McastShift_] [AddrParams set McastMask_]
 
 }
 
 
 AllocAddrBits instproc set-idbits {nlevel args} {
-    $self instvar size_ portsize_ idsize_ hlevel_ hbits_
-    if {$nlevel != [llength $args]} {
-	error "setid: hlevel < 1 OR nlevel and \# args donot match"
-    }
-    set a [$self get-AllocAddr] 
-    set old 0
-    set idsize_ 0
-
-    for {set i 0} {$i < $nlevel} {incr i} {
-	set bpl($i) [lindex $args $i]
-	set idsize_ [expr $idsize_ + $bpl($i)]
-	
-	# check to ensure there's no change in size
-	set chk [$self chksize $bpl($i) "setid"]
-	# assert {$chk ==0}
-	if {$chk > 0} {
-	    error "set-mcastbits: size_ has been changed."
+	$self instvar size_ portsize_ idsize_ hlevel_ hbits_
+	if {$nlevel != [llength $args]} {
+		error "setid: hlevel < 1 OR nlevel and \# args donot match"
 	}
-	set v [$a setbit $bpl($i) $size_]
-     	AddrParams set NodeMask_([expr $i+1]) [lindex $v 0]
-	set m([expr $i+1]) [lindex $v 0]
-     	AddrParams set NodeShift_([expr $i+1]) [lindex $v 1]
-	set s([expr $i+1]) [lindex $v 1]
-	AddrParams set hlevel_ $nlevel
-	set hlevel_ $nlevel
-	lappend hbits_ $bpl($i)
-	set old [expr $old + $bpl($i)]
-    }
-    
-    set ad [$self get-Address]
-    eval $ad idsbits-are [array get s]
-    eval $ad idmbits-are [array get m]
-    
-    ### TESTING
-    # set mask [lindex $v 0]
-    # set shift [lindex $v 1]
-    # puts "Nodeshift = $shift \n NodeMask = $mask\n"
+	set a [$self get-AllocAddr] 
+	set old 0
+	set idsize_ 0
+
+	for {set i 0} {$i < $nlevel} {incr i} {
+		set bpl($i) [lindex $args $i]
+		set idsize_ [expr $idsize_ + $bpl($i)]
+		
+		# check to ensure there's no change in size
+		set chk [$self chksize $bpl($i) "setid"]
+		# assert {$chk ==0}
+		if {$chk > 0} {
+			error "set-mcastbits: size_ has been changed."
+		}
+		set v [$a setbit $bpl($i) $size_]
+		AddrParams set NodeMask_([expr $i+1]) [lindex $v 0]
+		set m([expr $i+1]) [lindex $v 0]
+		AddrParams set NodeShift_([expr $i+1]) [lindex $v 1]
+		set s([expr $i+1]) [lindex $v 1]
+		AddrParams set hlevel_ $nlevel
+		set hlevel_ $nlevel
+		lappend hbits_ $bpl($i)
+		set old [expr $old + $bpl($i)]
+	}
+	
+	set ad [$self get-Address]
+	eval $ad idsbits-are [array get s]
+	eval $ad idmbits-are [array get m]
+	
+	### TESTING
+	# set mask [lindex $v 0]
+	# set shift [lindex $v 1]
+	# puts "Nodeshift = $shift \n NodeMask = $mask\n"
 }
 
 
@@ -389,44 +389,44 @@ AllocAddrBits instproc set-idbits {nlevel args} {
 # create a real hier address from addr string 
 #
 AddrParams proc set-hieraddr addrstr {
-    AddrParams instvar hlevel_ NodeShift_ NodeMask_
-    set L [split $addrstr .]
-    if { [llength $L] != $hlevel_ } {
-	error "set-hieraddr: hierarchical address doesn't match with \# hier.levels\n"
-    }
-    set word 0
-    for {set i 1} {$i <= $hlevel_} {incr i} {
-	set word [expr [expr [expr [lindex $L [expr $i-1]] & $NodeMask_($i)] << $NodeShift_($i)] | [expr [expr ~[expr $NodeMask_($i) << $NodeShift_($i)]] & $word]]
-	#TESTING
-	# puts "word = $word"
-    }
-    return $word
+	AddrParams instvar hlevel_ NodeShift_ NodeMask_
+	set L [split $addrstr .]
+	if { [llength $L] != $hlevel_ } {
+		error "set-hieraddr: hierarchical address doesn't match with \# hier.levels\n"
+	}
+	set word 0
+	for {set i 1} {$i <= $hlevel_} {incr i} {
+		set word [expr [expr [expr [lindex $L [expr $i-1]] & $NodeMask_($i)] << $NodeShift_($i)] | [expr [expr ~[expr $NodeMask_($i) << $NodeShift_($i)]] & $word]]
+		#TESTING
+		# puts "word = $word"
+	}
+	return $word
 }
-	
 
 
 
-    
+
+
 #
 # Routine to churn up the number of elements at a given level, that is visible to 
 # a node.
 #
 AddrParams proc elements-in-level? {nodeaddr level} {
-    AddrParams instvar hlevel_ domain_num_ cluster_num_ nodes_num_
-    set L [split $nodeaddr .] 
-    set level [expr $level + 1]
-    ### for now, assuming only 3 levels of hierarchy 
-    if { $level == 1} {
-	return $domain_num_
-    }
-    if { $level == 2} {
-	return [lindex $cluster_num_ [lindex $L 0]]
-    }
-    if { $level == 3} {
-	set cluster [lindex $cluster_num_ [lindex $L 0]]
-	set funct [expr [lindex $L 0] * $cluster + [lindex $L 1]]
-	return [lindex $nodes_num_ $funct]
-    } 
+	AddrParams instvar hlevel_ domain_num_ cluster_num_ nodes_num_
+	set L [split $nodeaddr .] 
+	set level [expr $level + 1]
+	### for now, assuming only 3 levels of hierarchy 
+	if { $level == 1} {
+		return $domain_num_
+	}
+	if { $level == 2} {
+		return [lindex $cluster_num_ [lindex $L 0]]
+	}
+	if { $level == 3} {
+		set cluster [lindex $cluster_num_ [lindex $L 0]]
+		set funct [expr [lindex $L 0] * $cluster + [lindex $L 1]]
+		return [lindex $nodes_num_ $funct]
+	} 
 	
 }
 
@@ -437,28 +437,28 @@ AddrParams proc elements-in-level? {nodeaddr level} {
 # Given an node's address, Return the node 
 #
 Simulator instproc get-node address {
-    $self instvar Node_
-    set n [Node set nn_]
-    for {set q 0} {$q < $n} {incr q} {
-	set nq $Node_($q)
-	if {[string compare [$nq node-addr] $address] == 0} {
-	    return $nq
+	$self instvar Node_
+	set n [Node set nn_]
+	for {set q 0} {$q < $n} {incr q} {
+		set nq $Node_($q)
+		if {[string compare [$nq node-addr] $address] == 0} {
+			return $nq
+		}
 	}
-    }
-    error "get-node:Cannot find node with given address"
+	error "get-node:Cannot find node with given address"
 }
 
 #
 # Given an node's address, Return the node-id
 #
 Simulator instproc get-node-id address {
-$self instvar Node_
-    set n [Node set nn_]
-    for {set q 0} {$q < $n} {incr q} {
-	set nq $Node_($q)
-	if {[string compare [$nq node-addr] $address] == 0} {
-	    return $q
+	$self instvar Node_
+	set n [Node set nn_]
+	for {set q 0} {$q < $n} {incr q} {
+		set nq $Node_($q)
+		if {[string compare [$nq node-addr] $address] == 0} {
+			return $q
+		}
 	}
-    }
-    error "get-node-id:Cannot find node with given address"
+	error "get-node-id:Cannot find node with given address"
 }
