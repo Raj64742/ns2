a3fdae9c23ec7b7045560638094c82a7ab318a5d
bajaj <bajaj>
1998-05-07 20:49:08 +0000

    Thu May  7 13:49:44 PDT 1998 Added timestamp in adc.cc Modified Files:  	adc.cc

--- adc/adc.cc
+++ adc/adc.cc
@@ -16,6 +16,10 @@
  * These notices must be retained in any copies of any part of this
  * software. 
  */
+#ifndef lint
+static const char rcsid[] =
+	"@(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/adc/adc.cc,v 1.3 1998/05/07 20:49:08 bajaj Exp $";
+#endif
 
 #include "adc.h"
 #include <stdlib.h>
