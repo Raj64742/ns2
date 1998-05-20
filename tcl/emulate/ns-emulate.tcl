545d275da173618be9e87b7dcb30ac23ba899048
kfall <kfall>
1998-05-20 22:43:06 +0000

    add default for maxpkt_

--- tcl/emulate/ns-emulate.tcl
+++ tcl/emulate/ns-emulate.tcl
@@ -1,10 +1,14 @@
 #
 # ns-emulate.tcl
-#	configuration for ns emulation
+#	defaults for nse [ns with emulation support]
 #
 if [TclObject is-class Network/Pcap/Live] {
-	Network/Pcap/Live set snaplen_ 4096
-	Network/Pcap/Live set promisc_ false
+	Network/Pcap/Live set snaplen_ 4096;# bpf snap len
+	Network/Pcap/Live set promisc_ false;
 	Network/Pcap/Live set timeout_ 0
-	Network/Pcap/Live set optimize_ true
+	Network/Pcap/Live set optimize_ true;# bpf code optimizer
+}
+
+if [TclObject is-class Agent/Tap]
+	Agent/Tap set maxpkt_ 1600
 }
