605da6c20b53aad152328ab8fe09d90646e5fd27
yaxu <yaxu>
1998-07-01 21:20:28 +0000

    restore the version back to 1.15

--- trace.h
+++ trace.h
@@ -1,3 +1,4 @@
+/* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
 /*
  * Copyright (c) 1997 Regents of the University of California.
  * All rights reserved.
@@ -30,7 +31,7 @@
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  *
- * @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/Attic/trace.h,v 1.16 1998/07/01 18:19:18 yaxu Exp $
+ * @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/Attic/trace.h,v 1.17 1998/07/01 21:20:28 yaxu Exp $
  */
 
 #ifndef ns_trace_h
@@ -75,7 +76,6 @@ class Trace : public Connector {
 	int off_ip_;
 	int off_tcp_;
 	int off_rtp_;
-	int off_srm_;
 };
 
 class DequeTrace : public Trace {
