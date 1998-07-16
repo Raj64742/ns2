68d91e2a09ef218193738ddc4c3e3bd82541ea01
polly <polly>
1998-07-16 18:22:07 +0000

    Polly Huang Thu Jul 16 11:21:00 PDT 1998 minor change to hierarchical session sim test file

--- tcl/test/session-hier-1000.tcl
+++ tcl/test/session-hier-1000.tcl
@@ -10,6 +10,26 @@ $ns namtrace-all [open session-hier-1000.nam w]
 
 create-hier-topology $linkBW
 
+
+set cbr [new Agent/CBR]
+$ns attach-agent $n(500) $cbr
+$cbr set dst_ 0x8002
+$ns create-session $n(500) $cbr
+
+set rcvr0 [new Agent/LossMonitor]
+set rcvr1 [new Agent/LossMonitor]
+set rcvr2 [new Agent/LossMonitor]
+
+$ns attach-agent $n(0) $rcvr0
+$ns attach-agent $n(1) $rcvr1
+$ns attach-agent $n(2) $rcvr2
+
+$ns at 0.2 "$n(0) join-group $rcvr0 0x8002"
+$ns at 0.2 "$n(1) join-group $rcvr1 0x8002"
+$ns at 0.2 "$n(2) join-group $rcvr2 0x8002"
+
+$ns at 1.0 "$cbr start"
+
 $ns at 3.0 "finish"
 proc finish {} {
     global ns
