3827e6688f96ff74f2771343c05bb8b0232dc271
haoboy <haoboy>
1998-07-22 00:48:46 +0000

    Changed old attach-nam to namattach.

--- tcl/lan/ns-lan.tcl
+++ tcl/lan/ns-lan.tcl
@@ -137,7 +137,7 @@ Link/LanDuplex instproc trace {ns f} {
 	set drpT_ [$ns create-trace Loss $f $toNode_ $fromNode_]
 	set namtrfd [$ns get-nam-traceall]
 	if {$namtrfd != ""} {
-		$drpT_ attach-nam $namtrfd
+		$drpT_ namattach $namtrfd
 	}
 	$link_ drop-target $drpT_
 }
