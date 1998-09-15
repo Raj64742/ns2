79d797a19547c83a1f9f5eec62ccfdd8553eefd5
kfall <kfall>
1998-09-15 02:25:47 +0000

    extended with a delay-sensing receiver (VegasLike). Only change is that if actualrate < expectedrate, you can reduce your sending rate by 1 pkt per rtt.

--- tfcc.cc
+++ tfcc.cc
@@ -33,7 +33,7 @@
 
 #ifndef lint
 static const char rcsid[] =
-    "@(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/Attic/tfcc.cc,v 1.6 1998/09/15 01:32:54 kfall Exp $";
+    "@(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/Attic/tfcc.cc,v 1.7 1998/09/15 02:25:47 kfall Exp $";
 #endif
 
 /* tfcc.cc -- TCP-friently congestion control protocol */
@@ -57,15 +57,14 @@ struct hdr_tfcc {
 	double ts_echo_;	// echo'd ts (for rtt estimates)
 	double interval_;	// sender's sending interval
 	int cong_seq_;		// congestion sequence number
-	int dataful_;		// packet contains data
+	int nlost_;		// total packets lost
 
 	// per-var methods
 	double& rttest() { return (rttest_); }
 	double& ts() { return (ts_); }
 	double& ts_echo() { return (ts_echo_); }
-	double& interval() { return (interval_); }
 	int& cseq() { return (cong_seq_); }
-	int& dataful() { return (dataful_); }
+	int& nlost() { return (nlost_); }
 
 	static int offset_;	// offset of tfcc header
 };
@@ -92,7 +91,7 @@ class TFCCAgent : public RTPAgent {
 public:
 	TFCCAgent() : srtt_(-1.0), rttvar_(-1.0), peer_rtt_est_(-1.0),
 	last_ts_(-1.0), last_loss_time_(-1.0), last_rtime_(-1.0),
-	expected_(0), cseq_(0), highest_cseq_seen_(-1),
+	expected_(0), nlost_(0), plost_(0), cseq_(0), highest_cseq_seen_(-1),
 	last_cseq_checked_(-1), needresponse_(0), last_ecn_(0),
 	ack_timer_(this), rtt_timer_(this) {
 		bind("alpha_", &alpha_);
@@ -138,6 +137,8 @@ protected:
 	double last_rtime_;	// last time a pkt was received
 
 	int expected_;		// next expected sequence number
+	int nlost_;		// # pkts lost (cumulative)
+	int plost_;		// # pkts peer reported lost
 	int cseq_;		// congest epoch seq # (as receiver)
 	int highest_cseq_seen_;	// peer's last cseq seen (I am sender)
 	int last_cseq_checked_;	// last cseq value at rtt heartbeat
@@ -151,7 +152,7 @@ protected:
 	public: 
 		TFCCAckTimer(TFCCAgent *a) : TimerHandler() { a_ = a; }
 	protected:      
-		void expire(Event *e) { a_->ack_timeout(this); }
+		void expire(Event *) { a_->ack_timeout(this); }
 		TFCCAgent *a_;
 	} ack_timer_;	// periodic timer for ack generation
 
@@ -159,7 +160,7 @@ protected:
 	public: 
 		TFCCRttTimer(TFCCAgent *a) : TimerHandler() { a_ = a; }
 	protected:
-		void expire(Event *e) { a_->rtt_timeout(this); }
+		void expire(Event *) { a_->rtt_timeout(this); }
 		TFCCAgent *a_;
 	} rtt_timer_; // periodic rtt-based heartbeat
 };
@@ -205,10 +206,10 @@ TFCCAgent::makepkt(Packet* p)
 
 	th->ts_echo() = last_ts_;
 	th->ts() = now();
-	th->interval() = interval_;
+//th->interval() = interval_;
+	th->nlost() = nlost_;
 	th->rttest() = srtt_;
 	th->cseq() = cseq_;
-	th->dataful() = running_;
 
 	fh->ecnecho() = last_ecn_;
 	fh->ect() = 1;
@@ -260,6 +261,7 @@ printf("%f: %s: recv: seq: %d, cseq: %d, expect:%d\n", now(), name(),
 
 	int loss = 0;
 	if (expected_ >= 0 && (loss = rh->seqno() - expected_) > 0) {
+		nlost_ += loss;
 		loss_event(loss);
 		expected_ = rh->seqno() + 1;
 	} else
@@ -282,6 +284,9 @@ printf("%f: %s: recv: seq: %d, cseq: %d, expect:%d\n", now(), name(),
 		// look at receiver's congestion report
 		highest_cseq_seen_ = th->cseq();
 	}
+
+	if (th->nlost() > plost_)
+		plost_ = th->nlost();	// cumulative
 
         last_rtime_ = now();
         Packet::free(pkt);
@@ -455,18 +460,19 @@ TFCCAgent::ack_timeout(TimerHandler* timer)
 	timer->resched(ack_interval_);
 }
 
-#ifdef notyet
 /******************** EXTENSIONS ****************************/
-
-// VTFCC -- "VegasLike" TFCC
-class VTFCC : public TFCC {
+// VTFCC -- "VegasLike" TFCC (not finished)
+class VTFCCAgent : public TFCCAgent {
 public:
-	VTFCC()  { }
+	VTFCCAgent() : lastseq_(0), lastlost_(0) { }
 protected:
 	void slowdown();
+	void linear_slowdown();
 	void speedup();
-	void peer_rtt_known(double rtt);
-	void rtt_known();
+	void rtt_known(double rtt);
+
+	int lastseq_;
+	int lastlost_;
 
 	double expectedrate_;
 	double actualrate_;
@@ -474,16 +480,68 @@ protected:
 	double upperthresh_;
 };
 
-VTFCC::slowdown()
+static class VTFCCAgentClass : public TclClass {
+public:
+	VTFCCAgentClass() : TclClass("Agent/RTP/TFCC/VegasLike") {}
+	TclObject* create(int, const char*const*) {
+		return (new VTFCCAgent());
+	}
+} class_vtfcc_agent;
+
+void
+VTFCCAgent::rtt_known(double rtt)
 {
+	expectedrate_ = 1.0 / interval_;
+	TFCCAgent::rtt_known(rtt);
 }
 
-VTFCC::speedup()
+void
+VTFCCAgent::slowdown()
 {
+	if (running_) {
+		interval_ *= 2.0;
+printf("%s V-SLOWDOWN [srtt: %f], new interval:%f, ppw:%d\n",
+	name(), srtt_, interval_, int(srtt_ / interval_));
+		rate_change();
+		expectedrate_ = srtt_ / (interval_ * minrtt_);
+	}
 }
 
-VTFCC::speedup()
+void
+VTFCCAgent::linear_slowdown()
 {
+	if (running_) {
+		interval_ = (srtt_ * interval_) / (srtt_ + interval_);
+printf("%s V-MILDSLOWDOWN [srtt: %f], new interval:%f, ppw:%d\n",
+	name(), srtt_, interval_, int(srtt_ / interval_));
+		rate_change();
+		expectedrate_ = srtt_ / (interval_ * minrtt_);
+	}
 }
 
-#endif
+void
+VTFCCAgent::speedup()
+{
+	if (running_) {
+		//
+		//  we might thing we want to speed up
+		// because we got no drops, but in Vegas, we might
+		// want to slow down still
+		//
+		double actualrate = ((seqno_ - lastseq_) -
+			(plost_ - lastlost_)) / srtt_;
+		lastlost_ = plost_;	// not quite right XXX
+		lastseq_ = seqno_;
+		if (actualrate < expectedrate_) {
+			linear_slowdown();
+			return;
+		}
+
+		// otherwise, increase by 1
+		interval_ = (srtt_ * interval_) / (srtt_ + interval_);
+printf("%s V-SPEEDUP [srtt:%f], new interval:%f, ppw:%d\n",
+	name(), srtt_, interval_, int(srtt_ / interval_));
+		rate_change();
+		expectedrate_ = srtt_ / (interval_ * minrtt_);
+	}
+}
