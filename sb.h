c9de4a857417328383e8caf6f3fdd0e6b9dcde4a
sfloyd <sfloyd>
1998-11-28 03:23:52 +0000

    I was unable to "make" without this file, so I got this from the contributed code web page.

---
+++ sb.h
@@ -0,0 +1,46 @@
+/*
+ * (c) 1997 StarBurst Communications Inc.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * Author: Christoph Haenle, chris@cs.vu.nl
+ * File: sb.h
+ * Last change: Nov. 14, 1997
+ *
+ * This software may freely be used only for non-commercial purposes
+ */
+
+#ifndef sb_h
+#define sb_h
+
+
+typedef void            sb_void;
+typedef unsigned        sb_unsigned;
+typedef char            sb_char;
+typedef char            sb_int8;
+typedef unsigned char   sb_uchar;
+typedef unsigned char   sb_uint8;
+typedef int             sb_int;
+typedef int             sb_long;
+typedef unsigned int    sb_ulong;
+typedef unsigned int    sb_uint;
+typedef short           sb_short;
+typedef short           sb_int16;
+typedef unsigned short  sb_ushort;
+typedef unsigned short  sb_uint16;
+typedef long            sb_int32;
+typedef unsigned long   sb_uint32;
+typedef double          sb_double;
+
+
+#endif
