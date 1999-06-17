7a54aaf8e0bdb45b32096ea59012482e73b00294
salehi <salehi>
1999-06-17 02:06:23 +0000

    Added copyright

--- doc/post-process.pl
+++ doc/post-process.pl
@@ -1,3 +1,24 @@
+# -*-	Mode:Perl; tab-width:8; indent-tabs-mode:t -*- 
+#
+# post-process.pl
+# Copyright (C) 1999 by USC/ISI
+# All rights reserved.                                            
+#                                                                
+# Redistribution and use in source and binary forms are permitted
+# provided that the above copyright notice and this paragraph are
+# duplicated in all such forms and that any documentation, advertising
+# materials, and other materials related to such distribution and use
+# acknowledge that the software was developed by the University of
+# Southern California, Information Sciences Institute.  The name of the
+# University may not be used to endorse or promote products derived from
+# this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+# 
+# Contributed by Nader Salehi (USC/ISI), http://www.isi.edu/~salehi
+# 
 #!/local/bin/perl
 
 die "usage: $0 working-directory\n" if ($#ARGV < 0);
