6eceea50372f28e6c24cf7b454b79c3bc6049ba3
sfloyd <sfloyd>
1999-07-10 15:26:51 +0000

    Adding parameter "bval" to the TCP-friendly formulas.

--- tcp/formula.h
+++ tcp/formula.h
@@ -1,13 +1,15 @@
-#define B 1
 #define MAXRATE 125000000.0 
 
-static double p_to_b(double p, double rtt, double tzero, int psize) 
+static double p_to_b(double p, double rtt, double tzero, int psize, int bval) 
 {
-	//equation from Padhye, Firoiu, Towsley and Kurose, Sigcomm 98
-	//ignoring the term for rwin limiting
+	/*
+	 * equation from Padhye, Firoiu, Towsley and Kurose, Sigcomm 98
+	 * ignoring the term for rwin limiting
+	 */
+
 	double tmp1, tmp2, res;
-	res=rtt*sqrt(2*B*p/3);
-	tmp1=3*sqrt(3*B*p/8);
+	res=rtt*sqrt(2*bval*p/3);
+	tmp1=3*sqrt(3*bval*p/8);
 	if (tmp1>1.0) tmp1=1.0;
 	tmp2=tzero*p*(1+32*p*p);
 	res+=tmp1*tmp2;
@@ -16,8 +18,5 @@ static double p_to_b(double p, double rtt, double tzero, int psize)
 	} else {
 		res=psize/res;
 	}
-#ifdef DEBUF
-	printf("p:%f rtt:%f tzero:%f result:%f\n", p, rtt, tzero, res);
-#endif
 	return res;
 }
