4b4f1ae6e5cf52cc5ba47b9426a7f9a299d71255
kkumar <kkumar>
1999-07-22 00:38:40 +0000

    Self-configuring hierarchy construction protocol based on Landmark Hierarchy. Designed in the context of an object-tracking sensor network application.

--- sensor-nets/landmark.h
+++ sensor-nets/landmark.h
@@ -13,6 +13,8 @@
 #include <ll.h>
 #include <mac.h>
 #include <priqueue.h>
+#include <mobilenode.h>
+#include "tags.h"
 
 
 typedef double Time;
@@ -21,15 +23,41 @@ typedef double Time;
 
 #define NOT_CHILD 0
 #define IS_CHILD 1
+#define NOT_POTL_CHILD 2 // Not within appropriate vicinity
 
 #define NO_PARENT 60000
 
 #define OLD_ENTRY 0     // Object already exists in list
 #define NEW_ENTRY 1     // New object created in list
 #define OLD_MESSAGE 2   // Old information; Object not added to list
+#define ENTRY_NOT_FOUND 3 // Object not found
+#define IGNORE_ENTRY 4 // Node is not potential child;
+                       // returned by UpdatePotlChild
 
 #define TRUE 1
 #define FALSE 0
+
+//class TagCache {
+//public:
+//  int obj_name_;
+//  int origin_time_;
+//  double X_;
+//  double Y_;
+//};
+
+
+
+class NodeIDList {
+public:
+  NodeIDList() {
+    next_ = NULL;
+  }
+  nsaddr_t dst_node_;
+  nsaddr_t dst_next_hop_;
+  int next_hop_level_;
+  NodeIDList *next_;
+};
+
 
 class LMNode {
   friend class ParentChildrenList;
@@ -46,8 +74,20 @@ public:
     energy_ = energy;
     last_upd_origin_time_ = origin_time;
     last_update_rcvd_ = update_time;
+    tag_list_ = NULL;
     next_ = NULL;
   }
+  
+  ~LMNode() {
+    compr_taglist *tag_ptr1, *tag_ptr2;
+    tag_ptr1 = tag_list_;
+    while(tag_ptr1) {
+      tag_ptr2 = tag_ptr1;
+      tag_ptr1 = tag_ptr1->next_;
+      delete tag_ptr2;
+    }
+  }
+
 private:
   nsaddr_t id_;             // ID of this node
   nsaddr_t next_hop_;	    // Next hop to reach this node
@@ -57,6 +97,7 @@ private:
   int energy_;              // Energy reserves of the child node
   int last_upd_origin_time_; // Origin time of last update
   double last_update_rcvd_; // Time at which last update received
+  compr_taglist *tag_list_;  // Tag advertisements of this node
   LMNode *next_;     
 };
 
@@ -79,8 +120,12 @@ public:
 private:
   int level_;          // my level
   LMNode *parent_;    // points to the appropriate object in the pparent_ list
+  int num_heard_;     // number of nodes heard at one level lower than 
+                      // our level; superset of potential children heard
+                      // with the "election" vicinity condition
   int num_children_;   // number of children
   int num_potl_children_; // number of potential children
+  int num_pparent_;     // number of potential parents			       
   LMNode *pchildren_;  // list of children and potential children
   LMNode *pparent_;   // list of potential parents			    
 
@@ -104,9 +149,14 @@ private:
   // One method might be enough but have two in case different
   // actions have to be taken for adding parent and child in the future
   int UpdatePotlParent(nsaddr_t id, nsaddr_t next_hop, int num_hops, int level, int energy, int origin_time, int delete_flag);
-  int UpdatePotlChild(nsaddr_t id, nsaddr_t next_hop, int num_hops, int level, int energy, int origin_time, int child_flag, int delete_flag);
+  int UpdatePotlChild(nsaddr_t id, nsaddr_t next_hop, int num_hops, int level, int energy, int origin_time, int child_flag, int delete_flag, compr_taglist *taglist);
 
   LandmarkAgent *a_; // Agent associated with this object
+  compr_taglist *tag_list_; // Aggregated list of tags for each level	     
+  int num_tags_;            // Number of tags in tag_list	       
+
+  int adverts_type_;  // Indicates whether adverts should be flooded, unicast
+                  // or suppresed when no changes as in a hard-state scheme
 };
 
 
@@ -151,7 +201,8 @@ protected:
   void Addpotentialchild(const nsaddr_t child, int level);
 
   // pkt_type is one of HIER_ADVS, OBJECT_ADVS, HIER_AND_OBJECT_ADVS
-  Packet *makeUpdate(ParentChildrenList *pcl, int pkt_type); 
+  // action is one of DEMOTION, PERIODIC_ADVERTS
+  Packet *makeUpdate(ParentChildrenList *pcl, int pkt_type, int action); 
                                   
   int radius(int level); // returns the LM radius for the specified level
 
@@ -161,9 +212,21 @@ protected:
   void ProcessHierUpdate(Packet *p);
   void ForwardPacket(Packet *p);
 
+  // Prints neighbour information for this node
+  void get_nbrinfo();
+
   // Tracing stuff
   void trace(char* fmt,...);       
   Trace *tracetarget_;  // Trace target
+
+  // Pointer to global tag database
+  tags_database *tag_dbase_;
+  compr_taglist *aggregate_taginfo(compr_taglist *unagg_tags, int agg_level, int *num_tags);
+  NodeIDList *search_tag(int obj_name, int prev_hop_level, int next_hop_level, nsaddr_t last_hop_id, int *num_dst);
+  nsaddr_t get_next_hop(nsaddr_t dst, int next_hop_level);
+
+  // Mobile node to which agent is attached; Used to get position information
+  MobileNode *node_;
 
   // Randomness/MAC/logging parameters
   int be_random_;    // set to 1 on initialization
@@ -173,10 +236,29 @@ protected:
                     // other LM hierarchy messages
   double total_wait_time_; // total time the node has spent in wait state
 
-  // Debug flag
+  // Debug flags
   int debug_;	  
+  int qry_debug_;
 
+  // Tag cache info
+  int cache_;      // set to 1 to enable caching
+  TagCache *tag_cache_;
+  int num_cached_items_;
+
+  // Update period info
+  double update_period_;
+  double update_timeout_;
+
+  // Option to indicate whether updates are to be flooded, unicast or
+  // suppressed when no changes occur as in a hard-state based scheme
+  int adverts_type_;  
+
+  // Random number generator
+  RNG *rn_;
+  inline double jitter(double max, int be_random_);
+  inline double random_timer(double max, int be_random_);
 };
+
 
 
 class LMPeriodicAdvtHandler : public Handler {
