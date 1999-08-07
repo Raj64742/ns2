f69ee9f8e9b89ef96d0682078bac08591e848cc4
yaxu <yaxu>
1999-08-07 21:18:38 +0000

    definition of location object prototype

---
+++ common/location.h
@@ -0,0 +1,37 @@
+#ifndef location_h_
+#define location_h_
+
+#include <assert.h>
+#include <tclcl.h>
+
+class Location : public TclObject {
+
+public:
+  Location(double x, double y, double z) {
+    X = x;
+    Y = y;
+    Z = z;
+  }
+  virtual void setLocation(double x, double y, double z) {
+
+    X = x;
+    Y = y;
+    Z = z;
+  }
+
+  virtual void getLocation(double *x, double *y, double *z) {
+    update_location();
+    *x = X;
+    *y = Y;
+    *z = Z;
+  }
+
+  virtual void update_location(){}
+
+ protected:
+  double X,Y,Z;        // 3-D coordinates
+
+};
+
+#endif
+
