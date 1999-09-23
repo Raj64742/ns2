54d6c8837a8dbf52a1154aabe945f4ba32523956
yaxu <yaxu>
1999-09-23 03:14:13 +0000

    new dsr test suite and test output to hide the float point computation difference b/w diff systems

--- tcl/test/test-suite-wireless-lan.tcl
+++ tcl/test/test-suite-wireless-lan.tcl
@@ -246,9 +246,47 @@ Test/dsr instproc init {} {
 
     $ns_ at $opt(stop).000000001 "puts \"NS EXITING...\" ;"
     #$ns_ halt"
-    $ns_ at $opt(stop).1 "$self finish"
+    $ns_ at $opt(stop).1 "$self finish-dsr"
 }
 
+TestSuite instproc finish-dsr {} {
+	$self instvar ns_
+	global quiet opt
+
+	$ns_ flush-trace
+        
+        set tracefd	[open $opt(tr) r]
+        set tracefd2    [open $opt(tr).w w]
+
+        while { [eof $tracefd] == 0 } {
+
+	    set line [gets $tracefd]
+	    set items [split $line " "]
+
+	    set time [lindex $items 1]
+	    
+	    set times [split $time "."]
+	    set time1 [lindex $times 0]
+	    set time2 [lindex $times 1]
+	    set newtime2 [string range $time2 0 4]
+	    set time $time1.$newtime2
+	    
+	    set newline [lreplace $line 1 1 $time] 
+
+	    puts $tracefd2 $newline
+
+	}
+	
+	close $tracefd
+	close $tracefd2
+
+	exec mv $opt(tr).w $opt(tr)
+	
+	puts "finishing.."
+	exit 0
+	
+
+}
 
 
 Test/dsr instproc run {} {
