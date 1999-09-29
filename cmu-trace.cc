0ad3c0ec50c998a7e6d4445c90b8fdabca3887b1
yaxu <yaxu>
1999-09-29 18:58:11 +0000

    bug-fix for energy tracing

--- cmu-trace.cc
+++ cmu-trace.cc
@@ -129,7 +129,7 @@ CMUTrace::format_mac(Packet *p, const char *why, int offset)
 #ifdef LOG_POSITION
 		"%c %.9f %d (%6.2f %6.2f) %3s %4s %d %s %d [%x %x %x %x] ",
 #else
-		"%c %.9f _%d_ %3s %4s %d %s %d [%x %x %x %x] [energy %f]",
+		"%c %.9f _%d_ %3s %4s %d %s %d [%x %x %x %x] [energy %f] ",
 #endif
 		op,
 		Scheduler::instance().clock(),
--- mobile/energy-model.h
+++ mobile/energy-model.h
@@ -19,7 +19,7 @@ class EnergyModel : public TclObject {
 public:
   EnergyModel(double energy) { energy_ = energy; }
   inline double energy() { return energy_; }
-  inline void setenergy(double e) {energy_ = 0;}
+  inline void setenergy(double e) {energy_ = e;}
   virtual void DecrTxEnergy(double txtime, double P_tx) {
     energy_ -= (P_tx * txtime);
   }
