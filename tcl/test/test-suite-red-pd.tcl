edc8829733d889c45b9a4b2543f6b5ca8a791fe9
ratul <ratul>
2000-11-21 05:51:12 +0000

    First complete version

--- tcl/test/test-suite-red-pd.tcl
+++ tcl/test/test-suite-red-pd.tcl
@@ -38,6 +38,9 @@ source misc_simple.tcl
 catch "cd $dir"
 Agent/TCP set oldCode_ true
 
+source ../red-pd/monitoring.tcl
+source ../red-pd/helper.tcl
+
 set flowfile fairflow.tr; # file where flow data is written
 set flowgraphfile fairflow.xgr; # file given to graph tool 
 
@@ -76,7 +79,7 @@ Topology instproc node? num {
 
 Class Topology/net2 -superclass Topology
 Topology/net2 instproc init ns {
-    $self instvar node_ redpdflowmon_ redpdq_
+    $self instvar node_ redpdflowmon_ redpdq_ redpdlink_
     set node_(s1) [$ns node]
     set node_(s2) [$ns node]    
     set node_(r1) [$ns node]    
@@ -88,7 +91,8 @@ Topology/net2 instproc init ns {
 
     $ns duplex-link $node_(s1) $node_(r1) 10Mb 2ms DropTail
     $ns duplex-link $node_(s2) $node_(r1) 10Mb 3ms DropTail
-    $ns duplex-link $node_(r1) $node_(r2) 1.5Mb 20ms RED/PD 
+    $ns simplex-link $node_(r1) $node_(r2) 0.5Mb 20ms RED/PD 
+    $ns simplex-link $node_(r2) $node_(r1) 0.5Mb 20ms DropTail
     $ns queue-limit $node_(r1) $node_(r2) 100
     $ns queue-limit $node_(r2) $node_(r1) 100
     $ns duplex-link $node_(s3) $node_(r2) 10Mb 4ms DropTail
@@ -180,27 +184,65 @@ TestSuite instproc setTopo {} {
     [$ns_ link $node_(r1) $node_(r2)] trace-dynamics $ns_ stdout
 }
 
+TestSuite instproc monitorFlow {fid prob} {
+    $self instvar topo_ 
+    $topo_ instvar redpdflowmon_ redpdq_
+
+    foreach flow [$redpdflowmon_ flows] {
+	if {[$flow set flowid_] == $fid} {
+	    #monitor the flow with probability $prob
+	    $redpdq_ monitor-flow $flow $prob 
+	}
+    }	
+}
+
+TestSuite instproc unmonitorFlow {fid} {
+    $self instvar topo_ 
+    $topo_ instvar redpdflowmon_ redpdq_
+
+    foreach flow [$redpdflowmon_ flows] {
+	if {[$flow set flowid_] == $fid} {
+	    #unmonitor the flow
+	    $redpdq_ unmonitor-flow $flow 
+	}
+    }	
+}
+
+TestSuite instproc unresponsiveFlow {fid penalty} {
+    $self instvar topo_ 
+    $topo_ instvar redpdflowmon_ redpdq_
+
+    foreach flow [$redpdflowmon_ flows] {
+	if {[$flow set flowid_] == $fid} {
+	    #set the flow unresponsive 
+	    $redpdq_ unresponsive-flow $flow 
+	    #set unresponsive penalty imposed on an unresponsive flow
+	    $redpdq_ set unresponsive_penalty_ $penalty
+	}
+    }	
+}
+
 #
-# Drop packets from one flow with prob. 0.1, starting at time 10.0,
-# if no danger of link underutilization.
+# Start monitoring tcp1 at time 10.0 with probability 0.1,
+# and unmonitor it at 20.0
 #
-Class Test/one -superclass TestSuite
-Test/one instproc init {} {
+Class Test/simple -superclass TestSuite
+Test/simple instproc init {} {
     $self instvar net_ test_
     set net_ net2 
-    set test_ one
+    set test_ simple
     $self next
 }
-Test/one instproc run {} {
+Test/simple instproc run {} {
     $self instvar ns_ node_ testName_ net_ topo_
     $self setTopo
     $topo_ instvar redpdflowmon_ redpdq_
 
 
-    set stoptime 20.0
-    set tcp1 [$ns_ create-connection TCP/Sack1 $node_(s1) TCPSink/Sack1 $node_(s3) 0]
+    set stoptime 30.0
+    set tcp1 [$ns_ create-connection TCP/Sack1 $node_(s1) TCPSink/Sack1 $node_(s3) 1]
     $tcp1 set window_ 50
-    set tcp2 [$ns_ create-connection TCP/Sack1 $node_(s2) TCPSink/Sack1 $node_(s3) 1]
+    set tcp2 [$ns_ create-connection TCP/Sack1 $node_(s2) TCPSink/Sack1 $node_(s3) 2]
     $tcp2 set window_ 50
     set ftp1 [$tcp1 attach-app FTP]
     set ftp2 [$tcp2 attach-app FTP]
@@ -208,7 +250,8 @@ Test/one instproc run {} {
     $ns_ at 0.0 "$ftp1 start"
     $ns_ at 1.0 "$ftp2 start"
 
-    $ns_ at 10.0 "$self monitorFlow"
+    $ns_ at 10.0 "$self monitorFlow 1 0.1"
+    $ns_ at 20.0 "$self unmonitorFlow 1"
 
     $self tcpDump $tcp1 5.0
     # trace only the bottleneck link
@@ -217,18 +260,122 @@ Test/one instproc run {} {
     $ns_ run
 }
 
-TestSuite instproc monitorFlow {} {
-    $self instvar_ redpdflowmon_ redpdq_
+#
+# Start monitoring udp at time 10.0 with probability 0.1, 
+# declare it unresponsive at 20.0 and set unresponsive_penalty_ 3.0
+#
+Class Test/unresponsive -superclass TestSuite
+Test/unresponsive instproc init {} {
+    $self instvar net_ test_
+    set net_ net2 
+    set test_ unresponsive
+    $self next
+}
+Test/unresponsive instproc run {} {
+    $self instvar ns_ node_ testName_ net_ topo_
+    $self setTopo
+    $topo_ instvar redpdflowmon_ redpdq_
 
-    set flow [lindex [$redpdflowmon_ flows] 0]
-    #$queue monitor-flow $flow $probability
-    $redpdq_ monitor-flow $flow .10 
-}	
+    set stoptime 30.0
+    set tcp1 [$ns_ create-connection TCP/Sack1 $node_(s1) TCPSink/Sack1 $node_(s3) 1]
+    $tcp1 set window_ 50
+    set tcp2 [$ns_ create-connection TCP/Sack1 $node_(s2) TCPSink/Sack1 $node_(s3) 2]
+    $tcp2 set window_ 50
+    set ftp1 [$tcp1 attach-app FTP]
+    set ftp2 [$tcp2 attach-app FTP]
+    $self enable_tracequeue $ns_
+    $ns_ at 0.0 "$ftp1 start"
+    $ns_ at 1.0 "$ftp2 start"
 
-# More tests:
-# $redpdq_ unmonitor-flow $flow
-# $redpdq_ unresponsive-flow $flow
-# unresponsive_penalty_
-# P_testFRp_
+    set udp [$ns_ create-connection UDP $node_(s1) Null $node_(s4) 3]
+    set cbr [$udp attach-app Traffic/CBR]
+    $cbr set rate_ 0.5Mb
+    $ns_ at 1.0 "$cbr start"
+    
+    $ns_ at 10.0 "$self monitorFlow 3 0.1"
+    $ns_ at 20.0 "$self unresponsiveFlow 3 3"
+
+    $self tcpDump $tcp1 5.0
+    # trace only the bottleneck link
+    #$self traceQueues $node_(r1) [$self openTrace $stoptime $testName_]
+    $ns_ at $stoptime "$self cleanupAll $testName_"
+    $ns_ run
+}
+
+#
+# drop from all flows at a fixed configured probability
+#
+Class Test/frp -superclass TestSuite
+Test/frp instproc init {} {
+    $self instvar net_ test_
+    set net_ net2 
+    set test_ frp
+    $self next
+}
+Test/frp instproc run {} {
+    $self instvar ns_ node_ testName_ net_ topo_
+    $self setTopo
+    $topo_ instvar redpdflowmon_ redpdq_
+
+    set stoptime 30.0
+    set tcp1 [$ns_ create-connection TCP/Sack1 $node_(s1) TCPSink/Sack1 $node_(s3) 1]
+    $tcp1 set window_ 50
+    set tcp2 [$ns_ create-connection TCP/Sack1 $node_(s2) TCPSink/Sack1 $node_(s3) 2]
+    $tcp2 set window_ 50
+    set ftp1 [$tcp1 attach-app FTP]
+    set ftp2 [$tcp2 attach-app FTP]
+    $self enable_tracequeue $ns_
+    $ns_ at 0.0 "$ftp1 start"
+    $ns_ at 1.0 "$ftp2 start"
+
+    #set the constant dropping probability
+    $redpdq_ set P_testFRp_ 0.1
+
+    $self tcpDump $tcp1 5.0
+    # trace only the bottleneck link
+    #$self traceQueues $node_(r1) [$self openTrace $stoptime $testName_]
+    $ns_ at $stoptime "$self cleanupAll $testName_"
+    $ns_ run
+}
+
+#
+# one complete test using the identification engine
+#
+Class Test/complete -superclass TestSuite
+Test/complete instproc init {} {
+    $self instvar net_ test_
+    set net_ net2 
+    set test_ complete
+    $self next
+}
+Test/complete instproc run {} {
+    $self instvar ns_ node_ testName_ net_ topo_
+    $self setTopo
+    $topo_ instvar redpdflowmon_ redpdq_ redpdlink_
+
+    set stoptime 30.0
+    set tcp1 [$ns_ create-connection TCP/Sack1 $node_(s1) TCPSink/Sack1 $node_(s3) 1]
+    $tcp1 set window_ 50
+    set tcp2 [$ns_ create-connection TCP/Sack1 $node_(s2) TCPSink/Sack1 $node_(s3) 2]
+    $tcp2 set window_ 50
+    set ftp1 [$tcp1 attach-app FTP]
+    set ftp2 [$tcp2 attach-app FTP]
+    $self enable_tracequeue $ns_
+    $ns_ at 0.0 "$ftp1 start"
+    $ns_ at 1.0 "$ftp2 start"
+
+    set udp [$ns_ create-connection UDP $node_(s1) Null $node_(s4) 3]
+    set cbr [$udp attach-app Traffic/CBR]
+    $cbr set rate_ 0.5Mb
+    $ns_ at 1.0 "$cbr start"
+
+    set redpdsim [new REDPDSim $ns_ $redpdq_ $redpdflowmon_ $redpdlink_ 0 true]
+    
+    $self tcpDump $tcp1 5.0
+    # trace only the bottleneck link
+    #$self traceQueues $node_(r1) [$self openTrace $stoptime $testName_]
+    $ns_ at $stoptime "$self cleanupAll $testName_"
+    $ns_ run
+}
 
 TestSuite runTest
