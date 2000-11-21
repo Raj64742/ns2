7b8d9d854957dbb80529d4a5e194de596a8dbc0b
ratul <ratul>
2000-11-21 04:26:44 +0000

    Enabled webtraf to use TCP Sack1 sink when needed. Put in other modes of assigning fid's to the TCP connections.

--- tcl/webcache/webtraf.tcl
+++ tcl/webcache/webtraf.tcl
@@ -21,32 +21,45 @@
 # configuration interface. Be very careful as what is configuration and 
 # what is functionality.
 #
-# $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/webcache/webtraf.tcl,v 1.4 2000/11/17 22:10:37 ratul Exp $
+# $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/webcache/webtraf.tcl,v 1.5 2000/11/21 04:26:44 ratul Exp $
 
 PagePool/WebTraf set debug_ false
 PagePool/WebTraf set TCPTYPE_ Reno
+PagePool/WebTraf set TCPSINKTYPE_ TCPSink   ;#required for SACK1 Sinks.
+
+#0 for default, fid=id
+#1 for increasing along with new agent allocation (as in red-pd). 
+#   useful when used with flow monitors (as they tend to run out of space in hash tables)
+#   (see red-pd scripts for usage of this)
+#2 for assigning the same fid to all connections.
+#    useful when want to differentiate between various web traffic generators using flow monitors.
+#   (see pushback scripts for usage of this).
+PagePool/WebTraf set FID_ASSIGNING_MODE_ 0 
 
 PagePool/WebTraf instproc launch-req { id clnt svr ctcp csnk stcp ssnk size } {
 	set ns [Simulator instance]
 
-#	modified for web traffic flow trace. 
-# 	puts "launch request $id : [$clnt id] -> [$svr id] size $size at [$ns now]"
-# 	puts "  client: $ctcp $csnk"
-# 	puts "  server: $stcp $ssnk"
-
-	$ns attach-agent $clnt $ctcp
-	$ns attach-agent $svr $csnk
-	$ns connect $ctcp $csnk
-	$ctcp set fid_ $id
-
 	$ns attach-agent $svr $stcp
 	$ns attach-agent $clnt $ssnk
 	$ns connect $stcp $ssnk
-	$stcp set fid_ $id
+	
+	$ns attach-agent $clnt $ctcp
+	$ns attach-agent $svr $csnk
+	$ns connect $ctcp $csnk
+
+	if {[PagePool/WebTraf set FID_ASSIGNING_MODE_] == 0} {
+		$stcp set fid_ $id
+		$ctcp set fid_ $id
+	}
+
+	#modified for web traffic flow trace. 
+	#puts "launch request $id: [$clnt id] -> [$svr id] size $size at [$ns now] cfid [$ctcp set fid_] sfid [$stcp set fid_] "
+	# puts "  client: $ctcp $csnk"
+	# puts "  server: $stcp $ssnk"
 
 	$ctcp proc done {} "$self done-req $id $clnt $svr $ctcp $csnk $stcp $size"
-	$stcp proc done {} "$self done-resp $id $clnt $svr $stcp $ssnk"
-
+	$stcp proc done {} "$self done-resp $id $clnt $svr $stcp $ssnk $size [$ns now]"
+	
 	# Send a single packet as a request
 	$ctcp advanceby 1
 }
@@ -54,8 +67,8 @@ PagePool/WebTraf instproc launch-req { id clnt svr ctcp csnk stcp ssnk size } {
 PagePool/WebTraf instproc done-req { id clnt svr ctcp csnk stcp size } {
 	set ns [Simulator instance]
 
-#	modified to trace web traffic flow
-#	puts "done request $id : [$clnt id] -> [$svr id] at [$ns now]"
+	# modified to trace web traffic flow
+	# puts "done request $id : [$clnt id] -> [$svr id] at [$ns now]"
 
 	# Recycle client-side TCP agents
 	$ns detach-agent $clnt $ctcp
@@ -68,11 +81,11 @@ PagePool/WebTraf instproc done-req { id clnt svr ctcp csnk stcp size } {
 	$stcp advanceby $size
 }
 
-PagePool/WebTraf instproc done-resp { id clnt svr stcp ssnk } {
+PagePool/WebTraf instproc done-resp { id clnt svr stcp ssnk size startTime} {
 	set ns [Simulator instance]
 
-#	modified to trace web traffic flow.
-#	puts "done response $id : [$clnt id] -> [$svr id] at [$ns now]"
+	#modified to trace web traffic flow.
+	#puts "done response $id : [$clnt id] -> [$svr id] at [$ns now] size $size duration [expr [$ns now] - $startTime]"
 
 	# Recycle server-side TCP agents
 	$ns detach-agent $clnt $ssnk
@@ -86,9 +99,22 @@ PagePool/WebTraf instproc done-resp { id clnt svr stcp ssnk } {
 # XXX Should allow customizable TCP types. Can be easily done via a 
 # class variable
 PagePool/WebTraf instproc alloc-tcp {} {
-	return [new Agent/TCP/[PagePool/WebTraf set TCPTYPE_]]
+	
+	set tcp [new Agent/TCP/[PagePool/WebTraf set TCPTYPE_]]
+	
+	set fidMode [PagePool/WebTraf set FID_ASSIGNING_MODE_]
+	if {$fidMode == 1} {
+		$self instvar maxFid_
+		$tcp set fid_ $maxFid_
+		incr maxFid_
+	} elseif  {$fidMode == 2} {
+		$self instvar sameFid_
+		$tcp set fid_ $sameFid_
+	}
+    
+	return $tcp
 }
 
 PagePool/WebTraf instproc alloc-tcp-sink {} {
-	return [new Agent/TCPSink]
+	return [new Agent/[PagePool/WebTraf set TCPSINKTYPE_]]
 }
