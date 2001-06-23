1122188c536ba20e477a2ff566e58c89c0538918
kclan <kclan>
2001-06-23 00:12:31 +0000

    bug fixed

--- empweb/scripts/util.tcl
+++ empweb/scripts/util.tcl
@@ -7,6 +7,16 @@ proc Pre { tfile } {
     set timezero [tcpdtimetosecs [lindex $aslist 0]]
 }
 
+proc max {arg1 arg2}  {
+    if { $arg1 > $arg2 } { return $arg1 }
+    return $arg2 
+}
+
+proc min {arg1 arg2}  {
+    if { $arg1 > $arg2 } { return $arg2 }
+    return $arg1 
+}
+
 proc tcpdtimetosecs tcpdtime  {
     global timezero 
 
@@ -28,3 +38,67 @@ proc tcpdtimetosecs tcpdtime  {
     return [expr $tim - $timezero]
 }
 
+proc outputPDF {cur_epoch incr tfile }  {
+
+set count 0
+set cur_time  0
+set tfileI [format "%s.sorted" $tfile]
+set tfileO [format "%s.pdf" $tfile]
+
+set fi [open $tfileI r ]
+set fo [open $tfileO w ]
+
+while {[gets $fi line] >= 0} {
+
+set cur_time [lindex $line 0]
+
+
+if {$cur_time > $cur_epoch} {
+   while {$cur_epoch < $cur_time} {
+
+     puts $fo "$cur_epoch   $count"
+
+     set cur_epoch [expr $cur_epoch + $incr]
+     set count 0
+   }
+   if {$cur_time <= $cur_epoch} {
+      set count 1
+   } else {
+      set count 0
+   }
+} else {
+   set count [expr $count + 1]
+}
+
+}
+
+puts $fo "$cur_epoch   $count"
+
+
+close $fo
+}
+
+proc outputCDF {tfile}  {
+
+set tfileI [format "%s.pdf" $tfile]
+set tfileO [format "%s.cdf" $tfile]
+
+set fi [open $tfileI r ]
+set fo [open $tfileO w ]
+
+set totalCnt 0
+set cumCnt 0
+while {[gets $fi line] >= 0} {
+   set cnt [lindex $line 1]
+   set totalCnt [expr $totalCnt + $cnt]
+}
+close $fi
+set fi [open $tfileI r ]
+while {[gets $fi line] >= 0} {
+   set value [lindex $line 0]
+   set cnt [lindex $line 1]
+   set cumCnt [expr $cumCnt + $cnt]
+   puts $fo "$value $cumCnt [expr ($cumCnt * 1.0) / ($totalCnt * 1.0)]"
+}
+
+}
