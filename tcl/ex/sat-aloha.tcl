76f75207d83e164921496ad3ee8f62efcf4394c2
tomh <tomh>
2001-10-11 14:17:33 +0000

    Added a couple of options: i) collision tracing can be enabled or disabled, ii) Poisson traffic generator can be used

--- tcl/ex/sat-aloha.tcl
+++ tcl/ex/sat-aloha.tcl
@@ -32,13 +32,37 @@
 #
 # Contributed by Tom Henderson, UCB Daedalus Research Group, June 1999
 #
-# Simple script with a geostationary bent-pipe (repeater) satellite and 
-# one hundred terminals using unslotted Aloha random access.  The
-# traffic sources consist of exponential traffic generators.  With the
-# traffic rates configured below, approximately one tenth of the packets 
-# collide and must be retransmitted (these collisions are not explicitly 
-# seen in the tracefile).
+# $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/ex/sat-aloha.tcl,v 1.4 2001/10/11 14:17:33 tomh Exp $
 # 
+# Simple script with a geostationary bent-pipe (repeater) satellite and 
+# one hundred terminals using unslotted (pure) Aloha random access.  The
+# traffic sources consist of exponential on-off traffic generators unless
+# indicated below.  With the traffic rates configured below, approximately 
+# one tenth of the packets collide and must be retransmitted.
+# 
+# Tests:
+# 1.  basic:  Mac operates in stop-and-wait mode (one outstanding packet
+#             at a time).  Collisions and drops are not traced.  This test
+#             should provide similar results to the satellite test suite.
+# 2.  basic_tracing:  Same as "basic", but drops ("d") and collisions ("c")
+#             are instead explicitly traced.  Again, similar to test suite   
+# 3.  poisson:  Packets arrive according to Poisson process.  Each source 
+#             still operates in stop-and-wait mode, and collisions and 
+#             drops are traced.  rtx_limit_ == 0 (no persistence).
+#	      This can be used to try to approximate theoretical 
+#	      unslotted Aloha results, if the number of terminals is large
+#             compared to the arrival rate (so that no packets are queued)
+# 4.  [FOR FUTURE WORK].  (larger than one packet rxmit buffer, different
+#             traffic generator?)
+# 
+
+if { $argc != 1 } {
+        puts stderr {usage: ns sat-aloha.tcl [basic basic_tracing poisson]}
+        exit 1
+}
+set test_ $argv
+puts "Running test $test_ ..."
+
 
 global ns
 set ns [new Simulator]
@@ -47,6 +71,11 @@ set ns [new Simulator]
 Mac/Sat/UnslottedAloha set mean_backoff_ 1s ; # mean exponential backoff time(s)
 Mac/Sat/UnslottedAloha set rtx_limit_ 3; # max number of retrans. attempted 
 Mac/Sat/UnslottedAloha set send_timeout_ 270ms; # resend if send times out
+
+if { $test_ == "basic"} {
+	Mac/Sat set trace_collisions_ false
+	Mac/Sat set trace_drops_ false
+}
 
 global opt
 set opt(chan)           Channel/Sat
@@ -77,9 +106,9 @@ set n1 [$ns node]
 $n1 set-position 0
 
 
-# Place 100 terminals at 100 different locations
+# Place 50 terminals at 100 different locations
 $ns node-config -satNodeType terminal
-set num_nodes		100
+set num_nodes	100
 for {set a 1} {$a <= $num_nodes} {incr a} {
 	set n($a) [$ns node]
 	$n($a) set-position [expr -15 + $a * 0.3] [expr 15 - $a * 0.3]
@@ -89,8 +118,8 @@ for {set a 1} {$a <= $num_nodes} {incr a} {
 
 for {set a 1} {$a <= $num_nodes} {incr a} {
 	set b [expr int($a + (0.5 * $num_nodes))]
-	if {$b > 100} {
-		incr b -100
+	if {$b > $num_nodes} {
+		incr b [expr -1 * $num_nodes]
 	}
 
 	set udp($a) [new Agent/UDP]
@@ -98,6 +127,11 @@ for {set a 1} {$a <= $num_nodes} {incr a} {
 	set exp($a) [new Application/Traffic/Exponential]
 	$exp($a) attach-agent $udp($a)
 	$exp($a) set rate_ 1Kb
+	if {$test_ == "poisson"} {
+		$exp($a) set rate_ 10000Mb
+		$exp($a) set burst_time_ 0
+		$exp($a) set idle_time_ 5 
+	}
 
 	set null($a) [new Agent/Null]
 	$ns attach-agent $n($b) $null($a)
