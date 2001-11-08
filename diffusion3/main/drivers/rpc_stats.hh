23644aa04d7f259c14e9cde3e0a375a9aba42572
haldar <haldar>
2001-11-08 18:56:31 +0000

    first-cut of diffusion port into ns

---
+++ diffusion3/main/drivers/rpc_stats.hh
@@ -0,0 +1,41 @@
+// *********************************************************
+// 
+// rpc_stats.hh  : Collect statistics from the RPC radio
+// authors       : Chalermek Intanagonwiwat and Fabio Silva
+//
+// $Id: rpc_stats.hh,v 1.1 2001/11/08 18:56:31 haldar Exp $
+//
+// *********************************************************
+
+#ifndef rpc_stats_hh
+#define rpc_stats_hh
+
+#include "stdio.h"
+#include "stdlib.h"
+
+class RPCStats {
+public:
+  RPCStats(int id);
+  void printStats(FILE *output);
+
+private:
+  int node_id;
+
+  // Counters
+  int rpc_tx_bytes;
+  int rpc_rx_bytes;
+  int rpc_tx_frames;
+  int rpc_rx_frames;
+
+  // RPC Interface files
+  FILE *tx_bytes;
+  FILE *rx_bytes;
+  FILE *tx_frames;
+  FILE *rx_frames;
+
+  void readCounters();
+  void openFiles();
+  void closeFiles();
+};
+
+#endif
