6f57cea3ca3da2dc85bc3d0272d7f737019824ad
haldar <haldar>
2001-11-08 18:17:57 +0000

    adding first-cut of diffusion into ns

---
+++ diffusion3/apps/agents/attr.h
@@ -0,0 +1,17 @@
+//attr.h
+
+
+#ifndef DIFF_ATTR
+#define DIFF_ATTR
+
+//for now keeping the attr templates here
+
+NRSimpleAttributeFactory<char *> TargetAttr(NRAttribute::TARGET_KEY, NRAttribute::STRING_TYPE);
+NRSimpleAttributeFactory<char *> AppStringAttr(APP_KEY2, NRAttribute::STRING_TYPE);
+NRSimpleAttributeFactory<int> AppCounterAttr(APP_KEY3, NRAttribute::INT32_TYPE);
+NRSimpleAttributeFactory<int> AppDummyAttr(APP_KEY1, NRAttribute::INT32_TYPE);
+
+
+
+
+#endif //attr
