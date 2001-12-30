7927b8fa6c79d9438d2901e5b97101dabbb5cb5d
sfloyd <sfloyd>
2001-12-30 07:25:41 +0000

    Adding values for RED parameters, for changing global defaults.

--- tcl/test/test-suite-sack-v1a.tcl
+++ tcl/test/test-suite-sack-v1a.tcl
@@ -37,7 +37,7 @@ Agent/TCP set singledup_ 0
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/Attic/test-suite-sack-v1a.tcl,v 1.7 2001/11/28 23:04:27 sfloyd Exp $
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/Attic/test-suite-sack-v1a.tcl,v 1.8 2001/12/30 07:25:41 sfloyd Exp $
 #
 
 #
@@ -46,11 +46,17 @@ Agent/TCP set singledup_ 0
 # copy the new code with the new interfaces!
 #
 
+# FOR UPDATING GLOBAL DEFAULTS:
+Queue/RED set q_weight_ 0.002
+Queue/RED set thresh_ 5
+Queue/RED set maxthresh_ 15
+# The RED parameter defaults are being changed for automatic configuration.
 Agent/TCP set minrto_ 0
 # The default is being changed to minrto_ 1
 Agent/TCP set syn_ false
 Agent/TCP set delay_growth_ false
 # In preparation for changing the default values for syn_ and delay_growth_.
+
 
 set quiet false
 Agent/TCP set oldCode_ true
