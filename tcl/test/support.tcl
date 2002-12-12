d96835ab8ef2423a95a8621ce6eea7116ff66618
sfloyd <sfloyd>
2002-12-12 04:07:11 +0000

    Adding the procedure dropPktsPeriodic for use in simulation tests.

--- tcl/test/support.tcl
+++ tcl/test/support.tcl
@@ -21,3 +21,20 @@ TestSuite instproc dropPkts { link fid pkts {delayPkt no} {delay -1}} {
     $emod bind $errmodel1 $fid
 }
 
+TestSuite instproc dropPktsPeriodic { link fid offset period {delayPkt no} {delay -1}} {
+    set emod [new ErrorModule Fid]
+    set errmodel1 [new ErrorModel/Periodic]
+    $errmodel1 unit pkt
+    $errmodel1 set offset_ $offset
+    $errmodel1 set period_ $period
+    if {$delayPkt != "no"} {
+        $errmodel1 set delay_pkt_ $delayPkt
+    }
+    if {$delay != -1} {
+        $errmodel1 set delay_ $delay
+    }
+    $link errormodule $emod
+    $emod insert $errmodel1
+    $emod bind $errmodel1 $fid
+}
+
