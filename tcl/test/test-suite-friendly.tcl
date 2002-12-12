cee33168883db00990d4225ca4611d6eab7cc23a
sfloyd <sfloyd>
2002-12-12 04:25:40 +0000

    Adding a new validation test.

--- tcl/test/test-suite-friendly.tcl
+++ tcl/test/test-suite-friendly.tcl
@@ -30,7 +30,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/test-suite-friendly.tcl,v 1.46 2002/12/12 04:04:28 sfloyd Exp $
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/test-suite-friendly.tcl,v 1.47 2002/12/12 04:26:33 sfloyd Exp $
 #
 
 source misc_simple.tcl
@@ -979,6 +979,58 @@ Test/HighLoss instproc run {} {
 		$cbr1 set rate_ 3Mb
 		$ns_ at [expr $stopTime1_/3.0] "$cbr1 start"   
 		$ns_ at [expr 2.0*$stopTime1_/3.0] "$cbr1 stop"   
+
+    $self tfccDump 1 $tf1 $interval_ $dumpfile_
+
+    $ns_ at $stopTime0 "close $dumpfile_; $self finish_1 $testName_"
+    #$self traceQueues $node_(r1) [$self openTrace $stopTime $testName_]
+    $ns_ at $stopTime "$self cleanupAll $testName_" 
+    if {$quiet == "false"} {
+	$ns_ at $stopTime2 "close $tracefile"
+    }
+    $ns_ at $stopTime2 "exec cp temp2.rands temp.rands; exit 0"
+
+    # trace only the bottleneck link
+    $ns_ run
+}
+  
+# PreciseLoss_ is turned off
+Class Test/HighLossImprecise -superclass TestSuite
+Test/HighLossImprecise instproc init {} {
+    $self instvar net_ test_ stopTime1_
+    set net_	net2
+    set test_ HighLossImprecise	
+    Agent/TFRCSink set discount_ 1
+    Agent/TFRCSink set smooth_ 1
+    Agent/TFRC set df_ 0.95
+    Agent/TFRC set ca_ 1
+    Agent/TFRCSink set PreciseLoss_ 0
+    set stopTime1_ 80
+    $self next
+}
+Test/HighLossImprecise instproc run {} {
+    global quiet
+    $self instvar ns_ node_ testName_ interval_ dumpfile_ stopTime1_
+    $self setTopo
+    set interval_ 1
+    set stopTime $stopTime1_
+    set stopTime0 [expr $stopTime - 0.001]
+    set stopTime2 [expr $stopTime + 0.001]
+
+    set dumpfile_ [open temp.s w]
+    if {$quiet == "false"} {
+        set tracefile [open all.tr w]
+        $ns_ trace-all $tracefile
+    }
+
+    set tf1 [$ns_ create-connection TFRC $node_(s1) TFRCSink $node_(s3) 0]
+    $ns_ at 0.0 "$tf1 start"
+
+    set udp1 [$ns_ create-connection UDP $node_(s2) UDP $node_(s4) 1]
+		set cbr1 [$udp1 attach-app Traffic/CBR]
+		$cbr1 set rate_ 3Mb
+		$ns_ at [expr $stopTime1_/4.0] "$cbr1 start"   
+		$ns_ at [expr 2.0*$stopTime1_/4.0] "$cbr1 stop"   
 
     $self tfccDump 1 $tf1 $interval_ $dumpfile_
 
