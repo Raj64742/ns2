03f583a425dc4a3096540aa314d38e906bd68ca3
sfloyd <sfloyd>
2003-01-25 01:59:30 +0000

    Adding validation tests for Tahoe, Reno, NewReno, Sack TCP.

--- tcl/test/test-suite-quickstart.tcl
+++ tcl/test/test-suite-quickstart.tcl
@@ -35,6 +35,17 @@
 source misc_simple.tcl
 source support.tcl
 
+Queue set util_weight_ 2  
+# 18 seconds
+Agent/QSAgent set alloc_rate_ 0.5    
+# up to 0.5 of link bandwidth.
+Agent/QSAgent set qs_enabled_ 1
+Agent/QSAgent set state_delay_ 0.35  
+# 0.35 seconds for past approvals
+Agent/TCP/Newreno/QS set rbp_scale_ 1.0
+Agent/TCPSink set enable_QuickStart_ true
+Agent/TCP set enable_QuickStart_ true
+
 # Uncomment the line below to use a random seed for the
 #  random number generator.
 # ns-random 0
@@ -70,7 +81,7 @@ Topology/net2 instproc init ns {
     $self next
     $ns duplex-link $node_(s1) $node_(r1) 1000Mb 2ms DropTail
     $ns duplex-link $node_(s2) $node_(r1) 1000Mb 3ms DropTail
-    $ns duplex-link $node_(r1) $node_(r2) 100Mb 200ms RED
+    $ns duplex-link $node_(r1) $node_(r2) 100Mb 500ms RED
     $ns queue-limit $node_(r1) $node_(r2) 100
     $ns queue-limit $node_(r2) $node_(r1) 100 
     $ns duplex-link $node_(s3) $node_(r2) 1000Mb 4ms DropTail
@@ -79,22 +90,24 @@ Topology/net2 instproc init ns {
 
 Class Test/no_quickstart -superclass TestSuite
 Test/no_quickstart instproc init {} {
-    $self instvar net_ test_ guide_ stopTime1_ sender 
+    $self instvar net_ test_ guide_ sndr rcvr qs
     set net_	net2
     set test_ no_quickstart	
     set guide_  \
     "Two TCPs, no quickstart."
-    set sender TFRC
-    set stopTime1_ 6
-    Agent/TCP set max_ssthresh_ 100
+    set sndr TCP/Newreno
+    set rcvr TCPSink
+    set qs OFF
+    Agent/QSAgent set qs_enabled_ 0
     $self next pktTraceFile
 }
 Test/no_quickstart instproc run {} {
     global quiet
-    $self instvar ns_ node_ testName_ guide_ stopTime1_ sender 
+    $self instvar ns_ node_ testName_ guide_ sndr rcvr qs
     if {$quiet == "false"} {puts $guide_}
+    $ns_ node-config -QS $qs
     $self setTopo
-    set stopTime $stopTime1_
+    set stopTime 6
 
     set tcp1 [$ns_ create-connection TCP/Newreno $node_(s1) TCPSink $node_(s3) 0]
     $tcp1 set window_ 8
@@ -102,8 +115,9 @@ Test/no_quickstart instproc run {} {
     $ftp1 attach-agent $tcp1
     $ns_ at 0 "$ftp1 start"
 
-    set tcp2 [$ns_ create-connection TCP/Newreno $node_(s1) TCPSink $node_(s3) 1]
+    set tcp2 [$ns_ create-connection $sndr $node_(s1) $rcvr $node_(s3) 1]
     $tcp2 set window_ 1000
+    $tcp2 set rate_request_ 20
     set ftp2 [new Application/FTP]
     $ftp2 attach-agent $tcp2
     $ns_ at 2 "$ftp2 produce 80"
@@ -115,48 +129,73 @@ Test/no_quickstart instproc run {} {
 
 Class Test/quickstart -superclass TestSuite
 Test/quickstart instproc init {} {
-    $self instvar net_ test_ guide_ stopTime1_ sender 
+    $self instvar net_ test_ guide_ sndr rcvr qs
     set net_	net2
     set test_ quickstart	
     set guide_  \
-    "Two TCPs, with quickstart."
-    set sender TFRC
-    set stopTime1_ 6
-    Agent/TCP set max_ssthresh_ 100
-    Queue set util_weight_ 2  
-    # 18 seconds
-    Agent/QSAgent set alloc_rate_ 0.5    
-    # up to 0.5 of link bandwidth.
-    Agent/QSAgent set qs_enabled_ 1
-    Agent/QSAgent set state_delay_ 0.35  
-    Agent/TCP/Newreno/QS set rbp_scale_ 1.0
-    # 0.35 seconds for past approvals
+    "Two TCPs, TCP/Newreno/QS, with QuickStart."
+    set sndr TCP/Newreno
+    set rcvr TCPSink
+    set qs ON
+    Agent/TCP/Newreno/QS set rate_request_ 20
+    Test/quickstart instproc run {} [Test/no_quickstart info instbody run ]
     $self next pktTraceFile
 }
-Test/quickstart instproc run {} {
-    global quiet
-    $self instvar ns_ node_ testName_ guide_ stopTime1_ sender 
-    if {$quiet == "false"} {puts $guide_}
-    $ns_ node-config -QS ON
-    $self setTopo
-    set stopTime $stopTime1_
 
-    set tcp1 [$ns_ create-connection TCP/Newreno $node_(s1) TCPSink $node_(s3) 0]
-    $tcp1 set window_ 8
-    set ftp1 [new Application/FTP]
-    $ftp1 attach-agent $tcp1
-    $ns_ at 0 "$ftp1 start"
+Class Test/quickstart1 -superclass TestSuite
+Test/quickstart1 instproc init {} {
+    $self instvar net_ test_ guide_ sndr rcvr qs
+    set net_	net2
+    set test_ quickstart1	
+    set guide_  \
+    "Two TCPs, plain TCP, with QuickStart."
+    set sndr TCP
+    set rcvr TCPSink
+    set qs ON
+    Test/quickstart1 instproc run {} [Test/no_quickstart info instbody run ]
+    $self next pktTraceFile
+}
 
-    set tcp2 [$ns_ create-connection TCP/Newreno/QS $node_(s1) TCPSink/QS $node_(s3) 1]
-    $tcp2 set window_ 1000
-    $tcp2 set rate_request_ 60000
-    set ftp2 [new Application/FTP]
-    $ftp2 attach-agent $tcp2
-    $ns_ at 2 "$ftp2 produce 80"
+Class Test/quickstart2 -superclass TestSuite
+Test/quickstart2 instproc init {} {
+    $self instvar net_ test_ guide_ sndr rcvr qs
+    set net_	net2
+    set test_ quickstart2	
+    set guide_  \
+    "Two TCPs, Reno TCP, with QuickStart."
+    set sndr TCP/Reno
+    set rcvr TCPSink
+    set qs ON
+    Test/quickstart2 instproc run {} [Test/no_quickstart info instbody run ]
+    $self next pktTraceFile
+}
 
-    $ns_ at $stopTime "$self cleanupAll $testName_ $stopTime"
+Class Test/quickstart3 -superclass TestSuite
+Test/quickstart3 instproc init {} {
+    $self instvar net_ test_ guide_ sndr rcvr qs
+    set net_	net2
+    set test_ quickstart3	
+    set guide_  \
+    "Two TCPs, NewReno TCP, with QuickStart."
+    set sndr TCP/Newreno
+    set rcvr TCPSink
+    set qs ON
+    Test/quickstart3 instproc run {} [Test/no_quickstart info instbody run ]
+    $self next pktTraceFile
+}
 
-    $ns_ run
+Class Test/quickstart4 -superclass TestSuite
+Test/quickstart4 instproc init {} {
+    $self instvar net_ test_ guide_ sndr rcvr qs
+    set net_	net2
+    set test_ quickstart4	
+    set guide_  \
+    "Two TCPs, Sack TCP, with QuickStart."
+    set sndr TCP/Sack1
+    set rcvr TCPSink/Sack1
+    set qs ON
+    Test/quickstart4 instproc run {} [Test/no_quickstart info instbody run ]
+    $self next pktTraceFile
 }
 
 TestSuite runTest
