62e1d16e1f33891f45521ef7b144c01f3d306a28
haldar <haldar>
2003-02-19 22:22:28 +0000

    Added obsolete comment for old base-station tcl procs in mobility/com.tcl

--- tcl/mobility/com.tcl
+++ tcl/mobility/com.tcl
@@ -31,146 +31,165 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/mobility/com.tcl,v 1.8 2000/09/14 18:19:28 haoboy Exp $
+# $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/mobility/com.tcl,v 1.9 2003/02/19 22:22:28 haldar Exp $
 #
 
+# These procedures are all obsolete and has been replaced by the newer node 
+# configuration. See test-suite-WL-tutorial.tcl for details.
+
 proc create-base-station-node {address } {
-    global topo tracefd opt node node_ ns_
-    set ns_ [Simulator instance]
-    if [Simulator set mobile_ip_] {
-	Simulator set node_factory_ MobileNode/MIPBS
-    } else {
-	Simulator set node_factory_ Node/MobileNode/BaseStationNode
-    }
-    set node [$ns_ node $address]
-    set id [$node id]
+    
+    puts "Method create-base-station-node is now obsolete. Use methods in ~ns/tcl/test/test-suite-WLtutorial.tcl to create base-station nodes\n\n"
+    exit 0
+
+    # global topo tracefd opt node node_ ns_
+#     set ns_ [Simulator instance]
+#     if [Simulator set mobile_ip_] {
+# 	Simulator set node_factory_ MobileNode/MIPBS
+#     } else {
+# 	Simulator set node_factory_ Node/MobileNode/BaseStationNode
+#     }
+#     set node [$ns_ node $address]
+#     set id [$node id]
 	
-    $node random-motion 0		;# disable random motion
-    $node topography $topo
-    #
-    # This Trace Target is used to log changes in direction
-    # and velocity for the mobile node.
-    #
-    set T [new Trace/Generic]
-    $T target [$ns_ set nullAgent_]
-    $T attach $tracefd
-    $T set src_ $id
-    $node log-target $T
-    $node base-station [AddrParams addr2id [$node node-addr]]
+#     $node random-motion 0		;# disable random motion
+#     $node topography $topo
+#     #
+#     # This Trace Target is used to log changes in direction
+#     # and velocity for the mobile node.
+#     #
+#     set T [new Trace/Generic]
+#     $T target [$ns_ set nullAgent_]
+#     $T attach $tracefd
+#     $T set src_ $id
+#     $node log-target $T
+#     $node base-station [AddrParams addr2id [$node node-addr]]
     
-    create-$opt(rp)-bs-node $node $id
+#     create-$opt(rp)-bs-node $node $id
     
-    Simulator set node_factory_ Node    ;# default value
-    return $node
+#     Simulator set node_factory_ Node    ;# default value
+#     return $node
 }
 
 
 proc create-dsdv-bs-node {node id} {
-	global ns_ chan prop opt node_
-	$node instvar regagent_ ragent_
 
-	$node add-interface $chan $prop $opt(ll) $opt(mac)	\
-		    $opt(ifq) $opt(ifqlen) $opt(netif) \
-		    $opt(ant)
-    
-	create-$opt(rp)-routing-agent $node $id
+    puts "Method create-dsdv-bs-node is now obsolete. Use methods in ~ns/tcl/test/test-suite-WLtutorial.tcl to create base-station nodes\n\n"
+    exit 0
 
-	if [info exists regagent_] {
-		$regagent_ ragent $ragent_
-	}
-	if { $opt(pos) == "Box" } {
-		#
-		# Box Configuration
-		#
-		set spacing 200
-		set maxrow 7
-		set col [expr ($id - 1) % $maxrow]
-		set row [expr ($id - 1) / $maxrow]
-		$node set X_ [expr $col * $spacing]
-		$node set Y_ [expr $row * $spacing]
-		$node set Z_ 0.0
-		$node set speed_ 0.0
-
-		$ns_ at 0.0 "$node_($id) start"
-	}
+#	global ns_ chan prop opt node_
+#	$node instvar regagent_ ragent_
+#
+#	$node add-interface $chan $prop $opt(ll) $opt(mac)	\
+#		    $opt(ifq) $opt(ifqlen) $opt(netif) \
+#		    $opt(ant)
+#    
+#	create-$opt(rp)-routing-agent $node $id
+#
+#	if [info exists regagent_] {
+#		$regagent_ ragent $ragent_
+#	}
+#	if { $opt(pos) == "Box" } {
+#		#
+#		# Box Configuration
+#		#
+#		set spacing 200
+#		set maxrow 7
+#		set col [expr ($id - 1) % $maxrow]
+#		set row [expr ($id - 1) / $maxrow]
+#		$node set X_ [expr $col * $spacing]
+#		$node set Y_ [expr $row * $spacing]
+#		$node set Z_ 0.0
+#		$node set speed_ 0.0
+#
+#		$ns_ at 0.0 "$node_($id) start"
+#	}
 }
 
 proc create-dsr-bs-node {node id} {
-    global ns_ chan prop opt
-    $node instvar regagent_ ragent_
-    
-    $node add-interface $chan $prop $opt(ll) $opt(mac)	\
-	    $opt(ifq) $opt(ifqlen) $opt(netif) \
-	    $opt(ant)
-    
-    create-$opt(rp)-routing-agent $node $id
-    $node create-xtra-interface 
-    
-    if [info exists regagent_] {
-	$regagent_ ragent $ragent_
-    }
-    
-    $ns_ at 0.0 "$node start-dsr"
+
+    puts "Method create-dsr-bs-node is now obsolete. Use methods in ~ns/tcl/test/test-suite-WLtutorial.tcl to create base-station nodes\n\n"
+    exit 0
+
+#    global ns_ chan prop opt
+#    $node instvar regagent_ ragent_
+#    
+#    $node add-interface $chan $prop $opt(ll) $opt(mac)	\
+#	    $opt(ifq) $opt(ifqlen) $opt(netif) \
+#	    $opt(ant)
+#    
+#    create-$opt(rp)-routing-agent $node $id
+#    $node create-xtra-interface 
+#    
+#    if [info exists regagent_] {
+#	$regagent_ ragent $ragent_
+#    }
+#    
+#    $ns_ at 0.0 "$node start-dsr"
 }
 
 
 proc create-dsr-routing-agent { node id } {
-    global ns_ ragent_ tracefd opt
 
-    # 
-    # Create routing agent and attach it to port 255
-    #
-    set ragent_($id) [new Agent/DSRAgent/BS_DSRAgent]
-    set ragent $ragent_($id)
+    puts "Method create-dsr-routing-agent is now obsolete. Use methods in ~ns/tcl/test/test-suite-WLtutorial.tcl to create base-station nodes\n\n"
+    exit 0
     
-    # setup address (supports hier-addr) for dsdv agent 
-    set address [$node node-addr]
-    $ragent addr $address
-    $ragent node $node
-    if [Simulator set mobile_ip_] {
-	$ragent port-dmux [$node set dmux_]
-    }
-    
-    $node addr $address
-    $node set ragent_ $ragent
-    
-    set dmux [$node set dmux_]
-    if {$dmux == "" } {
-	set dmux [new Classifier/Hash/Dest 32]
-	$dmux set mask_ [AddrParams PortMask]
-	$dmux set shift_ [AddrParams PortShift]
-	#
-	# point the node's routing entry to itself
-	# at the port demuxer (if there is one)
-	#
-	$node add-route $address $ragent
-	$node set dmux_ $dmux
-    }
-    set level [AddrParams hlevel]
-    
-    if { [Simulator set RouterTrace_] == "ON" } {
-	#
+#    global ns_ ragent_ tracefd opt
+#
+#    # 
+#    # Create routing agent and attach it to port 255
+#    #
+#    set ragent_($id) [new Agent/DSRAgent/BS_DSRAgent]
+#    set ragent $ragent_($id)
+#    
+#    # setup address (supports hier-addr) for dsdv agent 
+#    set address [$node node-addr]
+#    $ragent addr $address
+#    $ragent node $node
+#    if [Simulator set mobile_ip_] {
+#	$ragent port-dmux [$node set dmux_]
+#    }
+#    
+#    $node addr $address
+#    $node set ragent_ $ragent
+#    
+#    set dmux [$node set dmux_]
+#    if {$dmux == "" } {
+#	set dmux [new Classifier/Hash/Dest 32]
+#	$dmux set mask_ [AddrParams PortMask]
+#	$dmux set shift_ [AddrParams PortShift]
+#	#
+#	# point the node's routing entry to itself
+#	# at the port demuxer (if there is one)
+#	#
+#	$node add-route $address $ragent
+#	$node set dmux_ $dmux
+#    }
+#    set level [AddrParams hlevel]
+#    
+#    if { [Simulator set RouterTrace_] == "ON" } {
+#	#
 	# Recv Target
 	#
-	set rcvT [cmu-trace Recv "RTR" $node]
-	$rcvT target $ragent
-	for {set i 1} {$i <= $level} {incr i} {
-	    [$node set classifiers_($i)] defaulttarget $rcvT
-	    [$node set classifiers_($i)] bcast-receiver $rcvT
-	}
-    } else {
-	
-	for {set i 1} {$i <= $level} {incr i} {
-	    [$node set classifiers_($i)] defaulttarget $ragent
-	    [$node set classifiers_($i)] bcast-receiver $ragent
-	}
-    }
+#	set rcvT [cmu-trace Recv "RTR" $node]
+#	$rcvT target $ragent
+#	for {set i 1} {$i <= $level} {incr i} {
+#	    [$node set classifiers_($i)] defaulttarget $rcvT
+#	    [$node set classifiers_($i)] bcast-receiver $rcvT
+#	}
+#    } else {
+#	
+#	for {set i 1} {$i <= $level} {incr i} {
+#	    [$node set classifiers_($i)] defaulttarget $ragent
+#	    [$node set classifiers_($i)] bcast-receiver $ragent
+#	}
+#    }
     #
     # Drop Target (always on regardless of other tracing)
     #
-    set drpT [cmu-trace Drop "RTR" $node]
-    $ragent drop-target $drpT
-    
+#    set drpT [cmu-trace Drop "RTR" $node]
+#    $ragent drop-target $drpT
+#    
     #
     # Log Target
     #
@@ -180,11 +199,11 @@ proc create-dsr-routing-agent { node id } {
     #$T set src_ [$node id]
     #$ragent log-target $T
 
-    $ragent target $dmux
+#    $ragent target $dmux
     
     # packets to the DSR port should be handed over to ragent_
     # since all pkts now donot go thru ragent
-    $dmux install $opt(rt_port) $ragent
+#    $dmux install $opt(rt_port) $ragent
 }
 
 
