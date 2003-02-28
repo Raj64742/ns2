a46de39745b2f1b9d785cae244efe90d1be7c51c
buchheim <buchheim>
2003-02-28 02:06:27 +0000

    removed RtModule/Nix stuff which was somehow accidently reintroduced to this file (thus breaking builds on non-STL systems)

--- tcl/lib/ns-rtmodule.tcl
+++ tcl/lib/ns-rtmodule.tcl
@@ -26,7 +26,7 @@
 #  Other copyrights might apply to parts of this software and are so
 #  noted when applicable.
 # 
-#  $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/lib/ns-rtmodule.tcl,v 1.11 2003/02/21 00:40:22 haldar Exp $
+#  $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/lib/ns-rtmodule.tcl,v 1.12 2003/02/28 02:06:27 buchheim Exp $
 #
 # OTcl interface definition for the base routing module. They provide 
 # linkage to Node, hence all derived classes should inherit these interfaces
@@ -336,21 +336,4 @@ Classifier/Virtual instproc find dst {
 
 Classifier/Virtual instproc install {dst target} {
 }
-
-
-#Class RtModule/Nix -superclass RtModule
-
-#Nix-vector routing
-RtModule/Nix instproc register { node } {
-	$self next $node
-	# Create classifier
-	$self instvar classifier_
-	set classifier_ [new Classifier/Nix]
-	$classifier_ set-node-id [$node set id_]
-	#puts "RtModule/Nix set node id to [$node set id_]"
-	$node install-entry $self $classifier_
-}
-
-RtModule/Nix instproc route-notify { module } { }
-
 
