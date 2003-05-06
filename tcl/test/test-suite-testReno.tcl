51b68c99a51601fc48d78a6903e9c1bcb41d518c
sfloyd <sfloyd>
2003-05-06 04:48:09 +0000

    Changing to current default values for Agent/TCP/Newreno's newreno_changes1_ and partial_window_deflation_.

--- tcl/test/test-suite-testReno.tcl
+++ tcl/test/test-suite-testReno.tcl
@@ -30,7 +30,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/test-suite-testReno.tcl,v 1.14 2003/05/06 04:21:04 sfloyd Exp $
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/test-suite-testReno.tcl,v 1.15 2003/05/06 04:48:11 sfloyd Exp $
 #
 # To view a list of available tests to run with this script:
 # ns test-suite-testReno.tcl
@@ -42,10 +42,6 @@ Agent/TCP set tcpTick_ 0.1
 Agent/TCP set rfc2988_ false
 # The default for rfc2988_ is being changed to true.
 # FOR UPDATING GLOBAL DEFAULTS:
-Agent/TCP/Newreno set newreno_changes1_ 0
-# The default is being changed to 1 on 5/5/03, to reflect RFC 2582.
-Agent/TCP/Newreno set partial_window_deflation_ 0  
-# The default is being changed to 1 on 5/5/03, to reflect RFC 2582.
 Agent/TCP set useHeaders_ false
 # The default is being changed to useHeaders_ true.
 Agent/TCP set windowInit_ 1
