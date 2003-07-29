1aaf3db2b09778bfcdbeaf45978005e4ed2a877e
sfloyd <sfloyd>
2003-07-29 21:36:35 +0000

    Modifying to use the current defaults for Agent/TCP's exitFastRetrans_.

--- tcl/test/test-suite-ecn.tcl
+++ tcl/test/test-suite-ecn.tcl
@@ -30,7 +30,7 @@
 # OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 # SUCH DAMAGE.
 #
-# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/test-suite-ecn.tcl,v 1.37 2003/07/29 20:37:24 sfloyd Exp $
+# @(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tcl/test/test-suite-ecn.tcl,v 1.38 2003/07/29 21:36:35 sfloyd Exp $
 #
 # To run all tests: test-all-ecn
 
@@ -63,7 +63,6 @@ Agent/TCP set minrto_ 0
 Agent/TCP set syn_ false
 Agent/TCP set delay_growth_ false
 # In preparation for changing the default values for syn_ and delay_growth_.
-Agent/TCP set exitFastRetrans_ false
 
 set flowfile fairflow.tr; # file where flow data is written
 set flowgraphfile fairflow.xgr; # file given to graph tool 
