a011cc7883d0e6e417933d94cdeee6b2f52b7bd1
xuanc <xuanc>
2003-10-03 16:47:55 +0000

    Add event tracing support for simple MAC

---
+++ tcl/lan/ns-mac-simple.tcl
@@ -0,0 +1,33 @@
+# Copyright (c) 1999 by the University of Southern California
+# All rights reserved.
+#
+# Permission to use, copy, modify, and distribute this software and its
+# documentation in source and binary forms for non-commercial purposes
+# and without fee is hereby granted, provided that the above copyright
+# notice appear in all copies and that both the copyright notice and
+# this permission notice appear in supporting documentation. and that
+# any documentation, advertising materials, and other materials related
+# to such distribution and use acknowledge that the software was
+# developed by the University of Southern California, Information
+# Sciences Institute.  The name of the University may not be used to
+# endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THE UNIVERSITY OF SOUTHERN CALIFORNIA makes no representations about
+# the suitability of this software for any purpose.  THIS SOFTWARE IS
+# PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, WITHOUT LIMITATION, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+#
+# Other copyrights might apply to parts of this software and are so
+# noted when applicable.
+#
+# Event tracing support for simple MAC
+#   developed by Sushmita Aggarwal Singal (singal@nunki.usc.edu)
+
+Mac/Simple set debug_ false
+Mac/Simple instproc init {} {
+	eval $self next
+	set ns [Simulator instance]
+	$ns create-eventtrace Event $self
+}
