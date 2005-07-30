992d26ae2eb8ba21dcacaa623ba8477b7f2d0ad1
tomh <tomh>
2005-07-30 22:34:46 +0000

    Relicensed to GNU GPL per the consent of the original author, Pierre L'Ecuyer (lecuyer@iro.umontreal.ca)

--- tools/rng.h
+++ tools/rng.h
@@ -1,57 +1,58 @@
 /* -*-	Mode:C++; c-basic-offset:8; tab-width:8; indent-tabs-mode:t -*- */
 /*
- * Copyright (c) 1997 Regents of the University of California.
- * All rights reserved.
+ *  File:  RngStream.h for multiple streams of Random Numbers
+ *  Copyright (C) 2001  Pierre L'Ecuyer (lecuyer@iro.umontreal.ca)
  *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the distribution.
- * 3. All advertising materials mentioning features or use of this software
- *    must display the following acknowledgement:
- *	This product includes software developed by the Computer Systems
- *	Engineering Group at Lawrence Berkeley Laboratory.
- * 4. Neither the name of the University nor of the Laboratory may be used
- *    to endorse or promote products derived from this software without
- *    specific prior written permission.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
  *
- * THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
- * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
- * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
- * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
- * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ *  02110-1301 USA
  *
- * "@(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tools/rng.h,v 1.24 2003/12/17 21:56:15 haldar Exp $ (LBL)";
+ *  Linking this random number generator statically or dynamically with
+ *  other modules is making a combined work based on this random number
+ *  generator.  Thus, the terms and conditions of the GNU General Public
+ *  License cover the whole combination.
+ *
+ *  In addition, as a special exception, the copyright holders of this
+ *  random number generator give you permission to combine this random
+ *  number generator program with free software programs or libraries
+ *  that are released under the GNU LGPL and with code included in the
+ *  standard release of ns-2 under the Apache 2.0 license or under
+ *  otherwise-compatible licenses with advertising requirements (or
+ *  modified versions of such code, with unchanged license).  You may
+ *  copy and distribute such a system following the terms of the GNU GPL
+ *  for this random number generator and the licenses of the other code
+ *  concerned, provided  that you include the source code of that other
+ *  code when and as the GNU GPL requires distribution of source code.
+ *
+ *  Note that people who make modified versions of this random number
+ *  generator are not obligated to grant this special exception for
+ *  their modified versions; it is their choice whether to do so.
+ *  The GNU General Public License gives permission to release a
+ *  modified  version without this exception; this exception also makes
+ *  it possible to release a modified version which carries forward
+ *  this exception.
+ *
+ *  //Incorporated into rng.h and modified to maintain backward
+ *  //compatibility with ns-2.1b8.  Users can use their current scripts
+ *  //unmodified with the new RNG.  To get the same results as with the
+ *  //previous RNG, define OLD_RNG when compiling (e.g.,
+ *  //make -D OLD_RNG).
+ *  // - Michele Weigle, University of North Carolina
+ *  // (mcweigle@cs.unc.edu)  October 10, 2001
+ * 
+ * "@(#) $Header: /home/smtatapudi/Thesis/nsnam/nsnam/ns-2/tools/rng.h,v 1.25 2005/07/30 22:34:46 tomh Exp $ (LBL)";
  */
-
-/***********************************************************************\ 
-* 
-* File: RngStream.h for multiple streams of Random Numbers 
-* Language: C++ 
-* Copyright: Pierre L'Ecuyer, University of Montreal 
-* Notice: This code can be used freely for personnal, academic, 
-* or non-commercial purposes. For commercial purposes, 
-* please contact P. L'Ecuyer at: lecuyer@iro.umontreal.ca 
-* Date: 14 August 2001 
-* 
-* Incorporated into rng.h and modified to maintain backward 
-* compatibility with ns-2.1b8.  Users can use their current scripts 
-* unmodified with the new RNG.  To get the same results as with the 
-* previous RNG, define OLD_RNG when compiling (e.g., make -D OLD_RNG).
-* - Michele Weigle, University of North Carolina (mcweigle@cs.unc.edu)
-* October 10, 2001
-**********************************************************************/
 
 /* new random number generator */
 
