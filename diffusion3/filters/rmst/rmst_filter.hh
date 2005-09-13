dcf01b3ad2026b9f20245066ad1f0f790a0e2b35
tomh <tomh>
2005-09-13 04:53:45 +0000

    Added GPL specific exception to diffusion3 code

--- diffusion3/filters/rmst/rmst_filter.hh
+++ diffusion3/filters/rmst/rmst_filter.hh
@@ -5,7 +5,7 @@
 // Include file for RmstFilter - Reliable Multi-Segment Transport
 //
 // Copyright (C) 2003 by the University of Southern California
-// $Id: rmst_filter.hh,v 1.2 2003/07/10 21:18:57 haldar Exp $
+// $Id: rmst_filter.hh,v 1.3 2005/09/13 04:53:49 tomh Exp $
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License,
@@ -20,7 +20,27 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 //
+// Linking this file statically or dynamically with other modules is making
+// a combined work based on this file.  Thus, the terms and conditions of
+// the GNU General Public License cover the whole combination.
 //
+// In addition, as a special exception, the copyright holders of this file
+// give you permission to combine this file with free software programs or
+// libraries that are released under the GNU LGPL and with code included in
+// the standard release of ns-2 under the Apache 2.0 license or under
+// otherwise-compatible licenses with advertising requirements (or modified
+// versions of such code, with unchanged license).  You may copy and
+// distribute such a system following the terms of the GNU GPL for this
+// file and the licenses of the other code concerned, provided that you
+// include the source code of that other code when and as the GNU GPL
+// requires distribution of source code.
+//
+// Note that people who make modified versions of this file are not
+// obligated to grant this special exception for their modified versions;
+// it is their choice whether to do so.  The GNU General Public License
+// gives permission to release a modified version without this exception;
+// this exception also makes it possible to release a modified version
+// which carries forward this exception.
 
 #ifndef RMST_FILTER_HH
 #define RMST_FILTER_HH
--- diffusion3/lib/dr.cc
+++ diffusion3/lib/dr.cc
@@ -3,7 +3,7 @@
 // authors         : John Heidemann and Fabio Silva
 //
 // Copyright (C) 2000-2003 by the University of Southern California
-// $Id: dr.cc,v 1.16 2004/01/08 23:05:53 haldar Exp $
+// $Id: dr.cc,v 1.17 2005/09/13 04:53:49 tomh Exp $
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License,
@@ -18,6 +18,27 @@
 // with this program; if not, write to the Free Software Foundation, Inc.,
 // 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
 //
+// Linking this file statically or dynamically with other modules is making
+// a combined work based on this file.  Thus, the terms and conditions of
+// the GNU General Public License cover the whole combination.
+//
+// In addition, as a special exception, the copyright holders of this file
+// give you permission to combine this file with free software programs or
+// libraries that are released under the GNU LGPL and with code included in
+// the standard release of ns-2 under the Apache 2.0 license or under
+// otherwise-compatible licenses with advertising requirements (or modified
+// versions of such code, with unchanged license).  You may copy and
+// distribute such a system following the terms of the GNU GPL for this
+// file and the licenses of the other code concerned, provided that you
+// include the source code of that other code when and as the GNU GPL
+// requires distribution of source code.
+//
+// Note that people who make modified versions of this file are not
+// obligated to grant this special exception for their modified versions;
+// it is their choice whether to do so.  The GNU General Public License
+// gives permission to release a modified version without this exception;
+// this exception also makes it possible to release a modified version
+// which carries forward this exception.
 //
 
 #include <stdlib.h>
